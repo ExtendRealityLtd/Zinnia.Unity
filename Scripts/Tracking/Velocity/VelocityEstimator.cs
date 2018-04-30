dffb69bbabee1c4403f1b1c6fc1d27a38708fc33
Harvey Ball <me@harveyball.com>
2018-05-01 00:17:46 +0100

    feat(Velocity): ability to track velocity of a given object
    The Velocity Estimator class takes samples of the position and rotation
    of a given Transform over a set number of frames and then uses that
    cached data to estimate the object's velocity and angular velocity.
    
    The Velocity Estimator has been added to the UnityXRCameraRig prefab
    so it has a basic way of tracking the velocities of the tracked objects
    such as the HMD and controllers.

---
+++ Scripts/Tracking/Velocity/VelocityEstimator.cs
@@ -0,0 +1,205 @@
+﻿namespace VRTK.Core.Tracking.Velocity
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The VelocityEstimator takes average samples of a Transform position and rotation and uses this cached data to estimate velocity and angular velocity.
+    /// </summary>
+    public class VelocityEstimator : VelocityTracker
+    {
+        [Tooltip("The source transform to track and estimate velocities for.")]
+        public Transform source;
+        [Tooltip("Automatically begin collecting samples for estimation.")]
+        public bool autoStartSampling = true;
+        [Tooltip("The number of average frames to collect samples for velocity estimation.")]
+        public int velocityAverageFrames = 5;
+        [Tooltip("The number of average frames to collect samples for angular velocity estimation.")]
+        public int angularVelocityAverageFrames = 10;
+
+        protected bool collectSamples;
+        protected int currentSampleCount = 0;
+        protected Vector3[] velocitySamples = new Vector3[0];
+        protected Vector3[] angularVelocitySamples = new Vector3[0];
+        protected Vector3 previousPosition = Vector3.zero;
+        Quaternion previousRotation = Quaternion.identity;
+
+        /// <summary>
+        /// The IsActive method returns the state of whether the component is active.
+        /// </summary>
+        /// <returns>Returns `true` if the component is considered active.</returns>
+        public override bool IsActive()
+        {
+            return (source != null && source.gameObject.activeInHierarchy && isActiveAndEnabled);
+        }
+
+        /// <summary>
+        /// The GetVelocity method returns the velocity being tracked.
+        /// </summary>
+        /// <returns>A Vector3 of the current tracked velocity.</returns>
+        public override Vector3 GetVelocity()
+        {
+            return GetEstimate(velocitySamples);
+        }
+
+        /// <summary>
+        /// The GetAngularVelocityMethod returns the angular velocity being tracked.
+        /// </summary>
+        /// <returns>A Vector3 of the current tracked angular velocity.</returns>
+        public override Vector3 GetAngularVelocity()
+        {
+            return GetEstimate(angularVelocitySamples);
+        }
+
+        /// <summary>
+        /// The StartEstimation method manually enables the collecting of samples for the estimation process.
+        /// </summary>
+        public virtual void StartEstimation()
+        {
+            collectSamples = true;
+        }
+
+        /// <summary>
+        /// The EndEstimation method manually disables the collecting of samples for the estimation process.
+        /// </summary>
+        public virtual void EndEstimation()
+        {
+            collectSamples = false;
+        }
+
+        /// <summary>
+        /// The IsEstimating method returns whether the VelocityEstimator is currently collecting samples.
+        /// </summary>
+        /// <returns>Returns `true` if samples are currently being collected.</returns>
+        public virtual bool IsEstimating()
+        {
+            return collectSamples;
+        }
+
+        /// <summary>
+        /// The GetAcceleration method returns the acceleration of the tracked object.
+        /// </summary>
+        /// <returns>A Vector3 of the acceleration of the tracked object.</returns>
+        public virtual Vector3 GetAcceleration()
+        {
+            Vector3 average = Vector3.zero;
+            for (int i = 2 + currentSampleCount - velocitySamples.Length; i < currentSampleCount; i++)
+            {
+                if (i >= 2)
+                {
+                    int first = i - 2;
+                    int second = i - 1;
+
+                    Vector3 v1 = velocitySamples[first % velocitySamples.Length];
+                    Vector3 v2 = velocitySamples[second % velocitySamples.Length];
+                    average += v2 - v1;
+                }
+            }
+            average *= GetFactor();
+            return average;
+        }
+
+        protected virtual void OnEnable()
+        {
+            velocitySamples = new Vector3[velocityAverageFrames];
+            angularVelocitySamples = new Vector3[angularVelocityAverageFrames];
+            previousPosition = (source != null ? source.localPosition : Vector3.zero);
+            previousRotation = (source != null ? source.localRotation : Quaternion.identity);
+            collectSamples = autoStartSampling;
+        }
+
+        protected virtual void LateUpdate()
+        {
+            ProcessEstimation();
+        }
+
+        /// <summary>
+        /// The GetFactor method calulates the multiplication factor for the velocities.
+        /// </summary>
+        /// <returns>A float multiplication value.</returns>
+        protected virtual float GetFactor()
+        {
+            return 1.0f / Time.deltaTime;
+        }
+
+        /// <summary>
+        /// The GetEstimate method calculates the average estiamte for the given sample set.
+        /// </summary>
+        /// <param name="samples">A Vector3 array of samples to estimate with.</param>
+        /// <returns>A Vector3 of the estimated result.</returns>
+        protected virtual Vector3 GetEstimate(Vector3[] samples)
+        {
+            Vector3 estimate = Vector3.zero;
+            int sampleCount = Mathf.Min(currentSampleCount, samples.Length);
+            if (sampleCount != 0)
+            {
+                for (int i = 0; i < sampleCount; i++)
+                {
+                    estimate += samples[i];
+                }
+                estimate *= (1.0f / sampleCount);
+            }
+            return estimate;
+        }
+
+        /// <summary>
+        /// The ProcessEstimation method collects the appropriate samples for velocities and estimates the results.
+        /// </summary>
+        protected virtual void ProcessEstimation()
+        {
+            if (collectSamples)
+            {
+                float factor = GetFactor();
+                EstimateVelocity(factor);
+                EstimateAngularVelocity(factor);
+                currentSampleCount++;
+            }
+            else
+            {
+                currentSampleCount = 0;
+            }
+        }
+
+        /// <summary>
+        /// The EstimateVelocity method collects samples for velocity.
+        /// </summary>
+        /// <param name="factor">The multiplier to apply to the transform difference.</param>
+        protected virtual void EstimateVelocity(float factor)
+        {
+            Vector3 currentPosition = (source != null ? source.localPosition : Vector3.zero);
+            if (velocitySamples.Length > 0)
+            {
+                int sampleIndex = currentSampleCount % velocitySamples.Length;
+                velocitySamples[sampleIndex] = factor * (currentPosition - previousPosition);
+                previousPosition = currentPosition;
+            }
+        }
+
+        /// <summary>
+        /// The EstimateAngularVelocity method collects samples for angular velocity.
+        /// </summary>
+        /// <param name="factor">The multiplier to apply to the transform difference.</param>
+        protected virtual void EstimateAngularVelocity(float factor)
+        {
+            Quaternion currentRotation = (source != null ? source.localRotation : Quaternion.identity);
+            Quaternion deltaRotation = currentRotation * Quaternion.Inverse(previousRotation);
+            float theta = 2.0f * Mathf.Acos(Mathf.Clamp(deltaRotation.w, -1.0f, 1.0f));
+            if (theta > Mathf.PI)
+            {
+                theta -= 2.0f * Mathf.PI;
+            }
+
+            Vector3 angularVelocity = new Vector3(deltaRotation.x, deltaRotation.y, deltaRotation.z);
+            if (angularVelocity.sqrMagnitude > 0.0f)
+            {
+                angularVelocity = theta * factor * angularVelocity.normalized;
+            }
+
+            if (angularVelocitySamples.Length > 0)
+            {
+                int sampleIndex = currentSampleCount % angularVelocitySamples.Length;
+                angularVelocitySamples[sampleIndex] = angularVelocity;
+                previousRotation = currentRotation;
+            }
+        }
+    }
+}
---
+++ Tests/Editor/Tracking/Velocity/VelocityEstimatorTest.cs
@@ -0,0 +1,389 @@
+﻿namespace VRTK.Core.Tracking.Velocity
+{
+    using UnityEngine;
+    using NUnit.Framework;
+
+    public class VelocityEstimatorTest
+    {
+        private GameObject containingObject;
+        private Transform source;
+        private VelocityEstimatorMock subject;
+
+        private Vector3[] exampleSourcePositions = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),
+            new Vector3(1f, 1f, 1f),
+            new Vector3(3f, 3f, 3f),
+            new Vector3(6f, 6f, 6f),
+            new Vector3(8f, 8f, 8f)
+        };
+
+        private Quaternion[] exampleSourceRotations = new Quaternion[]
+        {
+            new Quaternion(1f, 0f, 0f, 0f),
+            new Quaternion(0.707f, 0.707f, 0f, 0f),
+            new Quaternion(0.707f, 0f, 0.707f, 0f),
+            new Quaternion(0.707f, 0f, 45f, 0.707f),
+            new Quaternion(0f, 0.707f, 0f, 0.707f)
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            source = containingObject.transform;
+            subject = containingObject.AddComponent<VelocityEstimatorMock>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            subject = null;
+            source = null;
+            containingObject = null;
+        }
+
+        [Test]
+        public void IsActiveSourceActive()
+        {
+            subject.source = source;
+
+            bool actualResult = subject.IsActive();
+            Assert.IsTrue(actualResult);
+        }
+
+        [Test]
+        public void IsActiveSourceInActive()
+        {
+            source.gameObject.SetActive(false);
+            subject.source = source;
+
+            bool actualResult = subject.IsActive();
+            Assert.IsFalse(actualResult);
+        }
+
+        [Test]
+        public void IsActiveNoSource()
+        {
+            bool actualResult = subject.IsActive();
+            Assert.IsFalse(actualResult);
+        }
+
+        [Test]
+        public void StartEstimation()
+        {
+            subject.autoStartSampling = false;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            Assert.IsTrue(subject.IsEstimating());
+        }
+
+        [Test]
+        public void EndEstimation()
+        {
+            subject.autoStartSampling = false;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+            subject.EndEstimation();
+
+            Assert.IsFalse(subject.IsEstimating());
+        }
+
+        [Test]
+        public void AutoStartEstimation()
+        {
+            subject.autoStartSampling = true;
+            subject.ManualOnEnable();
+
+            Assert.IsTrue(subject.IsEstimating());
+        }
+
+        [Test]
+        public void GetVelocityAutoStartEstimating()
+        {
+            Vector3 expectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = true;
+            subject.ManualOnEnable();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetVelocityOver0Frames()
+        {
+            Vector3 expectedResult = new Vector3(0f, 0f, 0f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.velocityAverageFrames = 0;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetVelocityOver4Frames()
+        {
+            Vector3 expectedResult = new Vector3(2f, 2f, 2f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.velocityAverageFrames = 4;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetVelocityOver5Frames()
+        {
+            Vector3 expectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.velocityAverageFrames = 5;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetVelocityWhenNotEstimating()
+        {
+            Vector3 expectedResult = new Vector3(0f, 0f, 0f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.velocityAverageFrames = 0;
+            subject.ManualOnEnable();
+            subject.EndEstimation();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetVelocityWithoutSource()
+        {
+            Vector3 expectedResult = new Vector3(0f, 0f, 0f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityAutoStartEstimating()
+        {
+            Vector3 expectedResult = new Vector3(0.4172499f, -0.5427582f, -0.36673f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = true;
+            subject.angularVelocityAverageFrames = 5;
+            subject.ManualOnEnable();
+
+            ProcessRotations(exampleSourceRotations);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult.ToString(), actualResult.ToString());
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityOver0Frames()
+        {
+            Vector3 expectedResult = new Vector3(0f, 0f, 0f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.angularVelocityAverageFrames = 0;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessRotations(exampleSourceRotations);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityOver4Frames()
+        {
+            Vector3 expectedResult = new Vector3(-0.2638358f, -0.6784477f, -0.4584125f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.angularVelocityAverageFrames = 4;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessRotations(exampleSourceRotations);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult.ToString(), actualResult.ToString());
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityOver5Frames()
+        {
+            Vector3 expectedResult = new Vector3(0.4172499f, -0.5427582f, -0.36673f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.angularVelocityAverageFrames = 5;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessRotations(exampleSourceRotations);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult.ToString(), actualResult.ToString());
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityWhenNotEstimating()
+        {
+            Vector3 expectedResult = new Vector3(0f, 0f, 0f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.angularVelocityAverageFrames = 0;
+            subject.ManualOnEnable();
+            subject.EndEstimation();
+
+            ProcessRotations(exampleSourceRotations);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityWithoutSource()
+        {
+            Vector3 expectedResult = new Vector3(0f, 0f, 0f);
+            Vector3 unexpectedResult = new Vector3(1.6f, 1.6f, 1.6f);
+
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessRotations(exampleSourceRotations);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAccelerationOver5Frames()
+        {
+            Vector3 expectedResult = new Vector3(3f, 3f, 3f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            // Ensure the subject has a valid source to check
+            subject.source = source;
+            subject.autoStartSampling = false;
+            subject.velocityAverageFrames = 5;
+            subject.ManualOnEnable();
+            subject.StartEstimation();
+
+            ProcessPositions(exampleSourcePositions);
+
+            Vector3 actualResult = subject.GetAcceleration();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        private void ProcessPositions(Vector3[] positions)
+        {
+            //Loop through the set custom positions and update the source's position then manually process the subject emulating an update loop
+            foreach (Vector3 updatedPosition in positions)
+            {
+                source.localPosition = updatedPosition;
+                subject.ManualLateUpdate();
+            }
+        }
+
+        private void ProcessRotations(Quaternion[] rotations)
+        {
+            //Loop through the set custom rotations and update the source's rotation then manually process the subject emulating an update loop
+            foreach (Quaternion updatedRotation in rotations)
+            {
+                source.localRotation = updatedRotation;
+                subject.ManualLateUpdate();
+            }
+        }
+    }
+
+    public class VelocityEstimatorMock : VelocityEstimator
+    {
+        public void ManualOnEnable()
+        {
+            OnEnable();
+        }
+
+        public void ManualLateUpdate()
+        {
+            ProcessEstimation();
+        }
+
+        protected override float GetFactor()
+        {
+            return 1f;
+        }
+    }
+}
