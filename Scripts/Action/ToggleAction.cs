f5097bc17ae7cea83fe4cc8ea2acb657b29c8492
Harvey Ball <me@harveyball.com>
2018-05-03 19:06:57 +0100

    feat(Input): action to provide toggle events
    The Toggle Action always emits the Activated action the first time the
    Receive method is called and always emits the Deactivated action the
    second time the Receive method is called, thus allowing a relevant
    event to emit to provide the effect of toggling.

---
+++ Scripts/Action/ToggleAction.cs
@@ -0,0 +1,30 @@
+﻿namespace VRTK.Core.Action
+{
+    /// <summary>
+    /// The BooleanAction emits a boolean value.
+    /// </summary>
+    public class ToggleAction : BooleanAction
+    {
+        /// <summary>
+        /// The Receive method allows an action to receive the payload from another action to enable action chaining.
+        /// </summary>
+        /// <param name="value">The value from the action.</param>
+        /// <param name="sender">The sender of the action.</param>
+        public override void Receive(bool value, object sender = null)
+        {
+            previousValue = Value;
+            Value = value;
+
+            if (!state)
+            {
+                OnActivated(true);
+            }
+            else
+            {
+                OnDeactivated(false);
+            }
+
+            OnChanged(value);
+        }
+    }
+}
