5974bca52f9e3250a62cf51002dede3fc89a0f26
Harvey Ball <me@harveyball.com>
2018-05-13 09:59:04 +0100

    feat(Action): determine if surface position has had specified change
    The SurfaceChangeAction digests SurfaceData and compares the current
    surface position and the previous surface position and checks to see if
    the distance between the two positions is within the specified
    threshold.
    
    This can be useful to capture SurfaceLocator events and only carry out
    actions if the surface difference is within the threshold. A good use
    case is only blink the camera if the surface difference is over a
    certain step size.

---
+++ Tests/Editor/Action/SurfaceChangeActionTest.cs
@@ -0,0 +1,101 @@
+﻿namespace VRTK.Core.Action
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using VRTK.Core.Utility.Mock;
+    using VRTK.Core.Data.Type;
+
+    public class SurfaceChangeActionTest
+    {
+        private GameObject containingObject;
+        private SurfaceChangeActionMock subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<SurfaceChangeActionMock>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void SurfaceChanged()
+        {
+            UnityEventListenerMock activatedListenerMock = new UnityEventListenerMock();
+            subject.Activated.AddListener(activatedListenerMock.Listen);
+            subject.changeDistance = 1f;
+            subject.checkAxis = Vector3State.True;
+
+            SurfaceData surfaceData = new SurfaceData(Vector3.one, Vector3.down);
+
+            //set current surface to zero
+            RaycastHit ray = new RaycastHit
+            {
+                point = Vector3.zero
+            };
+            surfaceData.CollisionData = ray;
+            surfaceData.positionOverride = ray.point;
+            //set surface to one so there is a change between surface positions
+            ray = new RaycastHit
+            {
+                point = Vector3.one
+            };
+            surfaceData.CollisionData = ray;
+            surfaceData.positionOverride = ray.point;
+
+            subject.Receive(surfaceData, null);
+
+            Assert.IsTrue(activatedListenerMock.Received);
+        }
+
+        [Test]
+        public void SurfaceUnchanged()
+        {
+            UnityEventListenerMock activatedListenerMock = new UnityEventListenerMock();
+            subject.Activated.AddListener(activatedListenerMock.Listen);
+            subject.changeDistance = 1f;
+            subject.checkAxis = new Vector3State(false, true, false);
+
+            SurfaceData surfaceData = new SurfaceData(Vector3.one, Vector3.down);
+
+            //set current surface to zero
+            RaycastHit ray = new RaycastHit
+            {
+                point = Vector3.zero
+            };
+            surfaceData.CollisionData = ray;
+            surfaceData.positionOverride = ray.point;
+            //set surface to one so there is a change between surface positions
+            ray = new RaycastHit
+            {
+                point = Vector3.one
+            };
+            surfaceData.CollisionData = ray;
+            surfaceData.positionOverride = ray.point;
+
+            subject.Receive(surfaceData, null);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+        }
+    }
+
+    public class SurfaceChangeActionMock : SurfaceChangeAction
+    {
+        //As The transform in the RaycastHit cannot be set without doing an actual raycast, just ignore that check for the test.
+        protected override bool ValidSurfaceData(SurfaceData surfaceData)
+        {
+            return true;
+        }
+
+        protected override Vector3 GetCollisionPoint(RaycastHit collisionData)
+        {
+            return GeneratePoint(collisionData.point);
+        }
+    }
+}
