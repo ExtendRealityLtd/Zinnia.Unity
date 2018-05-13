6ac70e58cc0326bd109861507ccf94ea2f075d56
Harvey Ball <me@harveyball.com>
2018-05-13 09:33:41 +0100

    chore(Extension): add tests for component extensions
    The ComponentExtensions now have some unit tests.

---
+++ Tests/Editor/Extension/ComponentExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿namespace VRTK.Core.Extension
+{
+    using UnityEngine;
+    using NUnit.Framework;
+
+    public class ComponentExtensionsTest
+    {
+        [Test]
+        public void TryGetTransformValid()
+        {
+            Transform validTransform = new GameObject().transform;
+            Assert.NotNull(validTransform.TryGetTransform());
+            Object.DestroyImmediate(validTransform.gameObject);
+        }
+
+        [Test]
+        public void TryGetTransformInvalid()
+        {
+            Component invalidComponent = new Component();
+            Assert.IsNull(invalidComponent.TryGetTransform());
+            Object.DestroyImmediate(invalidComponent);
+        }
+    }
+}
