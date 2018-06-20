19f2cb5ae21e187c70c911109e71f95bf683dccf
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-06-20 16:45:15 +0200

    feat(Tracking): allow chaining collision notifications
    To allow interactors like a controller to interact with an unlimited
    number of interactables performance has to be taken into
    consideration. Instead of all interactables taking part of Unity's
    physics notifications and checking whether the thing they collided
    with is an interactor they want to work with, this change allows to
    forward the collision events from the interactor to the interactable
    it's colliding with. To give the receiving side of a forwarded event
    all the needed information the sender of the forwarded collision
    notification is added to the `EventData`. A check against the
    forwarded collision's game object prevents an infinite loop when a
    collision would otherwise get forwarded to itself forever.
    
    Since the `CollisionTracker` always took part in the collision
    notifications via Unity message methods this change also splits the
    collision notification from the collision tracking by introducing a
    new superclass that only provides events to get emitted on calling
    its methods to do so. The existing tracker now subclasses this new
    class and emits the respective event in response to Unity notifying
    it about a collision.

--- Scripts/Tracking/CollisionTracker.cs
+++ Scripts/Tracking/CollisionTracker.cs
@@ -1,145 +1,40 @@
 ﻿namespace VRTK.Core.Tracking
 {
     using UnityEngine;
-    using UnityEngine.Events;
-    using System;
-    using VRTK.Core.Data.Attribute;
 
     /// <summary>
     /// Tracks collisions on the <see cref="GameObject"/> this component is on.
     /// </summary>
-    public class CollisionTracker : MonoBehaviour
+    public class CollisionTracker : CollisionNotifier
     {
-        /// <summary>
-        /// Holds data about a <see cref="CollisionTracker"/> event.
-        /// </summary>
-        [Serializable]
-        public class EventData
-        {
-            /// <summary>
-            /// Whether the collision was observed through a <see cref="Collider"/> with <see cref="Collider.isTrigger"/> set.
-            /// </summary>
-            public bool isTrigger;
-            /// <summary>
-            /// The observed <see cref="Collision"/>. <see langword="null"/> if <see cref="isTrigger"/> is <see langword="true"/>.
-            /// </summary>
-            public Collision collision;
-            /// <summary>
-            /// The observed <see cref="Collider"/>.
-            /// </summary>
-            public Collider collider;
-
-            public EventData Set(EventData source)
-            {
-                return Set(source.isTrigger, source.collision, source.collider);
-            }
-
-            public EventData Set(bool isTrigger, Collision collision, Collider collider)
-            {
-                this.isTrigger = isTrigger;
-                this.collision = collision;
-                this.collider = collider;
-                return this;
-            }
-
-            public void Clear()
-            {
-                Set(default(bool), default(Collision), default(Collider));
-            }
-        }
-
-        /// <summary>
-        /// Defines the event with the <see cref="EventData"/>.
-        /// </summary>
-        [Serializable]
-        public class UnityEvent : UnityEvent<EventData>
-        {
-        }
-
-        /// <summary>
-        /// The types of collisions that events will be emitted for.
-        /// </summary>
-        [Flags]
-        public enum CollisionTypes
-        {
-            /// <summary>
-            /// A regular, non-trigger collision.
-            /// </summary>
-            Collision = 1 << 0,
-            /// <summary>
-            /// A trigger collision
-            /// </summary>
-            Trigger = 1 << 1
-        }
-
-        /// <summary>
-        /// The types of collisions that events will be emitted for.
-        /// </summary>
-        [UnityFlags]
-        [Tooltip("The types of collisions that events will be emitted for.")]
-        public CollisionTypes emittedTypes = (CollisionTypes)(-1);
-
-        /// <summary>
-        /// Emitted when a collision starts.
-        /// </summary>
-        public UnityEvent CollisionStarted = new UnityEvent();
-        /// <summary>
-        /// Emitted when the current collision changes.
-        /// </summary>
-        public UnityEvent CollisionChanged = new UnityEvent();
-        /// <summary>
-        /// Emitted when the current collision stops.
-        /// </summary>
-        public UnityEvent CollisionStopped = new UnityEvent();
-
-        protected EventData eventData = new EventData();
-
         protected virtual void OnCollisionEnter(Collision collision)
         {
-            if (emittedTypes.HasFlag(CollisionTypes.Collision))
-            {
-                CollisionStarted?.Invoke(eventData.Set(false, collision, collision.collider));
-            }
+            OnCollisionStarted(eventData.Set(this, false, collision, collision.collider));
         }
 
         protected virtual void OnCollisionStay(Collision collision)
         {
-            if (emittedTypes.HasFlag(CollisionTypes.Collision))
-            {
-                CollisionChanged?.Invoke(eventData.Set(false, collision, collision.collider));
-            }
+            OnCollisionChanged(eventData.Set(this, false, collision, collision.collider));
         }
 
         protected virtual void OnCollisionExit(Collision collision)
         {
-            if (emittedTypes.HasFlag(CollisionTypes.Collision))
-            {
-                CollisionStopped?.Invoke(eventData.Set(false, collision, collision.collider));
-            }
+            OnCollisionStopped(eventData.Set(this, false, collision, collision.collider));
         }
 
         protected virtual void OnTriggerEnter(Collider collider)
         {
-            if (emittedTypes.HasFlag(CollisionTypes.Trigger))
-            {
-                CollisionStarted?.Invoke(eventData.Set(true, null, collider));
-            }
+            OnCollisionStarted(eventData.Set(this, true, null, collider));
         }
 
         protected virtual void OnTriggerStay(Collider collider)
         {
-            if (emittedTypes.HasFlag(CollisionTypes.Trigger))
-            {
-                CollisionChanged?.Invoke(eventData.Set(true, null, collider));
-            }
+            OnCollisionChanged(eventData.Set(this, true, null, collider));
         }
 
         protected virtual void OnTriggerExit(Collider collider)
         {
-            if (emittedTypes.HasFlag(CollisionTypes.Trigger))
-            {
-                CollisionStopped?.Invoke(eventData.Set(true, null, collider));
-            }
+            OnCollisionStopped(eventData.Set(this, true, null, collider));
         }
     }
 }
