4c0840abf241b9d4be0870a4b9681c2999da8104
Harvey Ball <me@harveyball.com>
2018-06-22 00:08:49 +0100

    feat(Velocity): ability to apply velocity data to a target rigidbody
    The ApplyVelocity component takes the data from a VelocityTracker
    and attempts to apply it to a given Rigidbody component.

---
+++ Tests/Editor/Tracking/Velocity/ApplyVelocityTest.cs
@@ -0,0 +1,55 @@
+﻿namespace VRTK.Core.Tracking.Velocity
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using VRTK.Core.Utility.Mock;
+
+    public class ApplyVelocityTest
+    {
+        private GameObject containingObject;
+        private ApplyVelocity subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<ApplyVelocity>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Apply()
+        {
+            VelocityTrackerMock tracker = VelocityTrackerMock.Generate(true, Vector3.one, Vector3.one);
+            subject.source = tracker;
+            subject.target = containingObject.AddComponent<Rigidbody>();
+            subject.Apply();
+            Assert.AreEqual(tracker.GetVelocity(), subject.target.velocity);
+            Assert.AreEqual(tracker.GetAngularVelocity(), subject.target.angularVelocity);
+
+            Object.DestroyImmediate(tracker.gameObject);
+        }
+
+        [Test]
+        public void ApplyNoSource()
+        {
+            subject.target = containingObject.AddComponent<Rigidbody>();
+
+            Vector3 originalVelocity = subject.target.velocity;
+            Vector3 originalAngularVelocity = subject.target.angularVelocity;
+
+            Assert.AreEqual(Vector3.zero, originalVelocity);
+            Assert.AreEqual(Vector3.zero, originalAngularVelocity);
+
+            subject.Apply();
+            Assert.AreEqual(originalVelocity, subject.target.velocity);
+            Assert.AreEqual(originalAngularVelocity, subject.target.angularVelocity);
+        }
+    }
+}
