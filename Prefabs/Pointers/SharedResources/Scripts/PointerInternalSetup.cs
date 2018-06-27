b2dab728a35da8dd33d321c0ec7cbad8ed3ce1d3
Harvey Ball <me@harveyball.com>
2018-06-27 13:01:18 +0100

    feat(Pointers): add prefab helper script for easier setup
    A new set of pointer prefab helper scripts have been added to the
    pointer prefabs. The main User setup script allows easier set up
    of the target Transform to get the pointer to follow without needing
    to go into the pointer hierarchy and add the transform to follow to
    the ObjectFollow script.

---
+++ Prefabs/Pointers/SharedResources/Scripts/PointerInternalSetup.cs
@@ -0,0 +1,31 @@
+﻿namespace VRTK.Core.Prefabs.Pointers
+{
+    using UnityEngine;
+    using VRTK.Core.Tracking.Follow;
+
+    /// <summary>
+    /// Provides internal settings for the pointer prefabs.
+    /// </summary>
+    public class PointerInternalSetup : MonoBehaviour
+    {
+        /// <summary>
+        /// The <see cref="PointerUserSetup"/> for providing required follow target information.
+        /// </summary>
+        [Tooltip("The Pointer User Setup for providing required follow target information.")]
+        public PointerUserSetup userSetup;
+        /// <summary>
+        /// The <see cref="ObjectFollow"/> component to set the target <see cref="Transform"/> on.
+        /// </summary>
+        [Tooltip("The Object Follow component to set the target Transform on.")]
+        public ObjectFollow objectFollow;
+
+        protected virtual void OnEnable()
+        {
+            if (objectFollow != null)
+            {
+                objectFollow.targetComponents.Clear();
+                objectFollow.targetComponents.Add(userSetup.followTarget);
+            }
+        }
+    }
+}
---
+++ Prefabs/Pointers/SharedResources/Scripts/PointerUserSetup.cs
@@ -0,0 +1,16 @@
+﻿namespace VRTK.Core.Prefabs.Pointers
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Provides user specific setup information for the pointer prefabs.
+    /// </summary>
+    public class PointerUserSetup : MonoBehaviour
+    {
+        /// <summary>
+        /// The <see cref="Transform"/> target for the pointer to follow around.
+        /// </summary>
+        [Tooltip("The Transform target for the pointer to follow around.")]
+        public Transform followTarget;
+    }
+}
