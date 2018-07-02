b77a4a30fdcfdd60459dc56ad05aa0547288a28a
Harvey Ball <me@harveyball.com>
2018-07-02 15:10:37 +0100

    feat(Velocity): provide game object proxy for velocity tracker
    The `VelocityTrackerGameObjectProxy" component allows a generic
    GameObject to be specified as a VelocityTracker and the component
    will look up the first available VelocityTracker on that GameObject.
    
    This is useful if the VelocityTracker needs updating at runtime but
    only a containing GameObject is known about.

---
+++ Scripts/Tracking/Velocity/VelocityTrackerGameObjectProxy.cs
@@ -0,0 +1,58 @@
+﻿namespace VRTK.Core.Tracking.Velocity
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Attempts to utilise the first <see cref="VelocityTracker"/> found on the given proxy <see cref="GameObject"/>.
+    /// </summary>
+    public class VelocityTrackerGameObjectProxy : VelocityTracker
+    {
+        /// <summary>
+        /// The <see cref="GameObject"/> that contains a <see cref="VelocityTracker"/>.
+        /// </summary>
+        [Tooltip("The GameObject that contains a VelocityTracker.")]
+        public GameObject proxySource;
+
+        protected VelocityTracker source;
+
+        /// <inheritdoc />
+        public override bool IsActive()
+        {
+            return (isActiveAndEnabled && source != null && source.isActiveAndEnabled);
+        }
+
+        /// <summary>
+        /// Sets the source of the <see cref="VelocityTracker"/> proxy.
+        /// </summary>
+        /// <param name="proxySource">The <see cref="GameObject"/> that contains a <see cref="VelocityTracker"/>.</param>
+        public virtual void SetProxySource(GameObject proxySource)
+        {
+            this.proxySource = proxySource;
+            if (this.proxySource != null)
+            {
+                source = proxySource.GetComponentInChildren<VelocityTracker>();
+            }
+        }
+
+        /// <summary>
+        /// Clears the existing proxy source.
+        /// </summary>
+        public virtual void ClearProxySource()
+        {
+            proxySource = null;
+            source = null;
+        }
+
+        /// <inheritdoc />
+        protected override Vector3 DoGetVelocity()
+        {
+            return source.GetVelocity();
+        }
+
+        /// <inheritdoc />
+        protected override Vector3 DoGetAngularVelocity()
+        {
+            return source.GetAngularVelocity();
+        }
+    }
+}
