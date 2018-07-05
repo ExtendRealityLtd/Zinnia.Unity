25dd6203048b9788280dd000a700aa4513cb67b4
Harvey Ball <me@harveyball.com>
2018-07-05 17:30:17 +0100

    feat(Collision): ability to create a game object at a collision point
    The CollisionPointContainer has the mechanism of being able to accept
    an active collision and generate a GameObject at the point the
    collision initially occured.
    
    This can be utilised in interactions to determine a precise point of
    grabbing an object.

---
+++ Scripts/Tracking/Collision/Active/CollisionPointContainer.cs
@@ -0,0 +1,81 @@
+﻿namespace VRTK.Core.Tracking.Collision.Active
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using VRTK.Core.Extension;
+
+    /// <summary>
+    /// Contains a <see cref="GameObject"/> at the point of a collision from the event data of an <see cref="ActiveCollisionConsumer"/>.
+    /// </summary>
+    public class CollisionPointContainer : MonoBehaviour
+    {
+        /// <summary>
+        /// Defines the event for the generated collision point <see cref="GameObject"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<GameObject>
+        {
+        }
+
+        /// <summary>
+        /// Emitted when the collision point container is created.
+        /// </summary>
+        public UnityEvent Created = new UnityEvent();
+        /// <summary>
+        /// Emitted when the collision point container is cleared.
+        /// </summary>
+        public UnityEvent Cleared = new UnityEvent();
+
+        /// <summary>
+        /// The created container;
+        /// </summary>
+        public GameObject Container
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// Creates a new container at the point of the collision in the given event data.
+        /// </summary>
+        /// <param name="eventData">Contains data about the collision.</param>
+        public virtual void Create(ActiveCollisionConsumer.EventData eventData)
+        {
+            if (!isActiveAndEnabled || eventData.publisher?.sourceContainer == null || eventData.currentCollision?.collider?.GetContainingTransform() == null)
+            {
+                return;
+            }
+
+            GameObject collisionInitiator = eventData.publisher.sourceContainer;
+            GameObject collidingObject = eventData.currentCollision?.collider?.GetContainingTransform().gameObject;
+
+            DestroyContainer();
+            Container = new GameObject(String.Format("[VRTK][CollisionPointContainer][{0}][{1}]", collisionInitiator.name, collidingObject.name));
+            Container.transform.SetParent(collisionInitiator.transform);
+            Container.transform.position = collidingObject.transform.position;
+            Container.transform.rotation = collidingObject.transform.rotation;
+            Container.transform.localScale = Vector3.one;
+
+            Created?.Invoke(Container);
+        }
+
+        /// <summary>
+        /// Clears the existing created container data.
+        /// </summary>
+        public virtual void Clear()
+        {
+            Cleared?.Invoke(Container);
+            DestroyContainer();
+        }
+
+        /// <summary>
+        /// Destroys the container object.
+        /// </summary>
+        protected virtual void DestroyContainer()
+        {
+            Destroy(Container);
+            Container = null;
+        }
+    }
+}
---
+++ Tests/Editor/Tracking/Collision/Active/CollisionPointContainerTest.cs
@@ -0,0 +1,87 @@
+﻿using VRTK.Core.Tracking.Collision.Active;
+using VRTK.Core.Tracking.Collision;
+
+namespace Test.VRTK.Core.Tracking.Collision.Active
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.VRTK.Core.Utility.Mock;
+    using Test.VRTK.Core.Utility.Helper;
+
+    public class CollisionPointContainerTest
+    {
+        private GameObject containingObject;
+        private CollisionPointContainerMock subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<CollisionPointContainerMock>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void CreateAndClear()
+        {
+            UnityEventListenerMock createdMock = new UnityEventListenerMock();
+            UnityEventListenerMock clearedMock = new UnityEventListenerMock();
+            subject.Created.AddListener(createdMock.Listen);
+            subject.Cleared.AddListener(clearedMock.Listen);
+
+            GameObject publisherObject = new GameObject();
+            ActiveCollisionPublisher publisher = publisherObject.AddComponent<ActiveCollisionPublisher>();
+            publisher.sourceContainer = publisherObject;
+            publisherObject.transform.position = Vector3.zero;
+
+            GameObject collisionNotifierContainer;
+            CollisionNotifier.EventData collisionNotifierEventData = CollisionNotifierHelper.GetEventData(out collisionNotifierContainer);
+            collisionNotifierContainer.transform.position = Vector3.one;
+            collisionNotifierContainer.transform.rotation = Quaternion.Euler(Vector3.forward * 90f);
+
+            ActiveCollisionConsumer.EventData eventData = new ActiveCollisionConsumer.EventData();
+            eventData.Set(publisher, collisionNotifierEventData);
+
+            Assert.IsFalse(createdMock.Received);
+            Assert.IsFalse(clearedMock.Received);
+            Assert.IsNull(subject.Container);
+
+            subject.Create(eventData);
+
+            Assert.IsTrue(createdMock.Received);
+            Assert.IsFalse(clearedMock.Received);
+            Assert.IsNotNull(subject.Container);
+
+            Assert.AreEqual(collisionNotifierContainer.transform.position, subject.Container.transform.position);
+            Assert.AreEqual(collisionNotifierContainer.transform.rotation, subject.Container.transform.rotation);
+            Assert.AreEqual(Vector3.one, subject.Container.transform.localScale);
+
+            createdMock.Reset();
+            clearedMock.Reset();
+
+            subject.Clear();
+
+            Assert.IsFalse(createdMock.Received);
+            Assert.IsTrue(clearedMock.Received);
+            Assert.IsNull(subject.Container);
+
+            Object.DestroyImmediate(publisherObject);
+            Object.DestroyImmediate(collisionNotifierContainer);
+        }
+    }
+
+    public class CollisionPointContainerMock : CollisionPointContainer
+    {
+        protected override void DestroyContainer()
+        {
+            DestroyImmediate(Container);
+            Container = null;
+        }
+    }
+}
