d2f6774a2e991388b693059a89635e92a20e54c3
Christopher - Marcel Böddecker <bddckr@icloud.com>
2018-07-16 09:54:47 +0200

    feat(Association): extract common functionality for reusability
    The existing `XrDeviceRelationActivator` introduced a concept that
    is useful in more scenarios: Some data defines whether specified
    game objects should be turned on or off. This change moves the
    activation logic into a new type and introduces a new base type
    describing any association. The existing association for the loaded
    XR device is changed to no longer just take a string, instead it
    allows using a regular expression to match the loaded XR device by
    name.

---
+++ Scripts/Association/LoadedXrDeviceAssociation.cs
@@ -0,0 +1,24 @@
+﻿namespace VRTK.Core.Association
+{
+    using UnityEngine;
+    using UnityEngine.XR;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Holds <see cref="GameObject"/>s to (de)activate based on the loaded XR device's name.
+    /// </summary>
+    public class LoadedXrDeviceAssociation : BaseGameObjectsAssociation
+    {
+        /// <summary>
+        /// A regular expression to match the name of the XR device that needs to be loaded.
+        /// </summary>
+        [Tooltip("A regular expression to match the name of the XR device that needs to be loaded.")]
+        public string xrDeviceNamePattern;
+
+        /// <inheritdoc/>
+        public override bool ShouldBeActive()
+        {
+            return Regex.IsMatch(XRSettings.loadedDeviceName, xrDeviceNamePattern);
+        }
+    }
+}
