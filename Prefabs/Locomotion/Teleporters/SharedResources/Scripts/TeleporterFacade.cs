0724a1437fd4c76e858105833ad29e25ee180e4b
Harvey Ball <me@harveyball.com>
2018-07-22 23:19:07 +0100

    chore(Locomotion): rename teleporter prefab setup script
    The setup script applied to the Teleporter prefab has been renamed to
    TeleporterSettings to keep consistent with all user setting.

---
+++ Prefabs/Locomotion/Teleporters/SharedResources/Scripts/TeleporterFacade.cs
@@ -0,0 +1,58 @@
+﻿namespace VRTK.Core.Prefabs.Locomotion.Teleporters
+{
+    using UnityEngine;
+    using VRTK.Core.Data.Type;
+    using VRTK.Core.Tracking.Modification;
+
+    /// <summary>
+    /// The public interface into the Teleporter Prefab.
+    /// </summary>
+    public class TeleporterFacade : MonoBehaviour
+    {
+        [Header("Teleporter Settings")]
+
+        /// <summary>
+        /// The alias for the CameraRig Play Area.
+        /// </summary>
+        [Tooltip("The alias for the CameraRig Play Area.")]
+        public GameObject playAreaAlias;
+        /// <summary>
+        /// The alias for the CameraRig Headset.
+        /// </summary>
+        [Tooltip("The alias for the CameraRig Headset.")]
+        public GameObject headsetAlias;
+        /// <summary>
+        /// The <see cref="CameraList"/> of scene <see cref="Camera"/>s to apply a fade to.
+        /// </summary>
+        [Tooltip("The list of scene Cameras to apply a fade to.")]
+        public CameraList sceneCameras;
+
+        [Header("Teleporter Events")]
+
+        /// <summary>
+        /// Emitted when the teleporting is about to initiate.
+        /// </summary>
+        public TransformModifier.UnityEvent Teleporting = new TransformModifier.UnityEvent();
+        /// <summary>
+        /// Emitted when the teleporting has completed.
+        /// </summary>
+        public TransformModifier.UnityEvent Teleported = new TransformModifier.UnityEvent();
+
+        [Header("Internal Settings")]
+
+        /// <summary>
+        /// **DO NOT CHANGE** - The linked Internal Setup.
+        /// </summary>
+        [Tooltip("**DO NOT CHANGE** - The linked Internal Setup.")]
+        public TeleporterInternalSetup internalSetup;
+
+        /// <summary>
+        /// Attempts to teleport the <see cref="playAreaAlias"/>.
+        /// </summary>
+        /// <param name="destination">The location to attempt to teleport to.</param>
+        public virtual void Teleport(TransformData destination)
+        {
+            internalSetup?.Teleport(destination);
+        }
+    }
+}
--- Prefabs/Locomotion/Teleporters/SharedResources/Scripts/TeleporterInternalSetup.cs
+++ Prefabs/Locomotion/Teleporters/SharedResources/Scripts/TeleporterInternalSetup.cs
@@ -9,15 +9,20 @@
     using VRTK.Core.Extension;
 
     /// <summary>
-    /// Provides internal settings for the teleporter prefabs.
+    /// Sets up the Teleport Prefab based on the provided user settings.
     /// </summary>
     public class TeleporterInternalSetup : MonoBehaviour
     {
+        [Header("Facade Settings")]
+
         /// <summary>
-        /// The <see cref="TeleporterUserSetup"/> for providing required alias information.
+        /// The public interface facade.
         /// </summary>
-        [Tooltip("The Teleporter User Setup for providing required alias information.")]
-        public TeleporterUserSetup userSetup;
+        [Tooltip("The public interface facade.")]
+        public TeleporterFacade facade;
+
+        [Header("Teleporter Settings")]
+
         /// <summary>
         /// The <see cref="SurfaceLocator"/> to use for the teleporting event.
         /// </summary>
@@ -28,6 +33,9 @@
         /// </summary>
         [Tooltip("The Transform Modifier to use for the teleporting event.")]
         public TransformModifier modifyTeleporter;
+
+        [Header("Alias Settings")]
+
         /// <summary>
         /// The <see cref="SurfaceLocator"/> to set aliases on.
         /// </summary>
@@ -45,7 +53,33 @@
         public List<CameraColorOverlay> cameraColorOverlays = new List<CameraColorOverlay>();
 
         /// <summary>
-        /// Attempts to teleport the <see cref="userSetup.playAreaAlias"/>.
+        /// Sets up the Teleporter prefab with the specified settings.
+        /// </summary>
+        public virtual void Setup()
+        {
+            if (InvalidParameters())
+            {
+                return;
+            }
+
+            ApplySettings(facade.playAreaAlias, facade.headsetAlias, facade.sceneCameras);
+        }
+
+        /// <summary>
+        /// Clears all of the settings from the Teleporter prefab.
+        /// </summary>
+        public virtual void Clear()
+        {
+            if (InvalidParameters())
+            {
+                return;
+            }
+
+            ApplySettings(null, null, null);
+        }
+
+        /// <summary>
+        /// Attempts to teleport the <see cref="playAreaAlias"/>.
         /// </summary>
         /// <param name="destination">The location to attempt to teleport to.</param>
         public virtual void Teleport(TransformData destination)
@@ -62,23 +96,66 @@
             }
         }
 
-        protected virtual void OnEnable()
+        /// <summary>
+        /// Notifies that the teleporter is about to initiate.
+        /// </summary>
+        /// <param name="data">The location data.</param>
+        public virtual void NotifyTeleporting(TransformModifier.EventData data)
+        {
+            facade?.Teleporting.Invoke(data);
+        }
+
+        /// <summary>
+        /// Notifies that the teleporter has completed.
+        /// </summary>
+        /// <param name="data">The location data.</param>
+        public virtual void NotifyTeleported(TransformModifier.EventData data)
+        {
+            facade?.Teleported.Invoke(data);
+        }
+
+        /// <summary>
+        /// Applies the provided settings to the Teleporter prefab.
+        /// </summary>
+        /// <param name="playAreaAlias">The PlayArea alias.</param>
+        /// <param name="headsetAlias">The Headset alias.</param>
+        /// <param name="sceneCameras">The scene cameras.</param>
+        protected virtual void ApplySettings(GameObject playAreaAlias, GameObject headsetAlias, CameraList sceneCameras)
         {
             foreach (SurfaceLocator currentLocator in surfaceLocatorAliases.EmptyIfNull())
             {
-                currentLocator.searchOrigin = userSetup.headsetAlias;
+                currentLocator.searchOrigin = headsetAlias;
             }
 
             foreach (TransformModifier currentModifier in transformModifierAliases.EmptyIfNull())
             {
-                currentModifier.target = userSetup.playAreaAlias;
-                currentModifier.offset = userSetup.headsetAlias;
+                currentModifier.target = playAreaAlias;
+                currentModifier.offset = headsetAlias;
             }
 
             foreach (CameraColorOverlay currentOverlay in cameraColorOverlays.EmptyIfNull())
             {
-                currentOverlay.validCameras = userSetup.sceneCameras;
+                currentOverlay.validCameras = sceneCameras;
             }
+        }
+
+        protected virtual void OnEnable()
+        {
+            Setup();
+        }
+
+        protected virtual void OnDisable()
+        {
+            Clear();
+        }
+
+        /// <summary>
+        /// Determines if the setup parameters are invalid.
+        /// </summary>
+        /// <returns><see langword="true"/> if the parameters are invalid.</returns>
+        protected virtual bool InvalidParameters()
+        {
+            return (facade == null || facade.playAreaAlias == null || facade.headsetAlias == null || facade.sceneCameras == null);
         }
     }
 }
