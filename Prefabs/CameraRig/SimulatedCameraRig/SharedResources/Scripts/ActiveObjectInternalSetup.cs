5f6eb56acd52f6415d1ee3b835d03fc21861ef7f
Harvey Ball <me@harveyball.com>
2018-08-08 19:30:48 +0100

    feat(CameraRig): add simulated camera rig
    The SimulatedCameraRig prefab provides a mock CameraRig that can
    be controlled via traditional input methods and simulates a HMD and
    two controllers.

---
+++ Prefabs/CameraRig/SimulatedCameraRig/SharedResources/Scripts/ActiveObjectInternalSetup.cs
@@ -0,0 +1,63 @@
+﻿namespace VRTK.Core.Prefabs.CameraRig.SimulatedCameraRig
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    using VRTK.Core.Data.Type.Transformation;
+    using VRTK.Core.Prefabs.CameraRig.SimulatedCameraRig.Input;
+
+    /// <summary>
+    /// Sets up the active controllable objects.
+    /// </summary>
+    public class ActiveObjectInternalSetup : MonoBehaviour
+    {
+        /// <summary>
+        /// The speed to move the free mouse cursor.
+        /// </summary>
+        [Tooltip("The speed to move the free mouse cursor.")]
+        public float freeCursorRotationSpeed = 0.2f;
+        /// <summary>
+        /// The speed to move the locked mouse cursor.
+        /// </summary>
+        [Tooltip("The speed to move the locked mouse cursor.")]
+        public float lockedCursorRotationSpeed = 3f;
+        /// <summary>
+        /// The speed to move the object at.
+        /// </summary>
+        [Tooltip("The speed to move the object at.")]
+        public float movementSpeed = 0.025f;
+        /// <summary>
+        /// The mouse input action to update.
+        /// </summary>
+        [Tooltip("The mouse input action to update.")]
+        public MouseVector2DAction mouseInput;
+        /// <summary>
+        /// A collection of multipliers that deal with positive floats.
+        /// </summary>
+        [Tooltip("A collection of multipliers that deal with positive floats.")]
+        public List<FloatMultiplier> positiveMultipliers = new List<FloatMultiplier>();
+        /// <summary>
+        /// A collection of multipliers that deal with negative floats.
+        /// </summary>
+        [Tooltip("A collection of multipliers that deal with negative floats.")]
+        public List<FloatMultiplier> negativeMultipliers = new List<FloatMultiplier>();
+
+        protected virtual void OnEnable()
+        {
+            if (mouseInput != null)
+            {
+                mouseInput.cursorMultiplier = freeCursorRotationSpeed;
+                mouseInput.lockedCursorMultiplier = lockedCursorRotationSpeed;
+            }
+
+            foreach (FloatMultiplier positiveMultiplier in positiveMultipliers)
+            {
+                positiveMultiplier.SetMultiplier(movementSpeed);
+            }
+
+            foreach (FloatMultiplier negativeMultiplier in negativeMultipliers)
+            {
+                negativeMultiplier.SetMultiplier(-movementSpeed);
+            }
+        }
+    }
+}
---
+++ Prefabs/CameraRig/SimulatedCameraRig/SharedResources/Scripts/KeyBindingDisplay.cs
@@ -0,0 +1,73 @@
+﻿namespace VRTK.Core.Prefabs.CameraRig.SimulatedCameraRig
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using VRTK.Core.Prefabs.CameraRig.UnityXRCameraRig.Input;
+
+    /// <summary>
+    /// Sets up the key binding display.
+    /// </summary>
+    public class KeyBindingDisplay : MonoBehaviour
+    {
+        public Text keyBindingText;
+        public UnityButtonAction forward;
+        public UnityButtonAction backward;
+        public UnityButtonAction strafeLeft;
+        public UnityButtonAction strafeRight;
+        public UnityButtonAction button1;
+        public UnityButtonAction button2;
+        public UnityButtonAction button3;
+        public UnityButtonAction switchToPlayer;
+        public UnityButtonAction switchToLeftController;
+        public UnityButtonAction switchToRightController;
+        public UnityButtonAction resetPlayer;
+        public UnityButtonAction resetControllers;
+        public UnityButtonAction toggleInstructions;
+        public UnityButtonAction lockCursorToggle;
+
+        protected string instructions = @"<b>Simulator Key Bindings</b>
+
+<b>Movement:</b>
+Forward: {0}
+Backward: {1}
+Strafe Left: {2}
+Strafe Right: {3}
+
+<b>Buttons</b>
+Button 1: {4}
+Button 2: {5}
+Button 3: {6}
+
+<b>Object Control</b>
+Move PlayArea: {7}
+Move Left Controller: {8}
+Move Right Controller: {9}
+Reset Player: Position {10}
+Reset Controller Position: {11}
+
+<b>Misc</b>
+Toggle Help Window: {12}
+Lock Mouse Cursor: {13}";
+
+        protected virtual void OnEnable()
+        {
+            keyBindingText.text = string.Format(
+                instructions,
+                forward.keyCode,
+                backward.keyCode,
+                strafeLeft.keyCode,
+                strafeRight.keyCode,
+                button1.keyCode,
+                button2.keyCode,
+                button3.keyCode,
+                switchToPlayer.keyCode,
+                switchToLeftController.keyCode,
+                switchToRightController.keyCode,
+                resetPlayer.keyCode,
+                resetControllers.keyCode,
+                toggleInstructions.keyCode,
+                lockCursorToggle.keyCode
+                );
+        }
+    }
+}
