6b5629036056061630f4e2ab6f90df97a3b096f8
Harvey Ball <me@harveyball.com>
2018-08-08 16:18:27 +0100

    feat(Mutation): mutate properties on a transform
    The PositionProperty and EularRotationProperty mutators allow the
    relevant property on a transform to be changed by either setting
    it to a new specific value or by incrementing the existing value
    by a given input value.

---
+++ Scripts/Mutation/TransformProperty/PositionProperty.cs
@@ -0,0 +1,78 @@
+﻿namespace VRTK.Core.Mutation.TransformProperty
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Mutates the position of a transform with an optional rotation offset.
+    /// </summary>
+    public class PositionProperty : BaseProperty
+    {
+        /// <summary>
+        /// An optional rotation offset.
+        /// </summary>
+        [Tooltip("An optional rotation offset.")]
+        public GameObject rotationOffset;
+
+        /// <summary>
+        /// Sets the rotation offset.
+        /// </summary>
+        /// <param name="rotationOffset">The new rotation offset.</param>
+        public virtual void SetRotationOffset(GameObject rotationOffset)
+        {
+            this.rotationOffset = rotationOffset;
+        }
+
+        /// <summary>
+        /// Clears the existing rotation offset.
+        /// </summary>
+        public virtual void ClearRotationOffset()
+        {
+            rotationOffset = null;
+        }
+
+        /// <inheritdoc/>
+        protected override float GetGlobalAxisValue(int axis)
+        {
+            return target.transform.position[axis];
+        }
+
+        /// <inheritdoc/>
+        protected override float GetLocalAxisValue(int axis)
+        {
+            return target.transform.localPosition[axis];
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 IncrementGlobal(Vector3 input)
+        {
+            return target.transform.position += LockIncrementInput(GetRotationOffset() * input);
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 IncrementLocal(Vector3 input)
+        {
+            return target.transform.localPosition += LockIncrementInput(GetRotationOffset() * input);
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 SetGlobal(Vector3 input)
+        {
+            return target.transform.position = LockSetInput(GetRotationOffset() * input);
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 SetLocal(Vector3 input)
+        {
+            return target.transform.localPosition = LockSetInput(GetRotationOffset() * input);
+        }
+
+        /// <summary>
+        /// Determines the value to use for the rotation offset.
+        /// </summary>
+        /// <returns>The rotation offset.</returns>
+        protected virtual Quaternion GetRotationOffset()
+        {
+            return (rotationOffset == null ? Quaternion.identity : (useLocalValues ? rotationOffset.transform.localRotation : rotationOffset.transform.rotation));
+        }
+    }
+}
---
+++ Tests/Editor/Mutation/TransformProperty/EulerRotationPropertyTest.cs
@@ -0,0 +1,244 @@
+﻿using VRTK.Core.Mutation.TransformProperty;
+using VRTK.Core.Data.Type;
+
+namespace Test.VRTK.Core.Mutation.TransformProperty
+{
+    using UnityEngine;
+    using NUnit.Framework;
+
+    public class EulerRotationPropertyTest
+    {
+        private GameObject containingObject;
+        private EulerRotationProperty subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<EulerRotationProperty>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void SetPropertyLocal()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = true;
+            subject.lockAxis = Vector3State.False;
+
+            Assert.AreEqual(Vector3.zero, target.transform.localEulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            subject.SetProperty(input);
+
+            Assert.AreEqual(input.ToString(), target.transform.localEulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void SetPropertyGlobal()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = false;
+            subject.lockAxis = Vector3State.False;
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            subject.SetProperty(input);
+
+            Assert.AreEqual(input.ToString(), target.transform.eulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void SetPropertyLocalLockYAxis()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = true;
+            subject.lockAxis = new Vector3State(false, true, false);
+
+            Assert.AreEqual(Vector3.zero, target.transform.localEulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            Vector3 expected = new Vector3(10f, 0f, 30f);
+
+            subject.SetProperty(input);
+
+            Assert.AreEqual(expected.ToString(), target.transform.localEulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void SetPropertyGlobalLockXZAxis()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = false;
+            subject.lockAxis = new Vector3State(true, false, true);
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            Vector3 expected = new Vector3(0f, 20f, 0f);
+
+            subject.SetProperty(input);
+
+            Assert.AreEqual(expected.ToString(), target.transform.eulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void IncrementPropertyLocal()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = true;
+            subject.lockAxis = Vector3State.False;
+
+            Assert.AreEqual(Vector3.zero, target.transform.localEulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual(input.ToString(), target.transform.localEulerAngles.ToString());
+
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual((input * 2f).ToString(), target.transform.localEulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void IncrementPropertyGlobal()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = false;
+            subject.lockAxis = Vector3State.False;
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual(input.ToString(), target.transform.eulerAngles.ToString());
+
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual((input * 2f).ToString(), target.transform.eulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void IncrementPropertyLocalLockYAxis()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = true;
+            subject.lockAxis = new Vector3State(false, true, false);
+
+            Assert.AreEqual(Vector3.zero, target.transform.localEulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            Vector3 expected = new Vector3(10f, 0f, 30f);
+
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual(expected.ToString(), target.transform.localEulerAngles.ToString());
+
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual((expected * 2f).ToString(), target.transform.localEulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void IncrementPropertyGlobalLockXZAxis()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = false;
+            subject.lockAxis = new Vector3State(true, false, true);
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            Vector3 expected = new Vector3(0f, 20f, 0f);
+
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual(expected.ToString(), target.transform.eulerAngles.ToString());
+
+            subject.IncrementProperty(input);
+
+            Assert.AreEqual((expected * 2f).ToString(), target.transform.eulerAngles.ToString());
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void SetPropertyInactiveGameObject()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = true;
+            subject.lockAxis = Vector3State.False;
+            subject.gameObject.SetActive(false);
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            subject.SetProperty(input);
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void SetPropertyInactiveComponent()
+        {
+            GameObject target = new GameObject();
+
+            subject.target = target;
+            subject.useLocalValues = true;
+            subject.lockAxis = Vector3State.False;
+            subject.enabled = false;
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Vector3 input = new Vector3(10f, 20f, 30f);
+            subject.SetProperty(input);
+
+            Assert.AreEqual(Vector3.zero, target.transform.eulerAngles);
+
+            Object.DestroyImmediate(target);
+        }
+    }
+}
