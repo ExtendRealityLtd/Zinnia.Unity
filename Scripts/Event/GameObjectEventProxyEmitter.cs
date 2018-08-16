da0fdfff89633e4ced332c51c5a539e926417bfe
Harvey Ball <me@harveyball.com>
2018-08-16 14:28:44 +0100

    feat(Event): add rule to GameObjectEventProxyEmitter
    The GameObjectEventProxyEmitter now has a validity rule that will be
    checked to see if the GameObject received is valid before re-emitting
    it.
    
    The structure of the EventProxyEmitters has been refactored so they
    all extend a common base class.
    
    A new ActiveCollisionConsumerEventProxyEmitter has been added as well
    to proxy the EventData from the ActiveCollisionConsumer.
    
    Tests have also been added for all of these EventProxyEmitters.

--- Scripts/Event/GameObjectEventProxyEmitter.cs
+++ Scripts/Event/GameObjectEventProxyEmitter.cs
@@ -3,18 +3,32 @@
     using UnityEngine;
     using UnityEngine.Events;
     using System;
+    using VRTK.Core.Rule;
+    using VRTK.Core.Extension;
 
     /// <summary>
     /// Emits a UnityEvent with a GameObject payload whenever the Receive method is called.
     /// </summary>
-    public class GameObjectEventProxyEmitter : SingleEventProxyEmitter<GameObjectEventProxyEmitter, GameObject, GameObjectEventProxyEmitter.UnityEvent>
+    public class GameObjectEventProxyEmitter : SingleEventProxyEmitter<GameObject, GameObjectEventProxyEmitter.UnityEvent>
     {
+        /// <summary>
+        /// Determines whether the received <see cref="GameObject"/> is valid to be re-emitted.
+        /// </summary>
+        [Tooltip("Determines whether the received GameObject is valid to be re-emitted.")]
+        public RuleContainer receiveValidity;
+
         /// <summary>
         /// Defines the event with the specified state.
         /// </summary>
         [Serializable]
         public class UnityEvent : UnityEvent<GameObject>
         {
+        }
+
+        /// <inheritdoc />
+        protected override bool IsValid()
+        {
+            return (base.IsValid() && receiveValidity.Accepts(Payload));
         }
     }
 }
---
+++ Tests/Editor/Event/EmptyEventProxyEmitterTest.cs
@@ -0,0 +1,63 @@
+﻿using VRTK.Core.Event;
+
+namespace Test.VRTK.Core.Event
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.VRTK.Core.Utility.Mock;
+
+    public class EmptyEventProxyEmitterTest
+    {
+        private GameObject containingObject;
+        private EmptyEventProxyEmitter subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<EmptyEventProxyEmitter>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Receive()
+        {
+            UnityEventListenerMock emittedMock = new UnityEventListenerMock();
+            subject.Emitted.AddListener(emittedMock.Listen);
+
+            Assert.IsFalse(emittedMock.Received);
+            subject.Receive();
+            Assert.IsTrue(emittedMock.Received);
+        }
+
+        [Test]
+        public void ReceiveInactiveGameObject()
+        {
+            UnityEventListenerMock emittedMock = new UnityEventListenerMock();
+            subject.Emitted.AddListener(emittedMock.Listen);
+            subject.gameObject.SetActive(false);
+
+            Assert.IsFalse(emittedMock.Received);
+            subject.Receive();
+            Assert.IsFalse(emittedMock.Received);
+        }
+
+        [Test]
+        public void ReceiveInactiveComponent()
+        {
+            UnityEventListenerMock emittedMock = new UnityEventListenerMock();
+            subject.Emitted.AddListener(emittedMock.Listen);
+            subject.enabled = false;
+
+            Assert.IsFalse(emittedMock.Received);
+            subject.Receive();
+            Assert.IsFalse(emittedMock.Received);
+        }
+    }
+}
