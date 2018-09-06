aebb096b5f5718f6243f657a6b146d23cac85807
Harvey Ball <me@harveyball.com>
2018-09-06 12:21:21 +0100

    refactor(Event): add common base for restrictable event proxy emitters
    A number of the EventProxyEmitters employed their own RuleContainer
    implementation which all basically provided the same functionality of
    checking to see if the Receive was valid.
    
    A new abstract base class of RestrictableSingleEventProxyEmitter has
    been added that wraps up that common functionality and all the relevant
    event proxy emitters that require it now extend that class.

---
+++ Scripts/Event/RestrictableSingleEventProxyEmitter.cs
@@ -0,0 +1,28 @@
+﻿namespace VRTK.Core.Event
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using VRTK.Core.Extension;
+    using VRTK.Core.Rule;
+
+    public abstract class RestrictableSingleEventProxyEmitter<TValue, TEvent> : SingleEventProxyEmitter<TValue, TEvent> where TEvent : UnityEvent<TValue>, new()
+    {
+        /// <summary>
+        /// Determines whether the received payload is valid to be re-emitted.
+        /// </summary>
+        [Tooltip("Determines whether the received payload is valid to be re-emitted.")]
+        public RuleContainer receiveValidity;
+
+        /// <summary>
+        /// Gets the target for the validity check.
+        /// </summary>
+        /// <returns>The target to check on.</returns>
+        protected abstract object GetTargetToCheck();
+
+        /// <inheritdoc />
+        protected override bool IsValid()
+        {
+            return (base.IsValid() && receiveValidity.Accepts(GetTargetToCheck()));
+        }
+    }
+}
