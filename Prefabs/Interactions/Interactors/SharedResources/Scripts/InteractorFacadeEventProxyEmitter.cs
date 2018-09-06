35ce14a532748adc62f6f075d8263176cb6addfc
Harvey Ball <me@harveyball.com>
2018-09-06 12:44:36 +0100

    feat(Interactions): add prefab to modify visibility of an interactor
    The InteractorVisibilityModifier prefab allows the showing or hiding
    of an Interactor by turning on or off it's underlying Renderer.

---
+++ Prefabs/Interactions/Interactors/SharedResources/Scripts/InteractorFacadeEventProxyEmitter.cs
@@ -0,0 +1,26 @@
+﻿namespace VRTK.Core.Prefabs.Interactions.Interactors
+{
+    using UnityEngine.Events;
+    using System;
+    using VRTK.Core.Event;
+
+    /// <summary>
+    /// Emits a UnityEvent with an <see cref="InteractorFacade"/> payload whenever the Receive method is called.
+    /// </summary>
+    public class InteractorFacadeEventProxyEmitter : RestrictableSingleEventProxyEmitter<InteractorFacade, InteractorFacadeEventProxyEmitter.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the specified state.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<InteractorFacade>
+        {
+        }
+
+        /// <inheritdoc />
+        protected override object GetTargetToCheck()
+        {
+            return Payload.gameObject;
+        }
+    }
+}
