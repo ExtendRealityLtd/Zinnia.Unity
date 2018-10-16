5da0b011501b4514e12c4641eaf013ce01a3eb29
Christopher-Marcel Böddecker <bddckr@icloud.com>
2018-10-16 07:04:43 +0200

    feat(Transformation): add a Vector3 multiplier
    A new type has been added that allows multiplying two Vector3s. This
    prevents having to write a component and instead allows to do this
    calculation by wiring components up to each other right in the
    Editor.

---
+++ Scripts/Data/Type/Transformation/Vector3Multiplier.cs
@@ -0,0 +1,75 @@
+﻿namespace VRTK.Core.Data.Type.Transformation
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+
+    /// <summary>
+    /// Transforms a <see cref="Vector3"/> by multiplying each coordinate by a given multiplier.
+    /// </summary>
+    /// <example>
+    /// (2f,3f,4f) * [3f,4f,5f] = (6f,12f,20f)
+    /// </example>
+    public class Vector3Multiplier : BaseTransformer<Vector3, Vector3, Vector3Multiplier.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the multiplied <see cref="Vector3"/> value.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3>
+        {
+        }
+
+        /// <summary>
+        /// The value to multiply the input by.
+        /// </summary>
+        [Tooltip("The value to multiply the input by."), SerializeField]
+        protected Vector3 multiplier = Vector3.one;
+
+        /// <summary>
+        /// Sets the value to multiply the input by.
+        /// </summary>
+        /// <param name="multiplier">The new multiplier value.</param>
+        public virtual void SetMultiplier(Vector3 multiplier)
+        {
+            this.multiplier = multiplier;
+        }
+
+        /// <summary>
+        /// Sets the x value to multiply the input x by.
+        /// </summary>
+        /// <param name="xMultiplier">The new x multiplier value.</param>
+        public virtual void SetXMultiplier(float xMultiplier)
+        {
+            multiplier.x = xMultiplier;
+        }
+
+        /// <summary>
+        /// Sets the y value to multiply the input y by.
+        /// </summary>
+        /// <param name="yMultiplier">The new y multiplier value.</param>
+        public virtual void SetYMultiplier(float yMultiplier)
+        {
+            multiplier.y = yMultiplier;
+        }
+
+        /// <summary>
+        /// Sets the z value to multiply the input z by.
+        /// </summary>
+        /// <param name="zMultiplier">The new z multiplier value.</param>
+        public virtual void SetZMultiplier(float zMultiplier)
+        {
+            multiplier.z = zMultiplier;
+        }
+
+        /// <summary>
+        /// Multiplies the input by the multipliers.
+        /// </summary>
+        /// <param name="input">The value to transform.</param>
+        /// <returns>The transformed value.</returns>
+        protected override Vector3 Process(Vector3 input)
+        {
+            return Vector3.Scale(input, multiplier);
+        }
+    }
+}
---
+++ Tests/Editor/Data/Type/Transformation/Vector3MultiplierTest.cs
@@ -0,0 +1,91 @@
+﻿using VRTK.Core.Data.Type.Transformation;
+
+namespace Test.VRTK.Core.Data.Type.Transformation
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.VRTK.Core.Utility.Mock;
+
+    public class Vector3MultiplierTest
+    {
+        private GameObject containingObject;
+        private Vector3Multiplier subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<Vector3Multiplier>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Transform()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetXMultiplier(3f);
+            subject.SetYMultiplier(4f);
+            subject.SetZMultiplier(5f);
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 4f);
+            Vector3 result = subject.Transform(input);
+            Vector3 expectedResult = new Vector3(6f, 12f, 20f);
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveGameObject()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetXMultiplier(3f);
+            subject.SetYMultiplier(4f);
+            subject.SetZMultiplier(5f);
+            subject.gameObject.SetActive(false);
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 4f);
+            Vector3 result = subject.Transform(input);
+
+            Assert.AreEqual(Vector3.zero, result);
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveComponent()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetXMultiplier(3f);
+            subject.SetYMultiplier(4f);
+            subject.SetZMultiplier(5f);
+            subject.enabled = false;
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 4f);
+            Vector3 result = subject.Transform(input);
+
+            Assert.AreEqual(Vector3.zero, result);
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+    }
+}
