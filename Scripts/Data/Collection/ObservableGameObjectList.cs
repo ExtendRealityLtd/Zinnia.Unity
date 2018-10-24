4f298776e2c514f5eff909b93a6c6df94e6fb8d6
Christopher-Marcel Böddecker <bddckr@icloud.com>
2018-10-24 12:14:16 +0200

    feat(Collection): add observable list for game objects
    A new type has been added that forms the basis for observable lists.
    An observable list is a list that is a Unity component and thus
    shows up in the Editor. The various events on it allow reacting to
    elements getting added to or removed from the collection. One
    subclass of the base type has been added to allow observing a list
    of GameObjects.

---
+++ Scripts/Data/Collection/ObservableGameObjectList.cs
@@ -0,0 +1,20 @@
+﻿namespace VRTK.Core.Data.Collection
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    /// <summary>
+    /// Allows observing changes to a <see cref="List{T}"/> of <see cref="GameObject"/>s.
+    /// </summary>
+    public class ObservableGameObjectList : ObservableListBase<GameObject, ObservableGameObjectList.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the <see cref="GameObject"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<GameObject>
+        {
+        }
+    }
+}
