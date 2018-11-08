8065c0a5b2c5c7015b4c2b6a82c0b238bd41a6dd
Harvey Ball <me@harveyball.com>
2018-11-08 19:32:18 +0000

    feat(Haptics): ability to create a haptic pulse on a device
    The haptics collection enables the ability to create haptic
    feedback on supported devices by providing a simple pulse to
    a haptic motor with a given intensity. A TimedHapticProcess allows
    this pulse to be repeated for a given duration with a given pause
    interval between.

---
+++ Scripts/Haptics/HapticProcessor.cs
@@ -0,0 +1,57 @@
+﻿namespace VRTK.Core.Haptics
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    using VRTK.Core.Extension;
+    using System.Linq;
+
+    /// <summary>
+    /// A proxy for managing the first active <see cref="HapticProcess"/> that is provided in the collection.
+    /// </summary>
+    public class HapticProcessor : HapticProcess
+    {
+        /// <summary>
+        /// Process the first active <see cref="HapticProcess"/> found in the collection.
+        /// </summary>
+        [Tooltip("Process the first active HapticProcess found in the collection.")]
+        public List<HapticProcess> hapticProcesses = new List<HapticProcess>();
+
+        /// <summary>
+        /// The current active <see cref="HapticProcess"/> being utilized.
+        /// </summary>
+        public HapticProcess ActiveProcess
+        {
+            get;
+            protected set;
+        }
+
+        /// <inheritdoc />
+        public override bool IsActive()
+        {
+            return (base.IsActive() && ActiveProcess != null && ActiveProcess.IsActive());
+        }
+
+        /// <summary>
+        /// Starts the first active <see cref="HapticProcess"/> found.
+        /// </summary>
+        protected override void DoBegin()
+        {
+            ActiveProcess = hapticProcesses.EmptyIfNull().FirstOrDefault(process => process.IsActive());
+            if (ActiveProcess != null)
+            {
+                ActiveProcess.Begin();
+            }
+        }
+
+        /// <summary>
+        /// Cancels the current <see cref="ActiveProcess"/> from running.
+        /// </summary>
+        protected override void DoCancel()
+        {
+            if (ActiveProcess != null)
+            {
+                ActiveProcess.Cancel();
+            }
+        }
+    }
+}
