f667ede3bacc8be07302ac13d8c0a78276625ede
Christopher-Marcel Böddecker <bddckr@icloud.com>
2018-11-11 12:31:07 +0100

    feat(Mutation): mutate the scale of a transform
    A new `BaseProperty` subclass has been added that allows the scale
    property of a transform to be changed.

---
+++ Scripts/Mutation/TransformProperty/ScaleProperty.cs
@@ -0,0 +1,50 @@
+﻿namespace VRTK.Core.Mutation.TransformProperty
+{
+    using UnityEngine;
+    using VRTK.Core.Extension;
+
+    /// <summary>
+    /// Mutates the scale of a transform.
+    /// </summary>
+    public class ScaleProperty : BaseProperty
+    {
+        /// <inheritdoc/>
+        protected override float GetGlobalAxisValue(int axis)
+        {
+            return target.transform.lossyScale[axis];
+        }
+
+        /// <inheritdoc/>
+        protected override float GetLocalAxisValue(int axis)
+        {
+            return target.transform.localScale[axis];
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 IncrementGlobal(Vector3 input)
+        {
+            Vector3 globalScale = target.transform.lossyScale + input;
+            target.transform.SetGlobalScale(globalScale);
+            return globalScale;
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 IncrementLocal(Vector3 input)
+        {
+            return target.transform.localScale += input;
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 SetGlobal(Vector3 input)
+        {
+            target.transform.SetGlobalScale(input);
+            return input;
+        }
+
+        /// <inheritdoc/>
+        protected override Vector3 SetLocal(Vector3 input)
+        {
+            return target.transform.localScale = input;
+        }
+    }
+}
