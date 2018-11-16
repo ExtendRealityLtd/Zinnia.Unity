36b821451c9665c812733ab440341cc9c88da69a
Harvey Ball <me@harveyball.com>
2018-11-16 15:06:31 +0000

    feat(Transformation): aggregator to subtract a Vector3 collection
    The Vector3Subtractor extends the newly created base class
    CollectionAggregator which allows for performing aggregate functions
    on a typed collection.
    
    Previously, there was a FloatCollectionAggregator that added up float
    values, but this has now become the FloatAdder and also extends the
    CollectionAggregator as the aggregation of the collections is generic
    up until the required aggregate function.

---
+++ Scripts/Data/Type/Transformation/CollectionAggregator.cs
@@ -0,0 +1,105 @@
+﻿namespace VRTK.Core.Data.Type.Transformation
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Aggregates the values in a collection based on a specified operation.
+    /// </summary>
+    /// <typeparam name="TInput">The variable type that will be input into transformation.</typeparam>
+    /// <typeparam name="TOutput">The variable type that will be output from the result of the transformation.</typeparam>
+    /// <typeparam name="TEvent">The <see cref="UnityEvent"/> type the transformation will emit.</typeparam>
+    public abstract class CollectionAggregator<TInput, TOutput, TEvent> : Transformer<TInput, TOutput, TEvent>
+        where TEvent : UnityEvent<TOutput>, new()
+    {
+        /// <summary>
+        /// The collection to aggregate.
+        /// </summary>
+        [Tooltip("The collection to aggregate.")]
+        public List<TInput> collection = new List<TInput>();
+
+        /// <summary>
+        /// The current collection index to add the given value to.
+        /// </summary>
+        public int CurrentIndex
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// Sets the collection index that will store a given input value. The given index will be clamped to the range of valid indexes.
+        /// </summary>
+        /// <param name="index">The index to set the collection value at.</param>
+        public virtual void SetIndex(int index)
+        {
+            index = (index >= 0 ? index : collection.Count + index);
+            CurrentIndex = Mathf.Clamp(index, 0, collection.Count - 1);
+        }
+
+        /// <summary>
+        /// Updates the current collection index value with the given <see cref="TInput"/>.
+        /// </summary>
+        /// <param name="input">The element to update the collection with.</param>
+        public virtual void SetElementAtCurrentIndex(TInput input)
+        {
+            collection[CurrentIndex] = input;
+        }
+
+        /// <summary>
+        /// Updates the element at the given index.
+        /// </summary>
+        /// <param name="index">The index in the collection to update at.</param>
+        /// <param name="input">The element to update the collection with.</param>
+        public virtual void SetElement(int index, TInput input)
+        {
+            SetIndex(index);
+            SetElementAtCurrentIndex(input);
+        }
+
+        /// <summary>
+        /// Processes the given input by adding it to the collection at the given index and sums the collection total.
+        /// </summary>
+        /// <param name="index">The index of the collection to set the value at.</param>
+        /// <param name="input">The value to add to the collection.</param>
+        /// <returns>The summed value of all values in the collection.</returns>
+        public virtual TOutput Transform(int index, TInput input)
+        {
+            SetIndex(index);
+            return Transform(input);
+        }
+
+        /// <summary>
+        /// Processes the given input by adding it to the collection at the given index and sums the collection total.
+        /// </summary>
+        /// <param name="index">The index of the collection to set the value at.</param>
+        /// <param name="input">The value to add to the collection.</param>
+        public virtual void DoTransform(int index, TInput input)
+        {
+            Transform(index, input);
+        }
+
+        /// <summary>
+        /// Process the collection and return the output;
+        /// </summary>
+        /// <returns>The processed collection result.</returns>
+        protected abstract TOutput ProcessCollection();
+
+        /// <summary>
+        /// Processes the given input by adding it to the collection at the saved current index.
+        /// </summary>
+        /// <param name="input">The value to add to the collection.</param>
+        /// <returns>The summed value of all values in the collection.</returns>
+        protected override TOutput Process(TInput input)
+        {
+            if (CurrentIndex < 0 || CurrentIndex >= collection.Count)
+            {
+                return default(TOutput);
+            }
+
+            SetElementAtCurrentIndex(input);
+            return ProcessCollection();
+        }
+    }
+}
---
+++ Tests/Editor/Data/Type/Transformation/FloatAdderTest.cs
@@ -0,0 +1,127 @@
+﻿using VRTK.Core.Data.Type.Transformation;
+
+namespace Test.VRTK.Core.Data.Type.Transformation
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Test.VRTK.Core.Utility.Mock;
+
+    public class FloatAdderTest
+    {
+        private GameObject containingObject;
+        private FloatAdder subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<FloatAdder>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Transform()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.collection = new List<float>(new float[3]);
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.SetElement(0, 1f);
+            subject.SetElement(1, 2f);
+            subject.SetIndex(2);
+            float result = subject.Transform(3f);
+
+            Assert.AreEqual(6f, result);
+            Assert.AreEqual(6f, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformWithIndex()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.collection = new List<float>(new float[3]);
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.SetElement(0, 1f);
+            subject.SetElement(1, 2f);
+            float result = subject.Transform(2, 3f);
+
+            Assert.AreEqual(6f, result);
+            Assert.AreEqual(6f, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformExceedingIndex()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.collection = new List<float>(new float[2]);
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            // adds 1 to index 0 -> adds 2 to index 1 -> attempts to add 3 to index 2 but is out of range so sets it at index 1
+            // collection result is [1f, 3f]
+
+            subject.SetElement(0, 1f);
+            subject.SetElement(1, 2f);
+            float result = subject.Transform(2, 3f);
+
+            Assert.AreEqual(4f, result);
+            Assert.AreEqual(4f, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveGameObject()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.collection = new List<float>(new float[2]);
+            subject.gameObject.SetActive(false);
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.SetElement(0, 1f);
+            float result = subject.Transform(1, 3f);
+
+            Assert.AreEqual(0f, result);
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveComponent()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.collection = new List<float>(new float[2]);
+            subject.enabled = false;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.SetElement(0, 1f);
+            float result = subject.Transform(1, 3f);
+
+            Assert.AreEqual(0f, result);
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+    }
+}
