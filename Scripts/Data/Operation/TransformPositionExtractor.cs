9b315e898c7b551f37de17bec98aa44786d1b5de
Harvey Ball <me@harveyball.com>
2018-11-16 15:10:04 +0000

    feat(Operation): ability to extract the position data from a Transform
    The TransformPositionExtractor extends the TransformPropertyExtractor
    which allows for the extraction of data about a Transform on a given
    GameObject.

---
+++ Scripts/Data/Operation/TransformPositionExtractor.cs
@@ -0,0 +1,16 @@
+﻿namespace VRTK.Core.Data.Operation
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Extracts the <see cref="Transform.position"/>.
+    /// </summary>
+    public class TransformPositionExtractor : TransformVector3PropertyExtractor
+    {
+        /// <inheritdoc />
+        protected override Vector3 ExtractValue()
+        {
+            return (useLocal ? source.transform.localPosition : source.transform.position);
+        }
+    }
+}
