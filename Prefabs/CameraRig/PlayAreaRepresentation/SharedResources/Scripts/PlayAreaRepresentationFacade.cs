3d54cf05684c6cfb399261368b0664924680815f
Harvey Ball <me@harveyball.com>
2018-11-16 16:13:21 +0000

    feat(PlayAreaRepresentation): option to move target based on offset
    The PlayAreaRepresentation is now a nested Prefab with its own Facade
    to control the relevant set up. It also has the ability to set an
    offset origin and offset destination to determine the offset position
    to apply to the PlayAreaRepresentation target. This is useful if
    the representation target is being used in the pointer cursor and it
    is appropriate to show where the cursor is in relation to the playarea.

---
+++ Prefabs/CameraRig/PlayAreaRepresentation/SharedResources/Scripts/PlayAreaRepresentationFacade.cs
@@ -0,0 +1,45 @@
+﻿namespace VRTK.Core.Prefabs.PlayAreaRepresentation
+{
+    using UnityEngine;
+    using VRTK.Core.Data.Attribute;
+
+    /// <summary>
+    /// The public interface into the PlayAreaRepresentation Prefab.
+    /// </summary>
+    public class PlayAreaRepresentationFacade : MonoBehaviour
+    {
+        #region Target Settings
+        /// <summary>
+        /// The target to represent the PlayArea.
+        /// </summary>
+        [Header("Target Settings"), Tooltip("The target to represent the PlayArea.")]
+        public GameObject target;
+        /// <summary>
+        /// An optional origin to use in a position offset calculation.
+        /// </summary>
+        [Tooltip("An optional origin to use in a position offset calculation.")]
+        public GameObject offsetOrigin;
+        /// <summary>
+        /// An optional destination to use in a position offset calculation.
+        /// </summary>
+        [Tooltip("An optional destination to use in a position offset calculation.")]
+        public GameObject offsetDestination;
+        #endregion
+
+        #region Internal Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting]
+        public PlayAreaRepresentationInternalSetup internalSetup;
+        #endregion
+
+        /// <summary>
+        /// Recalculates the PlayArea dimensions.
+        /// </summary>
+        public virtual void Recalculate()
+        {
+            internalSetup.dimensionExtractor.DoExtract();
+        }
+    }
+}
