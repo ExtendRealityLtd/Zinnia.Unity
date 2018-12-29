2e957c8e1b36eee86ace3deff16669b8d163734f
Christopher-Marcel Böddecker <bddckr@icloud.com>
2018-12-29 11:54:50 +0100

    feat(MovementAmplifier): amplify movement of a game object
    The movement of a game object can be amplified by applying the same
    movement to another object. The feature comes with a multiplier
    allows setting the amplification amount as well as a threshold that
    allows ignoring movement in a radius. In VR experiences this feature
    can be used to amplify movement of the headset to allow traveling
    larger distances than the physical play area allows. The threshold
    radius prevents amplifying movement while the user is not moving and
    instead just crouches.

---
+++ Prefabs/Locomotion/Movement/MovementAmplifier/SharedResources/Scripts/MovementAmplifierFacade.cs
@@ -0,0 +1,45 @@
+﻿namespace VRTK.Core.Prefabs.Locomotion.Movement.MovementAmplifier
+{
+    using UnityEngine;
+    using VRTK.Core.Data.Attribute;
+
+    /// <summary>
+    /// The public interface for the MovementAmplifier prefab.
+    /// </summary>
+    public class MovementAmplifierFacade : MonoBehaviour
+    {
+        #region Tracking Settings
+        /// <summary>
+        /// The source to observe movement of.
+        /// </summary>
+        [Header("Tracking Settings"), Tooltip("The source to observe movement of.")]
+        public GameObject source;
+        /// <summary>
+        /// The target to apply amplified movement to.
+        /// </summary>
+        [Tooltip("The target to apply amplified movement to.")]
+        public GameObject target;
+        #endregion
+
+        #region Movement Settings
+        /// <summary>
+        /// The radius in which <see cref="source"/> movement is ignored. Too small values can result in movement amplification happening during crouching which is often unexpected.
+        /// </summary>
+        [Header("Movement Settings"), Tooltip("The radius in which source movement is ignored. Too small values can result in movement amplification happening during crouching which is often unexpected.")]
+        public float ignoredRadius = 0.25f;
+        /// <summary>
+        /// How much to amplify movement of <see cref="source"/> to apply to <see cref="target"/>.
+        /// </summary>
+        [Tooltip("How much to amplify movement of source to apply to target.")]
+        public float multiplier = 2f;
+        #endregion
+
+        #region Internal Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting]
+        public MovementAmplifierInternalSetup internalSetup;
+        #endregion
+    }
+}
---
+++ Tests/Editor/Data/Type/Transformation/Vector3MagnitudeSetterTest.cs
@@ -0,0 +1,69 @@
+﻿using VRTK.Core.Data.Type.Transformation;
+
+namespace Test.VRTK.Core.Data.Type.Transformation
+{
+    using NUnit.Framework;
+    using Test.VRTK.Core.Utility.Mock;
+    using UnityEngine;
+
+    public class Vector3MagnitudeSetterTest
+    {
+        private Vector3MagnitudeSetter subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new GameObject().AddComponent<Vector3MagnitudeSetter>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject.gameObject);
+        }
+
+        [TestCase(0f), TestCase(1f), TestCase(-2f), TestCase(123f), TestCase(float.MaxValue)]
+        public void SetFloatMagnitude(float expected)
+        {
+            subject.magnitude = 2f;
+
+            Assert.AreEqual(2f, subject.magnitude);
+
+            subject.SetMagnitude(expected);
+
+            Assert.AreEqual(expected, subject.magnitude);
+        }
+
+        [Test]
+        public void SetVectorMagnitude()
+        {
+            subject.magnitude = 2f;
+
+            Assert.AreEqual(2f, subject.magnitude);
+
+            Vector3 expected = new Vector3(1f, 2f, 3f);
+            subject.SetMagnitude(expected);
+
+            Assert.AreEqual(expected.magnitude, subject.magnitude);
+        }
+
+        [Test]
+        public void Process()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.magnitude = 2f;
+            Vector3 input = new Vector3(1f, -2f, 5f);
+            Vector3 result = subject.Transform(input);
+            Vector3 expectedResult = new Vector3(0.3651484f, -0.7302967f, 1.825742f);
+
+            Assert.IsTrue(expectedResult == result);
+            Assert.IsTrue(expectedResult == subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+    }
+}
