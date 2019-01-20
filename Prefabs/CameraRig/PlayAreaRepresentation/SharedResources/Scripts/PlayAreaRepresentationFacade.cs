de43c20fc02b316deac719d51b635953e61e42d8
Harvey Ball <me@harveyball.com>
2019-01-20 15:16:16 +0000

    refactor(CameraRig): use correct access modifiers on representation
    The PlayAreaRepresentation prefab has been updated so the relevant
    components use the correct access modifiers for fields and provides
    private backing fields for public properties to allow changing of data
    at runtime.

--- Prefabs/CameraRig/PlayAreaRepresentation/SharedResources/Scripts/PlayAreaRepresentationFacade.cs
+++ Prefabs/CameraRig/PlayAreaRepresentation/SharedResources/Scripts/PlayAreaRepresentationFacade.cs
@@ -9,29 +9,58 @@
     public class PlayAreaRepresentationFacade : MonoBehaviour
     {
         #region Target Settings
+        [Header("Target Settings"), Tooltip("The target to represent the PlayArea."), SerializeField]
+        private GameObject _target;
         /// <summary>
         /// The target to represent the PlayArea.
         /// </summary>
-        [Header("Target Settings"), Tooltip("The target to represent the PlayArea.")]
-        public GameObject target;
+        public GameObject Target
+        {
+            get { return _target; }
+            set
+            {
+                _target = value;
+                internalSetup.ConfigureTarget();
+            }
+        }
+
+        [Tooltip("An optional origin to use in a position offset calculation."), SerializeField]
+        private GameObject _offsetOrigin;
         /// <summary>
         /// An optional origin to use in a position offset calculation.
         /// </summary>
-        [Tooltip("An optional origin to use in a position offset calculation.")]
-        public GameObject offsetOrigin;
+        public GameObject OffsetOrigin
+        {
+            get { return _offsetOrigin; }
+            set
+            {
+                _offsetOrigin = value;
+                internalSetup.ConfigureOffsetOrigin();
+            }
+        }
+
+        [Tooltip("An optional destination to use in a position offset calculation."), SerializeField]
+        private GameObject _offsetDestination;
         /// <summary>
         /// An optional destination to use in a position offset calculation.
         /// </summary>
-        [Tooltip("An optional destination to use in a position offset calculation.")]
-        public GameObject offsetDestination;
+        public GameObject OffsetDestination
+        {
+            get { return _offsetDestination; }
+            set
+            {
+                _offsetDestination = value;
+                internalSetup.ConfigureOffsetDestination();
+            }
+        }
         #endregion
 
         #region Internal Settings
         /// <summary>
         /// The linked Internal Setup.
         /// </summary>
-        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting]
-        public PlayAreaRepresentationInternalSetup internalSetup;
+        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting, SerializeField]
+        protected PlayAreaRepresentationInternalSetup internalSetup;
         #endregion
 
         /// <summary>
@@ -39,7 +68,19 @@
         /// </summary>
         public virtual void Recalculate()
         {
-            internalSetup.dimensionExtractor.DoExtract();
+            internalSetup.RecalculateDimensions();
+        }
+
+        protected virtual void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            internalSetup.ConfigureTarget();
+            internalSetup.ConfigureOffsetOrigin();
+            internalSetup.ConfigureOffsetDestination();
         }
     }
 }
