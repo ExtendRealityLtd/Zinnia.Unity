d34700e8d24cee79fef28f642bd91b15aeecda1b
Harvey Ball <me@harveyball.com>
2019-01-20 19:21:00 +0000

    refactor(Locomotion): use correct access modifiers on MovementAmplifier
    The MovementAmplifier has been updated so the relevant components use
    the correct access modifiers for fields and provides private backing
    fields for public properties to allow changing of data at runtime.

--- Prefabs/Locomotion/Movement/MovementAmplifier/SharedResources/Scripts/MovementAmplifierFacade.cs
+++ Prefabs/Locomotion/Movement/MovementAmplifier/SharedResources/Scripts/MovementAmplifierFacade.cs
@@ -9,37 +9,93 @@
     public class MovementAmplifierFacade : MonoBehaviour
     {
         #region Tracking Settings
+        [Header("Tracking Settings"), Tooltip("The source to observe movement of."), SerializeField]
+        private GameObject _source;
         /// <summary>
         /// The source to observe movement of.
         /// </summary>
-        [Header("Tracking Settings"), Tooltip("The source to observe movement of.")]
-        public GameObject source;
+        public GameObject Source
+        {
+            get { return _source; }
+            set
+            {
+                _source = value;
+                internalSetup.ConfigureRadiusOriginMover();
+                internalSetup.ConfigureDistanceChecker();
+                internalSetup.ConfigureObjectMover();
+            }
+        }
+
+        [Tooltip("The target to apply amplified movement to."), SerializeField]
+        private GameObject _target;
         /// <summary>
         /// The target to apply amplified movement to.
         /// </summary>
-        [Tooltip("The target to apply amplified movement to.")]
-        public GameObject target;
+        public GameObject Target
+        {
+            get { return _target; }
+            set
+            {
+                _target = value;
+                internalSetup.ConfigureTargetPositionMutator();
+            }
+        }
         #endregion
 
         #region Movement Settings
+        [Header("Movement Settings"), Tooltip("The radius in which source movement is ignored. Too small values can result in movement amplification happening during crouching which is often unexpected."), SerializeField]
+        private float _ignoredRadius = 0.25f;
         /// <summary>
         /// The radius in which <see cref="source"/> movement is ignored. Too small values can result in movement amplification happening during crouching which is often unexpected.
         /// </summary>
-        [Header("Movement Settings"), Tooltip("The radius in which source movement is ignored. Too small values can result in movement amplification happening during crouching which is often unexpected.")]
-        public float ignoredRadius = 0.25f;
+        public float IgnoredRadius
+        {
+            get { return _ignoredRadius; }
+            set
+            {
+                _ignoredRadius = value;
+                internalSetup.ConfigureDistanceChecker();
+                internalSetup.ConfigureRadiusSubtractor();
+            }
+        }
+
+        [Tooltip("How much to amplify movement of source to apply to target."), SerializeField]
+        private float _multiplier = 2f;
         /// <summary>
         /// How much to amplify movement of <see cref="source"/> to apply to <see cref="target"/>.
         /// </summary>
-        [Tooltip("How much to amplify movement of source to apply to target.")]
-        public float multiplier = 2f;
+        public float Multiplier
+        {
+            get { return _multiplier; }
+            set
+            {
+                _multiplier = value;
+                internalSetup.ConfigureMovementMultiplier();
+            }
+        }
         #endregion
 
         #region Internal Settings
         /// <summary>
         /// The linked Internal Setup.
         /// </summary>
-        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting]
-        public MovementAmplifierInternalSetup internalSetup;
+        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting, SerializeField]
+        protected MovementAmplifierInternalSetup internalSetup;
         #endregion
+
+        protected virtual void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            internalSetup.ConfigureRadiusOriginMover();
+            internalSetup.ConfigureDistanceChecker();
+            internalSetup.ConfigureObjectMover();
+            internalSetup.ConfigureRadiusSubtractor();
+            internalSetup.ConfigureMovementMultiplier();
+            internalSetup.ConfigureTargetPositionMutator();
+        }
     }
 }
--- Prefabs/Locomotion/Movement/MovementAmplifier/SharedResources/Scripts/MovementAmplifierInternalSetup.cs
+++ Prefabs/Locomotion/Movement/MovementAmplifier/SharedResources/Scripts/MovementAmplifierInternalSetup.cs
@@ -1,9 +1,10 @@
 ﻿namespace VRTK.Core.Prefabs.Locomotion.Movement.MovementAmplifier
 {
     using UnityEngine;
+    using VRTK.Core.Tracking.Follow;
+    using VRTK.Core.Data.Attribute;
     using VRTK.Core.Data.Operation;
     using VRTK.Core.Data.Type.Transformation;
-    using VRTK.Core.Tracking.Follow;
 
     /// <summary>
     /// Sets up the MovementAmplifier prefab based on the provided user settings.
@@ -14,66 +15,109 @@
         /// <summary>
         /// The public interface facade.
         /// </summary>
-        [Header("Facade Settings"), Tooltip("The public interface facade.")]
-        public MovementAmplifierFacade facade;
+        [Header("Facade Settings"), Tooltip("The public interface facade."), InternalSetting, SerializeField]
+        protected MovementAmplifierFacade facade;
         #endregion
 
         #region Reference Settings
         /// <summary>
         /// Moves the radius origin.
         /// </summary>
-        [Header("Reference Settings"), Tooltip("Moves the radius origin.")]
-        public ObjectDistanceComparator radiusOriginMover;
+        [Header("Reference Settings"), Tooltip("Moves the radius origin."), InternalSetting, SerializeField]
+        protected ObjectDistanceComparator radiusOriginMover;
         /// <summary>
         /// Determines whether <see cref="MovementAmplifierFacade.source"/> is inside the radius.
         /// </summary>
-        [Tooltip("Determines whether MovementAmplifierFacade.source is inside the radius.")]
-        public ObjectDistanceComparator distanceChecker;
+        [Tooltip("Determines whether MovementAmplifierFacade.source is inside the radius."), InternalSetting, SerializeField]
+        protected ObjectDistanceComparator distanceChecker;
         /// <summary>
         /// Moves the objects.
         /// </summary>
-        [Tooltip("Moves the objects.")]
-        public ObjectDistanceComparator objectMover;
+        [Tooltip("Moves the objects."), InternalSetting, SerializeField]
+        protected ObjectDistanceComparator objectMover;
         /// <summary>
         /// Subtracts the radius.
         /// </summary>
-        [Tooltip("Subtracts the radius.")]
-        public FloatAdder radiusSubtractor;
+        [Tooltip("Subtracts the radius."), InternalSetting, SerializeField]
+        protected FloatAdder radiusSubtractor;
         /// <summary>
         /// Stabilizes the radius by ensuring <see cref="MovementAmplifierFacade.target"/> moves back into the radius.
         /// </summary>
-        [Tooltip("Stabilizes the radius by ensuring MovementAmplifierFacade.target moves back into the radius.")]
-        public float radiusStabilizer = 0.001f;
+        [Tooltip("Stabilizes the radius by ensuring MovementAmplifierFacade.target moves back into the radius."), InternalSetting, SerializeField]
+        protected float radiusStabilizer = 0.001f;
         /// <summary>
         /// Amplifies the movement.
         /// </summary>
-        [Tooltip("Amplifies the movement.")]
-        public Vector3Multiplier movementMultiplier;
+        [Tooltip("Amplifies the movement."), InternalSetting, SerializeField]
+        protected Vector3Multiplier movementMultiplier;
         /// <summary>
         /// Moves the target.
         /// </summary>
-        [Tooltip("Moves the target.")]
-        public TransformPositionMutator targetPositionMutator;
+        [Tooltip("Moves the target."), InternalSetting, SerializeField]
+        protected TransformPositionMutator targetPositionMutator;
         #endregion
 
-        public virtual void OnEnable()
+        /// <summary>
+        /// Configures the <see cref="radiusOriginMover"/> with the facade settings.
+        /// </summary>
+        public virtual void ConfigureRadiusOriginMover()
         {
-            radiusOriginMover.transform.parent.position = facade.source.transform.position;
-            radiusOriginMover.SetTarget(facade.source);
-
-            distanceChecker.SetSource(facade.source);
-            distanceChecker.distanceThreshold = facade.ignoredRadius;
-
-            objectMover.SetSource(facade.source);
-
-            radiusSubtractor.SetElement(1, -facade.ignoredRadius + radiusStabilizer);
-
-            movementMultiplier.SetElement(1, Vector3.one * (facade.multiplier - 1f));
-
-            targetPositionMutator.SetTarget(facade.target);
+            radiusOriginMover.transform.parent.position = facade.Source.transform.position;
+            radiusOriginMover.SetTarget(facade.Source);
         }
 
-        public virtual void OnDisable()
+        /// <summary>
+        /// Configures the <see cref="distanceChecker"/> with the facade settings.
+        /// </summary>
+        public virtual void ConfigureDistanceChecker()
+        {
+            distanceChecker.SetSource(facade.Source);
+            distanceChecker.distanceThreshold = facade.IgnoredRadius;
+        }
+
+        /// <summary>
+        /// Configures the <see cref="objectMover"/> with the facade settings.
+        /// </summary>
+        public virtual void ConfigureObjectMover()
+        {
+            objectMover.SetSource(facade.Source);
+        }
+
+        /// <summary>
+        /// Configures the <see cref="radiusSubtractor"/> with the facade settings.
+        /// </summary>
+        public virtual void ConfigureRadiusSubtractor()
+        {
+            radiusSubtractor.SetElement(1, -facade.IgnoredRadius + radiusStabilizer);
+        }
+
+        /// <summary>
+        /// Configures the <see cref="movementMultiplier"/> with the facade settings.
+        /// </summary>
+        public virtual void ConfigureMovementMultiplier()
+        {
+            movementMultiplier.SetElement(1, Vector3.one * (facade.Multiplier - 1f));
+        }
+
+        /// <summary>
+        /// Configures the <see cref="targetPositionMutator"/> with the facade settings.
+        /// </summary>
+        public virtual void ConfigureTargetPositionMutator()
+        {
+            targetPositionMutator.SetTarget(facade.Target);
+        }
+
+        protected virtual void OnEnable()
+        {
+            ConfigureRadiusOriginMover();
+            ConfigureDistanceChecker();
+            ConfigureObjectMover();
+            ConfigureRadiusSubtractor();
+            ConfigureMovementMultiplier();
+            ConfigureTargetPositionMutator();
+        }
+
+        protected virtual void OnDisable()
         {
             objectMover.enabled = false;
         }
