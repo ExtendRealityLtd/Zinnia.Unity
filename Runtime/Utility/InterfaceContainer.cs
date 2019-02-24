ec183d5eb35698aa62fbf20966076872c8116cc1
Christopher - Marcel Böddecker <bddckr@icloud.com>
2019-02-24 13:50:09 +0100

    perf(Utility): look up interface on UnityEngine.Object only once
    `InterfaceContainer` allows setting a `UnityEngine.Object` field
    (which is serialized) to a type that implements a specific
    interface. The check whether the set value's type implements the
    interface was done every time the property getter was called. This
    change ensures that lookup is only done when the field actually
    changes.

--- Runtime/Utility/InterfaceContainer.cs
+++ Runtime/Utility/InterfaceContainer.cs
@@ -1,5 +1,9 @@
 ﻿namespace Zinnia.Utility
 {
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.PropertySetterMethod;
+    using Malimbe.PropertyValidationMethod;
+    using Malimbe.XmlDocumentationAttribute;
     using UnityEngine;
 
     /// <summary>
@@ -7,21 +11,35 @@
     /// </summary>
     public abstract class InterfaceContainer
     {
-        [SerializeField]
-        protected Object field;
+        /// <summary>
+        /// The contained object.
+        /// </summary>
+        [Serialized, Validated]
+        [field: DocumentedByXml]
+        protected Object Field { get; set; }
+
+        /// <summary>
+        /// Handles changes to <see cref="Field"/>.
+        /// </summary>
+        /// <param name="previousValue">The previous value.</param>
+        /// <param name="newValue">The new value.</param>
+        [CalledBySetter(nameof(Field))]
+        protected virtual void OnFieldChange(Object previousValue, ref Object newValue)
+        {
+        }
     }
 
     /// <summary>
     /// A container for a given interface type.
     /// </summary>
-    /// <typeparam name="TInterface">The type of container for the interface.</typeparam>
-    public abstract class InterfaceContainer<TInterface> : InterfaceContainer
+    /// <typeparam name="TInterface">The interface type to contain.</typeparam>
+    public abstract class InterfaceContainer<TInterface> : InterfaceContainer, ISerializationCallbackReceiver
     {
         public TInterface Interface
         {
             get
             {
-                return field == null ? _interface : (TInterface)(object)field;
+                return _interface;
             }
             set
             {
@@ -29,15 +47,39 @@
                 if (@object == null)
                 {
                     _interface = value;
-                    field = null;
+                    Field = null;
                 }
                 else
                 {
-                    field = @object;
                     _interface = default;
+                    Field = @object;
                 }
             }
         }
         private TInterface _interface;
+
+        /// <inheritdoc />
+        protected override void OnFieldChange(Object previousValue, ref Object newValue)
+        {
+            if (newValue is TInterface @interface)
+            {
+                _interface = @interface;
+            }
+            else
+            {
+                newValue = null;
+            }
+        }
+
+        /// <inheritdoc />
+        public void OnBeforeSerialize()
+        {
+        }
+
+        /// <inheritdoc />
+        public void OnAfterDeserialize()
+        {
+            Field = Field;
+        }
     }
 }
