7edfc407a3ba328e3bfc74b7689fc17cc268b914
Harvey Ball <me@harveyball.com>
2019-03-07 10:49:50 +0000

    fix(Type): ensure SerializableType updates when assembly name changes
    The internal assemblyQualifiedTypeName field within SerializableField
    can be changed via reflection (and in the TypePickerAttributeDrawer)
    but when the field data is changed, it never actually returned a new
    serialized type when the ActualType property was called because
    the setting of the internal private field wasn't ever checked for
    updates.
    
    This fix now stores the current value of the qualified name and will
    create a new SerializedType if that value has changed. If the value
    hasn't changed then it uses the current cached value to prevent
    needing to do the `Type.GetType(string)` lookup.
    
    A test has been added for the SerializableType as well.

--- Runtime/Data/Type/SerializableType.cs
+++ Runtime/Data/Type/SerializableType.cs
@@ -1,18 +1,25 @@
 ﻿namespace Zinnia.Data.Type
 {
-    using System;
     using UnityEngine;
+    using System;
 
+    /// <summary>
+    /// Specifies a serializable data type.
+    /// </summary>
     [Serializable]
     public class SerializableType
     {
+        /// <summary>
+        /// The actual <see cref="Type"/> of the held serializable type.
+        /// </summary>
         public Type ActualType
         {
             get
             {
-                if (actualType == null && !string.IsNullOrWhiteSpace(assemblyQualifiedTypeName))
+                if (!string.IsNullOrWhiteSpace(assemblyQualifiedTypeName) && !assemblyQualifiedTypeName.Equals(currentAssemblyQualifiedTypeName))
                 {
-                    actualType = Type.GetType(assemblyQualifiedTypeName);
+                    ActualType = Type.GetType(assemblyQualifiedTypeName);
+                    currentAssemblyQualifiedTypeName = assemblyQualifiedTypeName;
                 }
 
                 return actualType;
@@ -25,18 +32,34 @@
         }
         private Type actualType;
 
+        /// <summary>
+        /// The string equivalent of the type name in full assembly qualified format.
+        /// </summary>
         [SerializeField]
         private string assemblyQualifiedTypeName;
+        /// <summary>
+        /// The current set string equivalent of the type name.
+        /// </summary>
+        private string currentAssemblyQualifiedTypeName;
 
+        /// <summary>
+        /// Allows conversion of <see cref="SerializableType"/> to <see cref="Type"/>.
+        /// </summary>
+        /// <param name="serializableType">The item to convert.</param>
         public static implicit operator Type(SerializableType serializableType)
         {
             return serializableType.ActualType;
         }
 
+        /// <summary>
+        /// Allows conversion of <see cref="Type"/> to <see cref="SerializableType"/>.
+        /// </summary>
+        /// <param name="type">The item to convert.</param>
         public static implicit operator SerializableType(Type type)
         {
             return new SerializableType
             {
+                actualType = type,
                 assemblyQualifiedTypeName = type.AssemblyQualifiedName
             };
         }
---
+++ Tests/Editor/Data/Type/SerializableTypeTest.cs
@@ -0,0 +1,54 @@
+﻿using Zinnia.Data.Type;
+
+namespace Test.Zinnia.Data.Type
+{
+    using UnityEngine;
+    using System;
+    using NUnit.Framework;
+
+    public class SerializableTypeTest
+    {
+        [Test]
+        public void ConstructFromType()
+        {
+            Component componentType = new Component();
+            SerializableType subject = componentType.GetType();
+            Assert.AreEqual(componentType.GetType(), subject.ActualType);
+        }
+
+        [Test]
+        public void ConstructFromString()
+        {
+            Component componentType = new Component();
+            SerializableType subject = Type.GetType("UnityEngine.Component, UnityEngine.CoreModule, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+            Assert.AreEqual(componentType.GetType(), subject.ActualType);
+        }
+
+        [Test]
+        public void ChangeAssemblyQualifiedTypeName()
+        {
+            Component componentType = new Component();
+            FloatRange altType = new FloatRange();
+
+            SerializableType subject = componentType.GetType();
+
+            Assert.AreEqual(componentType.GetType(), subject.ActualType);
+
+            var prop = subject.GetType().GetField("assemblyQualifiedTypeName", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            prop.SetValue(subject, altType.GetType().AssemblyQualifiedName);
+
+            Assert.AreEqual(altType.GetType(), subject.ActualType);
+        }
+
+        [Test]
+        public void ConvertToType()
+        {
+            Component componentType = new Component();
+            SerializableType subject = componentType.GetType();
+
+            Type convertedType = subject;
+
+            Assert.AreEqual(componentType.GetType(), convertedType);
+        }
+    }
+}
