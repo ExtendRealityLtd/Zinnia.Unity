972c595c9118eb6bb5c89cf7616138eac12282bb
Harvey Ball <me@harveyball.com>
2019-03-08 09:17:21 +0000

    feat(Query): update fields to properties and rename pointing to facing
    All usage of public fields have now been converted to properties that
    are serialized with Malimbe to create an appropriate backing field to
    display in the Unity Inspector.
    
    The PointingQuery has also been renamed to FacingQuery as the actual
    use of the query is to determine if a GameObject is facing (down it's
    local forward direction) another GameObject. The events have also been
    updated to reflect this change.
    
    Tests have also been added for both Query types.

---
+++ Tests/Editor/Tracking/Query/ObscuranceQueryTest.cs
@@ -0,0 +1,98 @@
+﻿using Zinnia.Tracking.Query;
+
+namespace Test.Zinnia.Tracking.Query
+{
+    using UnityEngine;
+    using UnityEngine.TestTools;
+    using System.Collections;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+
+    public class ObscuranceQueryTest
+    {
+        private GameObject containingObject;
+        private ObscuranceQuery subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<ObscuranceQuery>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [UnityTest]
+        public IEnumerator ObscuredThenUnobscured()
+        {
+            UnityEventListenerMock targetObscuredMock = new UnityEventListenerMock();
+            UnityEventListenerMock targetUnobscuredMock = new UnityEventListenerMock();
+            subject.TargetObscured.AddListener(targetObscuredMock.Listen);
+            subject.TargetUnobscured.AddListener(targetUnobscuredMock.Listen);
+
+            GameObject objectA = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject objectB = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            objectA.transform.position = Vector3.left * 2f;
+            objectB.transform.position = Vector3.right * 2f;
+
+            subject.Source = objectA;
+            subject.Target = objectB.GetComponentInChildren<Collider>();
+
+            Assert.IsFalse(targetObscuredMock.Received);
+            Assert.IsFalse(targetUnobscuredMock.Received);
+
+            targetObscuredMock.Reset();
+            targetUnobscuredMock.Reset();
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsFalse(targetObscuredMock.Received);
+            Assert.IsTrue(targetUnobscuredMock.Received);
+
+            targetObscuredMock.Reset();
+            targetUnobscuredMock.Reset();
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsFalse(targetObscuredMock.Received);
+            Assert.IsFalse(targetUnobscuredMock.Received);
+
+            targetObscuredMock.Reset();
+            targetUnobscuredMock.Reset();
+
+            GameObject obscurer = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsTrue(targetObscuredMock.Received);
+            Assert.IsFalse(targetUnobscuredMock.Received);
+
+            targetObscuredMock.Reset();
+            targetUnobscuredMock.Reset();
+
+            Object.Destroy(obscurer);
+
+            yield return new WaitForEndOfFrame();
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsFalse(targetObscuredMock.Received);
+            Assert.IsTrue(targetUnobscuredMock.Received);
+
+            Object.Destroy(objectA);
+            Object.Destroy(objectB);
+        }
+    }
+}
