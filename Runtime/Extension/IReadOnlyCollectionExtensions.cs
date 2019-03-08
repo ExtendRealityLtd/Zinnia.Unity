b476d0bcdc8a9ba5f69ed13081f8a799821a8b00
Harvey Ball <me@harveyball.com>
2019-03-08 15:05:23 +0000

    refactor(Extension): use ReadOnlyCollection instead of Collection
    The current extension for ICollection only had a single method which
    was a read-only operation but any IReadOnlyCollection did not have
    access to the ICollection extensions.
    
    The extension is now for IReadOnlyCollection and as it is just a
    read-only operation method so far, this means that any ICollection
    will have access to the IReadOnlyCollection extension.

--- Runtime/Extension/IReadOnlyCollectionExtensions.cs
+++ Runtime/Extension/IReadOnlyCollectionExtensions.cs
@@ -4,9 +4,9 @@
     using System.Collections.Generic;
 
     /// <summary>
-    /// Extended methods for the <see cref="ICollection{T}"/> Type.
+    /// Extended methods for <see cref="IReadOnlyCollection{T}"/>.
     /// </summary>
-    public static class ICollectionExtensions
+    public static class IReadOnlyCollectionExtensions
     {
         /// <summary>
         /// Returns a wrapped and clamped collection index to prevent an index being out of bounds.
@@ -15,20 +15,20 @@
         /// An index less than zero will return the element based on that index value starting from the end of the collection.
         /// </remarks>
         /// <example>
-        /// Collection[] { A, B, C, D, E }
-        /// > Collection[1] -> 1 (B)
-        /// > Collection[-1] -> 4 (E)
-        /// > Collection[-2] -> 3 (D)
-        /// > Collection[7] -> 4 (E)
-        /// > Collection[-7] -> 0 (A)
+        /// ReadOnlyCollection[] { A, B, C, D, E }
+        /// > ReadOnlyCollection[1] -> 1 (B)
+        /// > ReadOnlyCollection[-1] -> 4 (E)
+        /// > ReadOnlyCollection[-2] -> 3 (D)
+        /// > ReadOnlyCollection[7] -> 4 (E)
+        /// > ReadOnlyCollection[-7] -> 0 (A)
         /// </example>
         /// <typeparam name="T">The type of the source.</typeparam>
-        /// <param name="source">The source <see cref="ICollection{T}"/></param>
+        /// <param name="source">The source <see cref="IReadOnlyCollection{T}"/></param>
         /// <param name="index">The index of the collection to wrap and clamp</param>
         /// <returns>The wrapped and clamped index to be within the bounds of the collection.</returns>
-        public static int GetWrappedAndClampedIndex<T>(this ICollection<T> source, int index)
+        public static int GetWrappedAndClampedIndex<T>(this IReadOnlyCollection<T> source, int index)
         {
-            index = (index >= 0 ? index : source.Count + index);
+            index = index >= 0 ? index : source.Count + index;
             return Mathf.Clamp(index, 0, source.Count - 1);
         }
     }
