fab438558179c229a6699ba09ca6019bb25ca14d
Harvey Ball <me@harveyball.com>
2019-03-08 09:17:21 +0000

    feat(Query): update fields to properties and rename pointing to facing
    All usage of public fields have now been converted to properties that
    are serialized with Malimbe to create an appropriate backing field to
    display in the Unity Inspector.
    
    The PointingQuery has also been renamed to FacingQuery as the actual
    use of the query is to determine if a GameObject is facing (down it's
    local forward direction) another GameObject. The events have also been
    updated to reflect this change.
    
    Tests have also been added for both Query types.

---
+++ Tests/Editor/Tracking/Query/FacingQueryTest.cs
@@ -0,0 +1,96 @@
+﻿using Zinnia.Tracking.Query;
+
+namespace Test.Zinnia.Tracking.Query
+{
+    using UnityEngine;
+    using UnityEngine.TestTools;
+    using System.Collections;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+
+    public class FacingQueryTest
+    {
+        private GameObject containingObject;
+        private FacingQuery subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<FacingQuery>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [UnityTest]
+        public IEnumerator FacingThenNotFacing()
+        {
+            UnityEventListenerMock targetFacedMock = new UnityEventListenerMock();
+            UnityEventListenerMock targetNotFacedMock = new UnityEventListenerMock();
+            subject.TargetFaced.AddListener(targetFacedMock.Listen);
+            subject.TargetNotFaced.AddListener(targetNotFacedMock.Listen);
+
+            GameObject objectA = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject objectB = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            objectA.transform.position = Vector3.left * 2f;
+            objectB.transform.position = Vector3.right * 2f;
+
+            subject.Source = objectA;
+            subject.Target = objectB;
+
+            Assert.IsFalse(targetFacedMock.Received);
+            Assert.IsFalse(targetNotFacedMock.Received);
+
+            targetFacedMock.Reset();
+            targetNotFacedMock.Reset();
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsFalse(targetFacedMock.Received);
+            Assert.IsTrue(targetNotFacedMock.Received);
+
+            targetFacedMock.Reset();
+            targetNotFacedMock.Reset();
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsFalse(targetFacedMock.Received);
+            Assert.IsFalse(targetNotFacedMock.Received);
+
+            targetFacedMock.Reset();
+            targetNotFacedMock.Reset();
+
+            objectA.transform.eulerAngles = Vector3.up * 90f;
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsTrue(targetFacedMock.Received);
+            Assert.IsFalse(targetNotFacedMock.Received);
+
+            targetFacedMock.Reset();
+            targetNotFacedMock.Reset();
+
+            objectA.transform.eulerAngles = Vector3.up * 180f;
+
+            subject.Process();
+
+            yield return new WaitForEndOfFrame();
+
+            Assert.IsFalse(targetFacedMock.Received);
+            Assert.IsTrue(targetNotFacedMock.Received);
+
+            Object.Destroy(objectA);
+            Object.Destroy(objectB);
+        }
+    }
+}
