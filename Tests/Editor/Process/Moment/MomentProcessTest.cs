d2434033ed13e3fa48e1f95274cb020540347b0f
Christopher - Marcel Böddecker <bddckr@icloud.com>
2019-03-17 14:49:25 +0100

    chore(Process): add test for MomentProcess
    `MomentProcess` has recently gained a few additions in functionality
    that should be (unit) tested. This change adds a new test file with
    various tests for the public methods on the type.

---
+++ Tests/Editor/Process/Moment/MomentProcessTest.cs
@@ -0,0 +1,122 @@
+﻿using Zinnia.Process;
+using Zinnia.Process.Moment;
+
+namespace Test.Zinnia.Process.Moment
+{
+    using UnityEngine;
+    using NUnit.Framework;
+
+    public class MomentProcessTest
+    {
+        private GameObject containingObject;
+        private MomentProcess subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            containingObject.SetActive(false);
+
+            subject = containingObject.AddComponent<MomentProcess>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Process()
+        {
+            MockProcessable mockProcessable = new MockProcessable();
+            subject.Source = new ProcessContainer
+            {
+                Interface = mockProcessable
+            };
+            containingObject.SetActive(true);
+            Assert.IsFalse(mockProcessable.WasProcessCalled);
+
+            subject.Process();
+            Assert.IsTrue(mockProcessable.WasProcessCalled);
+            mockProcessable.Reset();
+
+            subject.NextProcessTime = Time.time + 1f;
+            Assert.IsFalse(mockProcessable.WasProcessCalled);
+        }
+
+        [Test]
+        public void ProcessNow()
+        {
+            MockProcessable mockProcessable = new MockProcessable();
+            subject.Source = new ProcessContainer
+            {
+                Interface = mockProcessable
+            };
+            containingObject.SetActive(true);
+            Assert.IsFalse(mockProcessable.WasProcessCalled);
+            mockProcessable.Reset();
+
+            subject.ProcessNow();
+            Assert.IsTrue(mockProcessable.WasProcessCalled);
+            mockProcessable.Reset();
+
+            subject.enabled = false;
+            subject.ProcessNow();
+            Assert.IsFalse(mockProcessable.WasProcessCalled);
+            mockProcessable.Reset();
+
+            subject.enabled = true;
+            containingObject.SetActive(false);
+            subject.ProcessNow();
+            Assert.IsFalse(mockProcessable.WasProcessCalled);
+            mockProcessable.Reset();
+
+            containingObject.SetActive(true);
+            subject.Source = null;
+            subject.ProcessNow();
+            Assert.IsFalse(mockProcessable.WasProcessCalled);
+        }
+
+        [Test]
+        public void RandomizeNextProcessTime()
+        {
+            const float interval = 123.456f;
+            subject.Interval = interval;
+            containingObject.SetActive(true);
+
+            subject.RandomizeNextProcessTime();
+
+            Assert.GreaterOrEqual(subject.NextProcessTime, Time.time);
+            Assert.LessOrEqual(subject.NextProcessTime, Time.time + interval);
+        }
+
+        [Test]
+        public void Awake()
+        {
+            const float interval = 123.456f;
+            subject.Interval = interval;
+
+            Assert.Zero(subject.NextProcessTime);
+            containingObject.SetActive(true);
+
+            Assert.GreaterOrEqual(subject.NextProcessTime, Time.time);
+            Assert.LessOrEqual(subject.NextProcessTime, Time.time + interval);
+        }
+
+        private sealed class MockProcessable : IProcessable
+        {
+            public bool WasProcessCalled;
+
+            public void Process()
+            {
+                WasProcessCalled = true;
+            }
+
+            public void Reset()
+            {
+                WasProcessCalled = false;
+            }
+        }
+    }
+}
