3efed943947039bb344959688c5652df81f1d85b
Harvey Ball <me@harveyball.com>
2019-03-17 15:50:27 +0000

    feat(CameraRig): add observable list for linked alias association
    The LinkedAliasAssociationCollection now has an ObservableList type for
    holding a collection of linked alias associations.

---
+++ Runtime/Tracking/CameraRig/Collection/LinkedAliasAssociationCollectionObservableList.cs
@@ -0,0 +1,21 @@
+﻿namespace Zinnia.Tracking.CameraRig.Collection
+{
+    using UnityEngine.Events;
+    using System;
+    using System.Collections.Generic;
+    using Zinnia.Data.Collection;
+
+    /// <summary>
+    /// Allows observing changes to a <see cref="List{T}"/> of <see cref="LinkedAliasAssociationCollection"/>s.
+    /// </summary>
+    public class LinkedAliasAssociationCollectionObservableList : DefaultObservableList<LinkedAliasAssociationCollection, LinkedAliasAssociationCollectionObservableList.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the <see cref="LinkedAliasAssociationCollection"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<LinkedAliasAssociationCollection>
+        {
+        }
+    }
+}
