1a356a2a4b87a305778793bc969fa9ee874ca952
Harvey Ball <me@harveyball.com>
2019-03-21 21:03:51 +0000

    feat(Extension): ability to queue a behaviour action to run on enable
    The BehaviourExtensions provide a mechanism to queue up a behaviour
    action on a component to run when the component becomes enabled. This
    makes it possible to run methods on disabled components by waiting
    for them to become enabled and then running the appropriate method.
    
    This is very useful for calling ObservableList methods such as Add
    in the OnEnable method of other components in case there is a race
    condition of which component becomes enabled first.

---
+++ Runtime/Extension/BehaviourExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Zinnia.Extension
+{
+    using UnityEngine;
+    using System;
+
+    /// <summary>
+    /// Extended methods for <see cref="Behaviour"/>.
+    /// </summary>
+    public static class BehaviourExtensions
+    {
+        /// <summary>
+        /// Executes the given <see cref="Action"/> when the <see cref="Behaviour"/> becomes active and enabled or immediately runs if the <see cref="Behaviour"/> is already active and enabled.
+        /// </summary>
+        /// <param name="behaviour">The <see cref="Behaviour"/> to check against.</param>
+        /// <param name="action">The <see cref="Action"/> to execute.</param>
+        public static void RunWhenActiveAndEnabled(this Behaviour behaviour, Action action)
+        {
+            void OnBeforeRender()
+            {
+                if (!behaviour.isActiveAndEnabled)
+                {
+                    return;
+                }
+
+                Application.onBeforeRender -= OnBeforeRender;
+                action();
+            }
+
+            if (behaviour.isActiveAndEnabled)
+            {
+                action();
+                return;
+            }
+
+            Application.onBeforeRender += OnBeforeRender;
+        }
+    }
+}
