30e6b463a49212f0fd659c0a8947d62d123c285f
Harvey Ball <me@harveyball.com>
2019-04-14 09:03:11 +0100

    feat(Operation): extract related GameObject of TransformData
    The TransformDataGameObjectExtractor attempts to extract the related
    GameObject of the TransformData stored transform and emits the results.

---
+++ Runtime/Data/Operation/Extraction/TransformDataGameObjectExtractor.cs
@@ -0,0 +1,34 @@
+﻿namespace Zinnia.Data.Operation.Extraction
+{
+    using UnityEngine;
+    using Malimbe.MemberClearanceMethod;
+    using Malimbe.XmlDocumentationAttribute;
+    using Malimbe.PropertySerializationAttribute;
+    using Zinnia.Data.Type;
+
+    /// <summary>
+    /// Extracts and emits the <see cref="Source"/> residing <see cref="GameObject"/>.
+    /// </summary>
+    public class TransformDataGameObjectExtractor : GameObjectExtractor
+    {
+        /// <summary>
+        /// The source to extract from.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: DocumentedByXml]
+        public TransformData Source { get; set; }
+
+        /// <inheritdoc />
+        public override GameObject Extract()
+        {
+            if (Source == null || Source.Transform == null)
+            {
+                Result = null;
+                return null;
+            }
+
+            Result = Source.Transform.gameObject;
+            return base.Extract();
+        }
+    }
+}
---
+++ Tests/Editor/Data/Operation/Extraction/TransformDataGameObjectExtractorTest.cs
@@ -0,0 +1,126 @@
+﻿using Zinnia.Data.Operation.Extraction;
+using Zinnia.Data.Type;
+
+namespace Test.Zinnia.Data.Operation.Extraction
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+
+    public class TransformDataGameObjectExtractorTest
+    {
+        private GameObject containingObject;
+        private TransformDataGameObjectExtractor subject;
+        private GameObject transformData;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<TransformDataGameObjectExtractor>();
+            transformData = new GameObject();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+            Object.DestroyImmediate(transformData);
+        }
+
+        [Test]
+        public void Extract()
+        {
+            TransformData source = new TransformData(transformData);
+
+            UnityEventListenerMock extractedMock = new UnityEventListenerMock();
+            subject.Extracted.AddListener(extractedMock.Listen);
+
+            subject.Source = source;
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+
+            subject.Extract();
+
+            Assert.IsTrue(extractedMock.Received);
+            Assert.AreEqual(subject.Result, transformData);
+        }
+
+        [Test]
+        public void ExtractInvalidSource()
+        {
+            UnityEventListenerMock extractedMock = new UnityEventListenerMock();
+            subject.Extracted.AddListener(extractedMock.Listen);
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+
+            subject.Extract();
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+        }
+
+        [Test]
+        public void ExtractSourceNotSet()
+        {
+            TransformData source = new TransformData();
+
+            UnityEventListenerMock extractedMock = new UnityEventListenerMock();
+            subject.Extracted.AddListener(extractedMock.Listen);
+
+            subject.Source = source;
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+
+            subject.Extract();
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+        }
+
+        [Test]
+        public void ExtractInactiveGameObject()
+        {
+            TransformData source = new TransformData(transformData);
+
+            UnityEventListenerMock extractedMock = new UnityEventListenerMock();
+            subject.Extracted.AddListener(extractedMock.Listen);
+
+            subject.Source = source;
+
+            subject.gameObject.SetActive(false);
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+
+            subject.Extract();
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+        }
+
+        [Test]
+        public void ExtractInactiveComponent()
+        {
+            TransformData source = new TransformData(transformData);
+
+            UnityEventListenerMock extractedMock = new UnityEventListenerMock();
+            subject.Extracted.AddListener(extractedMock.Listen);
+
+            subject.Source = source;
+
+            subject.enabled = false;
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+
+            subject.Extract();
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+        }
+    }
+}
