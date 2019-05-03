e1cbb2f77d48a2041dfcdb5ade147e99854598a6
Harvey Ball <me@harveyball.com>
2019-05-03 10:07:41 +0100

    feat(Extraction): extract collision point from SurfaceData
    The SurfaceDataCollisionPointExtractor allows the point of collision to
    be extracted from SurfaceData and emits the Vector3 value.
    
    This can be useful for obtaining the actual point of a SurfaceData
    collision.

---
+++ Runtime/Data/Operation/Extraction/SurfaceDataCollisionPointExtractor.cs
@@ -0,0 +1,54 @@
+﻿namespace Zinnia.Data.Operation.Extraction
+{
+    using UnityEngine;
+    using Malimbe.MemberClearanceMethod;
+    using Malimbe.XmlDocumentationAttribute;
+    using Malimbe.PropertySerializationAttribute;
+    using Zinnia.Data.Type;
+
+    /// <summary>
+    /// Extracts and emits the point of collision from <see cref="SurfaceData"/>.
+    /// </summary>
+    public class SurfaceDataCollisionPointExtractor : Vector3Extractor
+    {
+        /// <summary>
+        /// The source to extract from.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: DocumentedByXml]
+        public SurfaceData Source { get; set; }
+
+        /// <inheritdoc />
+        public override Vector3? Extract()
+        {
+            if (Source == null || Source.CollisionData.transform == null)
+            {
+                Result = null;
+                return null;
+            }
+
+            Result = Source.CollisionData.point;
+            return base.Extract();
+        }
+
+        /// <summary>
+        /// Extracts the <see cref="Vector3"/> from the given <see cref="SurfaceData"/>.
+        /// </summary>
+        /// <param name="data">The data to extract from.</param>
+        /// <returns>The extracted collision point.</returns>
+        public virtual Vector3? Extract(SurfaceData data)
+        {
+            Source = data;
+            return Extract();
+        }
+
+        /// <summary>
+        /// Extracts the <see cref="Vector3"/> from the given <see cref="SurfaceData"/>.
+        /// </summary>
+        /// <param name="data">The data to extract from.</param>
+        public virtual void DoExtract(SurfaceData data)
+        {
+            Extract(data);
+        }
+    }
+}
---
+++ Runtime/Data/Operation/Extraction/Vector3Extractor.cs
@@ -0,0 +1,54 @@
+﻿namespace Zinnia.Data.Operation.Extraction
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.XmlDocumentationAttribute;
+
+    /// <summary>
+    /// Provides the basis for emitting a <see cref="Vector3"/>.
+    /// </summary>
+    public abstract class Vector3Extractor : MonoBehaviour
+    {
+        /// <summary>
+        /// Defines an event with a <see cref="Vector3"/> value.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3> { }
+
+        /// <summary>
+        /// Emitted when the <see cref="Vector3"/> is extracted.
+        /// </summary>
+        [DocumentedByXml]
+        public UnityEvent Extracted = new UnityEvent();
+
+        /// <summary>
+        /// The extracted <see cref="Vector3"/>.
+        /// </summary>
+        public Vector3? Result { get; protected set; }
+
+        /// <summary>
+        /// Extracts the <see cref="Vector3"/>.
+        /// </summary>
+        /// <returns>The extracted <see cref="Vector3"/>.</returns>
+        public virtual Vector3? Extract()
+        {
+            if (!isActiveAndEnabled || Result == null)
+            {
+                Result = null;
+                return null;
+            }
+
+            Extracted?.Invoke((Vector3)Result);
+            return Result;
+        }
+
+        /// <summary>
+        /// Extracts the <see cref="Vector3"/>.
+        /// </summary>
+        public virtual void DoExtract()
+        {
+            Extract();
+        }
+    }
+}
