3e294a78707c8f7c1d727a8771f56be46c4fa125
Harvey Ball <me@harveyball.com>
2019-06-04 13:53:35 +0100

    feat(Extension): get collider attached rigidbody even when inactive
    The default `Collider.attachedRigidbody` does not report the parent
    Rigidbody for a Collider if the GameObject (or parent GameObject)
    is inactive, instead it returns null.
    
    This isn't helpful when attempting to deal with stopping collisions
    when the GameObject is being disabled. The solution is to provide
    a new extension method that uses the `Collider.attachedRigidbody`
    getter by default but if the collider GameObject is not active then
    it falls back to using a more brute force approach of getting
    all Rigidbodies in the parent GameObjects and returning the first
    one found.

--- Runtime/Extension/ColliderExtensions.cs
+++ Runtime/Extension/ColliderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace Zinnia.Extension
 {
     using UnityEngine;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Extended methods for the <see cref="Collider"/> Type.
@@ -19,8 +20,33 @@
                 return null;
             }
 
-            Rigidbody attachedRigidbody = collider.attachedRigidbody;
+            Rigidbody attachedRigidbody = collider.GetAttachedRigidbody();
             return attachedRigidbody == null ? collider.transform : attachedRigidbody.transform;
         }
+
+        /// <summary>
+        /// Gets the parent <see cref="Rigidbody"/> for the given <see cref="Collider"/> even if the <see cref="GameObject"/> is disabled.
+        /// </summary>
+        /// <param name="collider">The <see cref="Collider"/> to check against.</param>
+        /// <returns>The parent <see cref="Rigidbody"/>.</returns>
+        public static Rigidbody GetAttachedRigidbody(this Collider collider)
+        {
+            Rigidbody attachedRigidbody = collider.attachedRigidbody;
+            if (!collider.gameObject.activeInHierarchy)
+            {
+                collider.GetComponentsInParent(true, foundRigidbodies);
+                foreach (Rigidbody foundRigidbody in foundRigidbodies)
+                {
+                    attachedRigidbody = foundRigidbody;
+                    break;
+                }
+            }
+            return attachedRigidbody;
+        }
+
+        /// <summary>
+        /// A <see cref="Rigidbody"/> collection to store found <see cref="Rigidbody"/>s in.
+        /// </summary>
+        private static List<Rigidbody> foundRigidbodies = new List<Rigidbody>();
     }
 }
