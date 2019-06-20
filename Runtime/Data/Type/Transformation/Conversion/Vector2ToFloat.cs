d59d1b7003fa489656f9d0443a7d3951600f7455
Harvey Ball <me@harveyball.com>
2019-06-20 14:28:27 +0100

    feat(Transformation): convert a vector2 coordinate to a float
    The Vector2ToFloat allows a Vector2 coordinate to be extracted and
    converted to a float value.

---
+++ Runtime/Data/Type/Transformation/Conversion/Vector2ToFloat.cs
@@ -0,0 +1,63 @@
+﻿namespace Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.XmlDocumentationAttribute;
+    using Malimbe.PropertySerializationAttribute;
+
+    /// <summary>
+    /// Transforms a <see cref="Vector2"/> to a <see cref="float"/> and allows mapping of the relevant coordinates.
+    /// </summary>
+    /// <example>
+    /// Vector2(1f, 2f) -> ExtractX -> 1f
+    /// Vector2(1f, 2f) -> ExtractY -> 2f
+    /// </example>
+    public class Vector2ToFloat : Transformer<Vector2, float, Vector2ToFloat.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the transformed <see cref="float"/> value.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<float> { }
+
+        /// <summary>
+        /// The <see cref="Vector2"/> coordinate to extract.
+        /// </summary>
+        public enum ExtractionCoordinate
+        {
+            /// <summary>
+            /// Extracts the X value.
+            /// </summary>
+            ExtractX,
+            /// <summary>
+            /// Extracts the Y value.
+            /// </summary>
+            ExtractY
+        }
+
+        /// <summary>
+        /// Which <see cref="Vector2"/> coordinate to extract.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public ExtractionCoordinate CoordinateToExtract { get; set; } = ExtractionCoordinate.ExtractX;
+
+        /// <summary>
+        /// Transforms the given <see cref="Vector2"/> into a <see cref="float"/>.
+        /// </summary>
+        /// <param name="input">The value to transform.</param>
+        /// <returns>The transformed value.</returns>
+        protected override float Process(Vector2 input)
+        {
+            switch (CoordinateToExtract)
+            {
+                case ExtractionCoordinate.ExtractX:
+                    return input.x;
+                case ExtractionCoordinate.ExtractY:
+                    return input.y;
+            }
+            return 0f;
+        }
+    }
+}
---
+++ Tests/Editor/Data/Type/Transformation/Conversion/Vector2ToFloatTest.cs
@@ -0,0 +1,66 @@
+﻿using Zinnia.Data.Type.Transformation.Conversion;
+
+namespace Test.Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+
+    public class Vector2ToFloatTest : MonoBehaviour
+    {
+        private GameObject containingObject;
+        private Vector2ToFloat subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<Vector2ToFloat>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DestroyImmediate(subject);
+            DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void TransformExtractX()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector2ToFloat.ExtractionCoordinate.ExtractX;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector2 input = new Vector3(1f, 2f);
+            float result = subject.Transform(input);
+            float expectedResult = 1f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformExtractY()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector2ToFloat.ExtractionCoordinate.ExtractY;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector2 input = new Vector3(1f, 2f);
+            float result = subject.Transform(input);
+            float expectedResult = 2f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+    }
+}
