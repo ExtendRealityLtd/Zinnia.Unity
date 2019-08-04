d25605bab71e3ac810923f5cb21633c5c30876e0
Ethan Cheung <ethan@fight4dream.com>
2019-08-04 17:51:10 +0800

    feat(Data): transform float to Vector3
    The FloatToVector3 component allows to transform a float array
    to a Vector3.

---
+++ Runtime/Data/Type/Transformation/Conversion/FloatToVector3.cs
@@ -0,0 +1,78 @@
+﻿namespace Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.XmlDocumentationAttribute;
+    using Malimbe.PropertySerializationAttribute;
+
+    /// <summary>
+    /// Transforms a float array to a Vector3.
+    /// </summary>
+    /// <example>
+    /// float[2f, 3f, 4f] = Vector3(2f, 3f, 4f)
+    /// </example>
+    public class FloatToVector3 : Transformer<float[], Vector3, FloatToVector3.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the transformed <see cref="Vector3"/> value.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3>
+        {
+        }
+
+        /// <summary>
+        /// A float to use as the current x value of the Vector3.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public float CurrentX { get; set; }
+        /// <summary>
+        /// A float to use as the current y value of the Vector3.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public float CurrentY { get; set; }
+        /// <summary>
+        /// A float to use as the current z value of the Vector3.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public float CurrentZ { get; set; }
+
+        /// <summary>
+        /// A reusable array of three <see cref="float"/>s.
+        /// </summary>
+        protected readonly float[] input = new float[3];
+
+        /// <summary>
+        /// Builds a float array from the current set x, y and z values and transforms it into a Vector3.
+        /// </summary>
+        public virtual Vector3 Transform()
+        {
+            input[0] = CurrentX;
+            input[1] = CurrentY;
+            input[2] = CurrentZ;
+            return Transform(input);
+        }
+
+        /// <summary>
+        /// Builds a float array from the current set x, y and z values and transforms it into a Vector3.
+        /// </summary>
+        public virtual void DoTransform()
+        {
+            Transform();
+        }
+
+        /// <summary>
+        /// Transforms the given float array into a Vector3.
+        /// </summary>
+        /// <param name="input">The value to transform.</param>
+        /// <returns>The transformed value or <see cref="Vector3.zero"/> if the input isn't three-dimensional.</returns>
+        protected override Vector3 Process(float[] input)
+        {
+            return input.Length >= 3 ? new Vector3(input[0], input[1], input[2]) : Vector3.zero;
+        }
+    }
+}
---
+++ Tests/Editor/Data/Type/Transformation/Conversion/FloatToVector3Test.cs
@@ -0,0 +1,103 @@
+﻿using Zinnia.Data.Type.Transformation.Conversion;
+
+namespace Test.Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class FloatToVector3Test
+    {
+        private GameObject containingObject;
+        private FloatToVector3 subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<FloatToVector3>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Transform()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float[] input = new float[] { 2f, 3f, 4f };
+            Vector3 result = subject.Transform(input);
+            Vector3 expectedResult = new Vector3(2f, 3f, 4f);
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformWithSet()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.CurrentX = 2f;
+            subject.CurrentY = 3f;
+            subject.CurrentZ = 4f;
+            Vector3 result = subject.Transform();
+            Vector3 expectedResult = new Vector3(2f, 3f, 4f);
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveGameObject()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.gameObject.SetActive(false);
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float[] input = new float[] { 2f, 3f, 4f };
+            Vector3 result = subject.Transform(input);
+
+            Assert.AreEqual(Vector3.zero, result);
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveComponent()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.enabled = false;
+
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float[] input = new float[] { 2f, 3f, 4f };
+            Vector3 result = subject.Transform(input);
+
+            Assert.AreEqual(Vector3.zero, result);
+            Assert.AreEqual(Vector3.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+    }
+}
