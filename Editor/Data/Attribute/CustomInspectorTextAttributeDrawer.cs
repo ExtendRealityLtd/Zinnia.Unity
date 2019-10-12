6261ed0b666054ca2e55cd403b3bb52e7af4c98d
extendreality <hello@extendreality.io>
2019-10-12 15:35:10 +0100

    feat(Attribute): provide custom inspector text for inspector properties
    The new CustomInspectorText attribute allows an inspector field to have
    custom text instead of Unity using the field/property name for the
    inspector text.
    
    This only works with simple fields at the moment and does not work with
    nested fields such as the ObservableList Drawer.

---
+++ Editor/Data/Attribute/CustomInspectorTextAttributeDrawer.cs
@@ -0,0 +1,14 @@
+﻿namespace Zinnia.Data.Attribute
+{
+    using UnityEngine;
+    using UnityEditor;
+
+    [CustomPropertyDrawer(typeof(CustomInspectorTextAttribute))]
+    public class CustomInspectorTextAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property, new GUIContent(((CustomInspectorTextAttribute)attribute).customText));
+        }
+    }
+}
