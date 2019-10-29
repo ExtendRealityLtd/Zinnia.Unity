e4be751fc6764aa38be63fcf9f37c97a50a7cade
Ethan Cheung <ethan@fight4dream.com>
2019-10-30 02:14:05 +0800

    feat(Utility): provide remaining and elapsed time when requested
    The CountdownTimer now has EmitElapsedTime and EmitRemainingTime methods
    which emit the ElapsedTime and RemainingTime of the timer.

--- Runtime/Utility/CountdownTimer.cs
+++ Runtime/Utility/CountdownTimer.cs
@@ -2,7 +2,9 @@
 {
     using UnityEngine;
     using UnityEngine.Events;
+    using System;
     using Malimbe.BehaviourStateRequirementMethod;
+    using Malimbe.MemberChangeMethod;
     using Malimbe.PropertySerializationAttribute;
     using Malimbe.XmlDocumentationAttribute;
 
@@ -11,6 +13,14 @@
     /// </summary>
     public class CountdownTimer : MonoBehaviour
     {
+        /// <summary>
+        /// Defines the event with the specified <see cref="float"/>.
+        /// </summary>
+        [Serializable]
+        public class FloatUnityEvent : UnityEvent<float>
+        {
+        }
+
         /// <summary>
         /// The time to start the countdown at.
         /// </summary>
@@ -43,6 +53,16 @@
         /// </summary>
         [DocumentedByXml]
         public UnityEvent NotRunning = new UnityEvent();
+        /// <summary>
+        /// Emitted when the elapsed time is checked.
+        /// </summary>
+        [DocumentedByXml]
+        public FloatUnityEvent ElapsedTimeEmitted = new FloatUnityEvent();
+        /// <summary>
+        /// Emitted when the remaining time is checked.
+        /// </summary>
+        [DocumentedByXml]
+        public FloatUnityEvent RemainingTimeEmitted = new FloatUnityEvent();
 
         /// <summary>
         /// Determines if the countdown is still running.
@@ -54,12 +74,52 @@
         }
 
         /// <summary>
+        /// Elapsed time of the timer.
+        /// </summary>
+        public float ElapsedTime
+        {
+            get
+            {
+                if (IsRunning)
+                {
+                    currentTime = Time.time;
+                }
+                return currentTime - beginTime;
+            }
+        }
+
+        /// <summary>
+        /// Remaining time of the timer.
+        /// </summary>
+        public float RemainingTime
+        {
+            get
+            {
+                if (IsRunning)
+                {
+                    currentTime = Time.time;
+                }
+                return StartTime + (beginTime - currentTime);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="Time.time"/> when <see cref="Begin"/> is called.
+        /// </summary>
+        protected float beginTime;
+        /// <summary>
+        /// <see cref="Time.time"/> of the current frame.
+        /// </summary>
+        protected float currentTime;
+
+        /// <summary>
         /// Starts the timer counting down.
         /// </summary>
         [RequiresBehaviourState]
         public virtual void Begin()
         {
             IsRunning = true;
+            SetInternalStates();
             Invoke(nameof(Complete), StartTime);
             Started?.Invoke();
         }
@@ -72,6 +132,7 @@
             CancelInvoke(nameof(Complete));
             if (IsRunning)
             {
+                currentTime = Time.time;
                 Cancelled?.Invoke();
                 IsRunning = false;
             }
@@ -93,6 +154,29 @@
             }
         }
 
+        /// <summary>
+        /// Emits the elapsed time of the timer.
+        /// </summary>
+        [RequiresBehaviourState]
+        public virtual void EmitElapsedTime()
+        {
+            ElapsedTimeEmitted?.Invoke(ElapsedTime);
+        }
+
+        /// <summary>
+        /// Emits the remaining time of the timer.
+        /// </summary>
+        [RequiresBehaviourState]
+        public virtual void EmitRemainingTime()
+        {
+            RemainingTimeEmitted?.Invoke(RemainingTime);
+        }
+
+        protected virtual void OnEnable()
+        {
+            SetInternalStates();
+        }
+
         protected virtual void OnDisable()
         {
             Cancel();
@@ -103,8 +187,27 @@
         /// </summary>
         protected virtual void Complete()
         {
+            currentTime = StartTime + beginTime;
             IsRunning = false;
             Completed?.Invoke();
+        }
+
+        /// <summary>
+        /// Stores current <see cref="Time.time"/> for calculations.
+        /// </summary>
+        protected virtual void SetInternalStates()
+        {
+            beginTime = Time.time;
+            currentTime = Time.time;
+        }
+
+        /// <summary>
+        /// Called after <see cref="StartTime"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(StartTime))]
+        protected virtual void OnAfterStartTimeChange()
+        {
+            SetInternalStates();
         }
     }
 }
--- Tests/Editor/Utility/CountdownTimerTest.cs
+++ Tests/Editor/Utility/CountdownTimerTest.cs
@@ -2,15 +2,16 @@
 
 namespace Test.Zinnia.Utility
 {
-    using UnityEngine;
-    using UnityEngine.TestTools;
     using System.Collections;
     using NUnit.Framework;
     using Test.Zinnia.Utility.Mock;
+    using UnityEngine;
+    using UnityEngine.TestTools;
     using Assert = UnityEngine.Assertions.Assert;
 
     public class CountdownTimerTest
     {
+        private const float Tolerance = 0.01f;
         private GameObject containingObject;
         private CountdownTimer subject;
 
@@ -317,6 +318,239 @@ namespace Test.Zinnia.Utility
 
             Assert.IsFalse(timerStillRunningMock.Received);
             Assert.IsFalse(timerNotRunningMock.Received);
+        }
+
+        [UnityTest]
+        public IEnumerator TimerEmitTime()
+        {
+            UnityEventListenerMock timerCompleteMock = new UnityEventListenerMock();
+            UnityEventValueListenerMock<float> timerElapsedTimeMock = new UnityEventValueListenerMock<float>();
+            UnityEventValueListenerMock<float> timerRemainingTimeMock = new UnityEventValueListenerMock<float>();
+
+            subject.Completed.AddListener(timerCompleteMock.Listen);
+            subject.ElapsedTimeEmitted.AddListener(timerElapsedTimeMock.Listen);
+            subject.RemainingTimeEmitted.AddListener(timerRemainingTimeMock.Listen);
+
+            float beginTime = Time.time;
+            float startTime = 0.5f;
+
+            subject.StartTime = startTime;
+
+            Assert.IsFalse(timerElapsedTimeMock.Received);
+            Assert.IsFalse(timerRemainingTimeMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0f, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0.5f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            subject.Begin();
+
+            Assert.IsFalse(timerCompleteMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0f, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0.5f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            yield return new WaitForSeconds(0.2f);
+
+            float elapsedTime = Time.time - beginTime;
+            float remainingTime = startTime + (beginTime - Time.time);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreEqual(timerElapsedTimeMock.Value, elapsedTime);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreEqual(timerRemainingTimeMock.Value, remainingTime);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            yield return new WaitForSeconds(0.3f);
+
+            Assert.IsTrue(timerCompleteMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0.5f, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0f, Tolerance);
+        }
+
+        [UnityTest]
+        public IEnumerator TimerEmitTimeUnchangedAfterComplete()
+        {
+            UnityEventListenerMock timerCompleteMock = new UnityEventListenerMock();
+            UnityEventValueListenerMock<float> timerElapsedTimeMock = new UnityEventValueListenerMock<float>();
+            UnityEventValueListenerMock<float> timerRemainingTimeMock = new UnityEventValueListenerMock<float>();
+
+            subject.Completed.AddListener(timerCompleteMock.Listen);
+            subject.ElapsedTimeEmitted.AddListener(timerElapsedTimeMock.Listen);
+            subject.RemainingTimeEmitted.AddListener(timerRemainingTimeMock.Listen);
+
+            float beginTime = Time.time;
+            float startTime = 0.5f;
+            subject.StartTime = startTime;
+
+            Assert.IsFalse(timerElapsedTimeMock.Received);
+            Assert.IsFalse(timerRemainingTimeMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0.5f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            subject.Begin();
+
+            Assert.IsFalse(timerCompleteMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0.5f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            yield return new WaitForSeconds(0.5f);
+
+            Assert.IsTrue(timerCompleteMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0.5f, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            yield return new WaitForSeconds(0.5f);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0.5f, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0f, Tolerance);
+        }
+
+        [UnityTest]
+        public IEnumerator TimerEmitTimeUnchangedAfterCancelled()
+        {
+            UnityEventListenerMock timerCompleteMock = new UnityEventListenerMock();
+            UnityEventListenerMock timerCancelledMock = new UnityEventListenerMock();
+            UnityEventValueListenerMock<float> timerElapsedTimeMock = new UnityEventValueListenerMock<float>();
+            UnityEventValueListenerMock<float> timerRemainingTimeMock = new UnityEventValueListenerMock<float>();
+
+            subject.Completed.AddListener(timerCompleteMock.Listen);
+            subject.Cancelled.AddListener(timerCancelledMock.Listen);
+            subject.ElapsedTimeEmitted.AddListener(timerElapsedTimeMock.Listen);
+            subject.RemainingTimeEmitted.AddListener(timerRemainingTimeMock.Listen);
+
+            float beginTime = Time.time;
+            float startTime = 0.5f;
+            subject.StartTime = startTime;
+
+            Assert.IsFalse(timerElapsedTimeMock.Received);
+            Assert.IsFalse(timerRemainingTimeMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0.5f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            subject.Begin();
+
+            Assert.IsFalse(timerCompleteMock.Received);
+            Assert.IsFalse(timerCancelledMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerElapsedTimeMock.Value, 0, Tolerance);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreApproximatelyEqual(timerRemainingTimeMock.Value, 0.5f, Tolerance);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            yield return new WaitForSeconds(0.2f);
+
+            float elapsedTime = Time.time - beginTime;
+            float remainingTime = startTime + (beginTime - Time.time);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreEqual(timerElapsedTimeMock.Value, elapsedTime);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreEqual(timerRemainingTimeMock.Value, remainingTime);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            subject.Cancel();
+            Assert.IsFalse(timerCompleteMock.Received);
+            Assert.IsTrue(timerCancelledMock.Received);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreEqual(timerElapsedTimeMock.Value, elapsedTime);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreEqual(timerRemainingTimeMock.Value, remainingTime);
+
+            timerElapsedTimeMock.Reset();
+            timerRemainingTimeMock.Reset();
+
+            yield return new WaitForSeconds(0.5f);
+
+            subject.EmitElapsedTime();
+            Assert.IsTrue(timerElapsedTimeMock.Received);
+            Assert.AreEqual(timerElapsedTimeMock.Value, elapsedTime);
+
+            subject.EmitRemainingTime();
+            Assert.IsTrue(timerRemainingTimeMock.Received);
+            Assert.AreEqual(timerRemainingTimeMock.Value, remainingTime);
         }
     }
 }
