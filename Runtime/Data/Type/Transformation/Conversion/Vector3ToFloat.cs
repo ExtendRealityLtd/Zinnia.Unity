5bca07019aa326622ccb47d43d8854a606fb6ed4
Ethan Cheung <ethan@fight4dream.com>
2019-11-04 23:49:05 +0800

    feat(Data): add properties to extract float from Vector2 and Vector3
    Added Vector3ToFloat to extract x, y, z, magnitude and sqrMagnitude
    from Vector3.
    Added extraction of magnitude and sqrMagnitude from Vector2.
    Added Vector2MagnitudeSetter.

--- Runtime/Data/Type/Transformation/Conversion/Vector3ToFloat.cs
+++ Runtime/Data/Type/Transformation/Conversion/Vector3ToFloat.cs
@@ -7,15 +7,16 @@
     using Malimbe.PropertySerializationAttribute;
 
     /// <summary>
-    /// Transforms a <see cref="Vector2"/> to a <see cref="float"/> and allows mapping of the relevant coordinates.
+    /// Transforms a <see cref="Vector3"/> to a <see cref="float"/> and allows mapping of the relevant coordinates.
     /// </summary>
     /// <example>
-    /// Vector2(3f, 4f) -> ExtractX -> 3f
-    /// Vector2(3f, 4f) -> ExtractY -> 4f
-    /// Vector2(3f, 4f) -> ExtractMagnitude -> 5f
-    /// Vector2(3f, 4f) -> ExtractSqrMagnitude -> 25f
+    /// Vector3(2f, 3f, 6f) -> ExtractX -> 2f
+    /// Vector3(2f, 3f, 6f) -> ExtractY -> 3f
+    /// Vector3(2f, 3f, 6f) -> ExtractZ -> 6f
+    /// Vector3(2f, 3f, 6f) -> ExtractMagnitude -> 7f
+    /// Vector3(2f, 3f, 6f) -> ExtractSqrMagnitude -> 49f
     /// </example>
-    public class Vector2ToFloat : Transformer<Vector2, float, Vector2ToFloat.UnityEvent>
+    public class Vector3ToFloat : Transformer<Vector3, float, Vector3ToFloat.UnityEvent>
     {
         /// <summary>
         /// Defines the event with the transformed <see cref="float"/> value.
@@ -24,7 +25,7 @@
         public class UnityEvent : UnityEvent<float> { }
 
         /// <summary>
-        /// The <see cref="Vector2"/> coordinate to extract.
+        /// The <see cref="Vector3"/> coordinate to extract.
         /// </summary>
         public enum ExtractionCoordinate
         {
@@ -37,6 +38,10 @@
             /// </summary>
             ExtractY,
             /// <summary>
+            /// Extracts the Z value.
+            /// </summary>
+            ExtractZ,
+            /// <summary>
             /// Extracts the magnitude.
             /// </summary>
             ExtractMagnitude,
@@ -47,18 +52,18 @@
         }
 
         /// <summary>
-        /// Which <see cref="Vector2"/> coordinate to extract.
+        /// Which <see cref="Vector3"/> coordinate to extract.
         /// </summary>
         [Serialized]
         [field: DocumentedByXml]
         public ExtractionCoordinate CoordinateToExtract { get; set; } = ExtractionCoordinate.ExtractX;
 
         /// <summary>
-        /// Transforms the given <see cref="Vector2"/> into a <see cref="float"/>.
+        /// Transforms the given <see cref="Vector3"/> into a <see cref="float"/>.
         /// </summary>
         /// <param name="input">The value to transform.</param>
         /// <returns>The transformed value.</returns>
-        protected override float Process(Vector2 input)
+        protected override float Process(Vector3 input)
         {
             switch (CoordinateToExtract)
             {
@@ -66,6 +71,8 @@
                     return input.x;
                 case ExtractionCoordinate.ExtractY:
                     return input.y;
+                case ExtractionCoordinate.ExtractZ:
+                    return input.z;
                 case ExtractionCoordinate.ExtractMagnitude:
                     return input.magnitude;
                 case ExtractionCoordinate.ExtractSqrMagnitude:
---
+++ Tests/Editor/Data/Type/Transformation/Vector2MagnitudeSetterTest.cs
@@ -0,0 +1,58 @@
+﻿using Zinnia.Data.Type.Transformation;
+
+namespace Test.Zinnia.Data.Type.Transformation
+{
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+    using UnityEngine;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class Vector2MagnitudeSetterTest
+    {
+        private Vector2MagnitudeSetter subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new GameObject().AddComponent<Vector2MagnitudeSetter>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject.gameObject);
+        }
+
+        [Test]
+        public void SetVectorMagnitude()
+        {
+            subject.Magnitude = 2f;
+
+            Assert.AreEqual(2f, subject.Magnitude);
+
+            Vector2 expected = new Vector2(1f, 2f);
+            subject.SetMagnitude(expected);
+
+            Assert.AreEqual(expected.magnitude, subject.Magnitude);
+        }
+
+        [Test]
+        public void Process()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+
+            Assert.AreEqual(Vector2.zero, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            subject.Magnitude = 2f;
+            Vector2 input = new Vector2(1f, -2f);
+            Vector2 result = subject.Transform(input);
+            Vector2 expectedResult = new Vector2(0.8944272f, -1.7888544f);
+
+            Assert.IsTrue(expectedResult == result);
+            Assert.IsTrue(expectedResult == subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+    }
+}
