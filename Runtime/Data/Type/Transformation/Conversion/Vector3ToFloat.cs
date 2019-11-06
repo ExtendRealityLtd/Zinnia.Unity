0fba5952001a313792125faa0f394e072349dfac
Ethan Cheung <ethan@fight4dream.com>
2019-11-06 23:52:54 +0800

    feat(Data): add properties to extract float from Vector3
    Added Vector3ToFloat to extract x, y, z, magnitude and sqrMagnitude
    from Vector3.

---
+++ Runtime/Data/Type/Transformation/Conversion/Vector3ToFloat.cs
@@ -0,0 +1,84 @@
+﻿namespace Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.XmlDocumentationAttribute;
+    using Malimbe.PropertySerializationAttribute;
+
+    /// <summary>
+    /// Transforms a <see cref="Vector3"/> to a <see cref="float"/> and allows mapping of the relevant coordinates.
+    /// </summary>
+    /// <example>
+    /// Vector3(2f, 3f, 6f) -> ExtractX -> 2f
+    /// Vector3(2f, 3f, 6f) -> ExtractY -> 3f
+    /// Vector3(2f, 3f, 6f) -> ExtractZ -> 6f
+    /// Vector3(2f, 3f, 6f) -> ExtractMagnitude -> 7f
+    /// Vector3(2f, 3f, 6f) -> ExtractSqrMagnitude -> 49f
+    /// </example>
+    public class Vector3ToFloat : Transformer<Vector3, float, Vector3ToFloat.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the transformed <see cref="float"/> value.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<float> { }
+
+        /// <summary>
+        /// The <see cref="Vector3"/> coordinate to extract.
+        /// </summary>
+        public enum ExtractionCoordinate
+        {
+            /// <summary>
+            /// Extracts the X value.
+            /// </summary>
+            ExtractX,
+            /// <summary>
+            /// Extracts the Y value.
+            /// </summary>
+            ExtractY,
+            /// <summary>
+            /// Extracts the Z value.
+            /// </summary>
+            ExtractZ,
+            /// <summary>
+            /// Extracts the magnitude.
+            /// </summary>
+            ExtractMagnitude,
+            /// <summary>
+            /// Extracts the squared magnitude.
+            /// </summary>
+            ExtractSqrMagnitude
+        }
+
+        /// <summary>
+        /// Which <see cref="Vector3"/> coordinate to extract.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public ExtractionCoordinate CoordinateToExtract { get; set; } = ExtractionCoordinate.ExtractX;
+
+        /// <summary>
+        /// Transforms the given <see cref="Vector3"/> into a <see cref="float"/>.
+        /// </summary>
+        /// <param name="input">The value to transform.</param>
+        /// <returns>The transformed value.</returns>
+        protected override float Process(Vector3 input)
+        {
+            switch (CoordinateToExtract)
+            {
+                case ExtractionCoordinate.ExtractX:
+                    return input.x;
+                case ExtractionCoordinate.ExtractY:
+                    return input.y;
+                case ExtractionCoordinate.ExtractZ:
+                    return input.z;
+                case ExtractionCoordinate.ExtractMagnitude:
+                    return input.magnitude;
+                case ExtractionCoordinate.ExtractSqrMagnitude:
+                    return input.sqrMagnitude;
+            }
+            return 0f;
+        }
+    }
+}
---
+++ Tests/Editor/Data/Type/Transformation/Conversion/Vector3ToFloatTest.cs
@@ -0,0 +1,124 @@
+﻿using Zinnia.Data.Type.Transformation.Conversion;
+
+namespace Test.Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class Vector3ToFloatTest
+    {
+        private GameObject containingObject;
+        private Vector3ToFloat subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<Vector3ToFloat>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void TransformExtractX()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector3ToFloat.ExtractionCoordinate.ExtractX;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 6f);
+            float result = subject.Transform(input);
+            float expectedResult = 2f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformExtractY()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector3ToFloat.ExtractionCoordinate.ExtractY;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 6f);
+            float result = subject.Transform(input);
+            float expectedResult = 3f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformExtractZ()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector3ToFloat.ExtractionCoordinate.ExtractZ;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 6f);
+            float result = subject.Transform(input);
+            float expectedResult = 6f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformExtractMagnitude()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector3ToFloat.ExtractionCoordinate.ExtractMagnitude;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 6f);
+            float result = subject.Transform(input);
+            float expectedResult = 7f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformExtractSqrMagnitude()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.CoordinateToExtract = Vector3ToFloat.ExtractionCoordinate.ExtractSqrMagnitude;
+
+            Assert.AreEqual(0f, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            Vector3 input = new Vector3(2f, 3f, 6f);
+            float result = subject.Transform(input);
+            float expectedResult = 49f;
+
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(expectedResult, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+    }
+}
