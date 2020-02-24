b75f607c461bd04189b24c5c981e11d880efcfab
Christopher - Marcel Böddecker <bddckr@icloud.com>
2020-02-24 17:58:15 +0000

    feat(Data): add KeyNotFound event to GameObjectRelations
    The KeyNotFound event is raised whenever the GetValue method on the
    GameObjectRelations component is called but no key can be matched
    because either the key is not in the dictionary or the index given
    is out of bounds of the collection.

--- Tests/Editor/Data/Collection/GameObjectRelationsTest.cs
+++ Tests/Editor/Data/Collection/GameObjectRelationsTest.cs
@@ -30,7 +30,9 @@ namespace Test.Zinnia.Data.Collection
         public void GetValueByKey()
         {
             UnityEventListenerMock valueRetrievedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock valueNotFoundListenerMock = new UnityEventListenerMock();
             subject.ValueRetrieved.AddListener(valueRetrievedListenerMock.Listen);
+            subject.KeyNotFound.AddListener(valueNotFoundListenerMock.Listen);
 
             GameObject keyOne = new GameObject();
             GameObject valueOne = new GameObject();
@@ -52,10 +54,12 @@ namespace Test.Zinnia.Data.Collection
             subject.Relations.Add(relationThree);
 
             Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
 
             Assert.AreEqual(valueTwo, subject.GetValue(keyTwo));
 
             Assert.IsTrue(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
 
             Object.DestroyImmediate(keyOne);
             Object.DestroyImmediate(valueOne);
@@ -69,7 +73,9 @@ namespace Test.Zinnia.Data.Collection
         public void GetValueByKeyNotFound()
         {
             UnityEventListenerMock valueRetrievedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock valueNotFoundListenerMock = new UnityEventListenerMock();
             subject.ValueRetrieved.AddListener(valueRetrievedListenerMock.Listen);
+            subject.KeyNotFound.AddListener(valueNotFoundListenerMock.Listen);
 
             GameObject keyOne = new GameObject();
             GameObject valueOne = new GameObject();
@@ -88,10 +94,12 @@ namespace Test.Zinnia.Data.Collection
             subject.Relations.Add(relationThree);
 
             Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
 
             Assert.IsNull(subject.GetValue(keyTwo));
 
             Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsTrue(valueNotFoundListenerMock.Received);
 
             Object.DestroyImmediate(keyOne);
             Object.DestroyImmediate(valueOne);
@@ -105,7 +113,9 @@ namespace Test.Zinnia.Data.Collection
         public void GetValueByIndex()
         {
             UnityEventListenerMock valueRetrievedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock valueNotFoundListenerMock = new UnityEventListenerMock();
             subject.ValueRetrieved.AddListener(valueRetrievedListenerMock.Listen);
+            subject.KeyNotFound.AddListener(valueNotFoundListenerMock.Listen);
 
             GameObject keyOne = new GameObject();
             GameObject valueOne = new GameObject();
@@ -126,10 +136,12 @@ namespace Test.Zinnia.Data.Collection
             subject.Relations.Add(relationThree);
 
             Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
 
             Assert.AreEqual(valueTwo, subject.GetValue(1));
 
             Assert.IsTrue(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
 
             Object.DestroyImmediate(keyOne);
             Object.DestroyImmediate(valueOne);
@@ -143,7 +155,9 @@ namespace Test.Zinnia.Data.Collection
         public void GetValueByIndexNotFound()
         {
             UnityEventListenerMock valueRetrievedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock valueNotFoundListenerMock = new UnityEventListenerMock();
             subject.ValueRetrieved.AddListener(valueRetrievedListenerMock.Listen);
+            subject.KeyNotFound.AddListener(valueNotFoundListenerMock.Listen);
 
             GameObject keyOne = new GameObject();
             GameObject valueOne = new GameObject();
@@ -162,10 +176,72 @@ namespace Test.Zinnia.Data.Collection
             subject.Relations.Add(relationThree);
 
             Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
 
             Assert.IsNull(subject.GetValue(2));
 
             Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsTrue(valueNotFoundListenerMock.Received);
+
+            Object.DestroyImmediate(keyOne);
+            Object.DestroyImmediate(valueOne);
+            Object.DestroyImmediate(keyTwo);
+            Object.DestroyImmediate(valueTwo);
+            Object.DestroyImmediate(keyThree);
+            Object.DestroyImmediate(valueThree);
+        }
+
+        [Test]
+        public void GetValueByIndexNotFoundEmpty()
+        {
+            UnityEventListenerMock valueRetrievedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock valueNotFoundListenerMock = new UnityEventListenerMock();
+            subject.ValueRetrieved.AddListener(valueRetrievedListenerMock.Listen);
+            subject.KeyNotFound.AddListener(valueNotFoundListenerMock.Listen);
+
+            GameObjectRelationObservableList relations = containingObject.AddComponent<GameObjectRelationObservableList>();
+            subject.Relations = relations;
+
+            Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
+
+            Assert.IsNull(subject.GetValue(0));
+
+            Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsTrue(valueNotFoundListenerMock.Received);
+        }
+
+        [Test]
+        public void GetValueByIndexNotFoundOutOfBoundsIndex()
+        {
+            UnityEventListenerMock valueRetrievedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock valueNotFoundListenerMock = new UnityEventListenerMock();
+            subject.ValueRetrieved.AddListener(valueRetrievedListenerMock.Listen);
+            subject.KeyNotFound.AddListener(valueNotFoundListenerMock.Listen);
+
+            GameObject keyOne = new GameObject();
+            GameObject valueOne = new GameObject();
+            GameObject keyTwo = new GameObject();
+            GameObject valueTwo = new GameObject();
+            GameObject keyThree = new GameObject();
+            GameObject valueThree = new GameObject();
+
+            GameObjectRelationObservableList relations = containingObject.AddComponent<GameObjectRelationObservableList>();
+            subject.Relations = relations;
+
+            GameObjectRelationObservableList.Relation relationOne = new GameObjectRelationObservableList.Relation() { Key = keyOne, Value = valueOne };
+            GameObjectRelationObservableList.Relation relationThree = new GameObjectRelationObservableList.Relation() { Key = keyThree, Value = valueThree };
+
+            subject.Relations.Add(relationOne);
+            subject.Relations.Add(relationThree);
+
+            Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsFalse(valueNotFoundListenerMock.Received);
+
+            Assert.IsNull(subject.GetValue(5));
+
+            Assert.IsFalse(valueRetrievedListenerMock.Received);
+            Assert.IsTrue(valueNotFoundListenerMock.Received);
 
             Object.DestroyImmediate(keyOne);
             Object.DestroyImmediate(valueOne);
