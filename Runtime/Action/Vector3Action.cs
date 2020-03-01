ff534bf90046f133b265cd418c7832f182648672
Christopher - Marcel Böddecker <bddckr@icloud.com>
2020-03-01 18:36:20 +0000

    feat(Action): add Vector3 action
    The Vector3 Action raises a Vector3 value in the same way as the
    Vector2 Action.

---
+++ Runtime/Action/Vector3Action.cs
@@ -0,0 +1,52 @@
+﻿namespace Zinnia.Action
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+    using Malimbe.MemberChangeMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using Zinnia.Extension;
+
+    /// <summary>
+    /// Emits a <see cref="Vector3"/> value.
+    /// </summary>
+    public class Vector3Action : Action<Vector3Action, Vector3, Vector3Action.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the <see cref="Vector3"/> state.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3>
+        {
+        }
+
+        /// <summary>
+        /// The tolerance of equality between two <see cref="Vector3"/> values.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public float EqualityTolerance { get; set; } = float.Epsilon;
+
+        /// <inheritdoc />
+        protected override bool IsValueEqual(Vector3 value)
+        {
+            return Value.ApproxEquals(value, EqualityTolerance);
+        }
+
+        /// <inheritdoc />
+        protected override bool ShouldActivate(Vector3 value)
+        {
+            return !DefaultValue.ApproxEquals(value, EqualityTolerance);
+        }
+
+        /// <summary>
+        /// Called after <see cref="EqualityTolerance"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(EqualityTolerance))]
+        protected virtual void OnAfterEqualityToleranceChange()
+        {
+            Receive(Value);
+        }
+    }
+}
---
+++ Tests/Editor/Action/Vector3ActionTest.cs
@@ -0,0 +1,159 @@
+﻿using Zinnia.Action;
+
+namespace Test.Zinnia.Action
+{
+    using UnityEngine;
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class Vector3ActionTest
+    {
+        private GameObject containingObject;
+        private Vector3ActionMock subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<Vector3ActionMock>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void ActivatedEmitted()
+        {
+            UnityEventListenerMock activatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock deactivatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock changedListenerMock = new UnityEventListenerMock();
+
+            subject.Activated.AddListener(activatedListenerMock.Listen);
+            subject.Deactivated.AddListener(deactivatedListenerMock.Listen);
+            subject.ValueChanged.AddListener(changedListenerMock.Listen);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(Vector3.one);
+
+            Assert.IsTrue(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsTrue(changedListenerMock.Received);
+        }
+
+        [Test]
+        public void DeactivatedEmitted()
+        {
+            subject.SetIsActivated(true);
+            subject.SetValue(Vector3.one);
+
+            UnityEventListenerMock activatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock deactivatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock changedListenerMock = new UnityEventListenerMock();
+
+            subject.Activated.AddListener(activatedListenerMock.Listen);
+            subject.Deactivated.AddListener(deactivatedListenerMock.Listen);
+            subject.ValueChanged.AddListener(changedListenerMock.Listen);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(Vector3.zero);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsTrue(deactivatedListenerMock.Received);
+            Assert.IsTrue(changedListenerMock.Received);
+        }
+
+        [Test]
+        public void ChangedEmitted()
+        {
+            UnityEventListenerMock changedListenerMock = new UnityEventListenerMock();
+
+            subject.ValueChanged.AddListener(changedListenerMock.Listen);
+
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(new Vector3(0.1f, 0.1f, 0.1f));
+            Assert.IsTrue(changedListenerMock.Received);
+
+            changedListenerMock.Reset();
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(new Vector3(0.1f, 0.1f, 0.1f));
+            Assert.IsFalse(changedListenerMock.Received);
+
+            changedListenerMock.Reset();
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(new Vector3(0.2f, 0.2f, 0.2f));
+            Assert.IsTrue(changedListenerMock.Received);
+        }
+
+        [Test]
+        public void EventsNotEmittedOnInactiveGameObject()
+        {
+            UnityEventListenerMock activatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock deactivatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock changedListenerMock = new UnityEventListenerMock();
+
+            subject.gameObject.SetActive(false);
+            subject.Activated.AddListener(activatedListenerMock.Listen);
+            subject.Deactivated.AddListener(deactivatedListenerMock.Listen);
+            subject.ValueChanged.AddListener(changedListenerMock.Listen);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(Vector3.one);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsFalse(changedListenerMock.Received);
+        }
+
+        [Test]
+        public void EventsNotEmittedOnDisabledComponent()
+        {
+            UnityEventListenerMock activatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock deactivatedListenerMock = new UnityEventListenerMock();
+            UnityEventListenerMock changedListenerMock = new UnityEventListenerMock();
+
+            subject.enabled = false;
+            subject.Activated.AddListener(activatedListenerMock.Listen);
+            subject.Deactivated.AddListener(deactivatedListenerMock.Listen);
+            subject.ValueChanged.AddListener(changedListenerMock.Listen);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsFalse(changedListenerMock.Received);
+
+            subject.Receive(Vector3.one);
+
+            Assert.IsFalse(activatedListenerMock.Received);
+            Assert.IsFalse(deactivatedListenerMock.Received);
+            Assert.IsFalse(changedListenerMock.Received);
+        }
+    }
+
+    public class Vector3ActionMock : Vector3Action
+    {
+        public virtual void SetIsActivated(bool value)
+        {
+            IsActivated = value;
+        }
+
+        public virtual void SetValue(Vector3 value)
+        {
+            Value = value;
+        }
+    }
+}
