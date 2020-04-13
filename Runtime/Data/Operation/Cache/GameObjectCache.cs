f1ef2f06c4087f4df30dd7b3eda307c3774c47b8
Harvey Ball <me@harveyball.com>
2020-04-13 17:54:17 +0100

    feat(Cache): provide cache operations for common data types
    The Cache operation allows a data type value to be stored in a cache
    and then an appropriate event is raised when the value is updated.
    
    If the value is considered equal then the Unmodified event is
    emitted. However, if the values are not equal then the Modified event
    is emitted.

---
+++ Runtime/Data/Operation/Cache/GameObjectCache.cs
@@ -0,0 +1,17 @@
+﻿namespace Zinnia.Data.Operation.Cache
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class GameObjectCache : ValueCache<GameObject, GameObjectCache.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the specified <see cref="GameObject"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<GameObject>
+        {
+        }
+    }
+}
---
+++ Runtime/Data/Operation/Cache/Vector3Cache.cs
@@ -0,0 +1,33 @@
+﻿namespace Zinnia.Data.Operation.Cache
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using System;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using Zinnia.Extension;
+
+    public class Vector3Cache : ValueCache<Vector3, Vector3Cache.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the specified <see cref="Vector3"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<Vector3>
+        {
+        }
+
+        /// <summary>
+        /// The tolerance to consider the current value and the cached value equal.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public float EqualityTolerance { get; set; } = float.Epsilon;
+
+        /// <inheritdoc/>
+        protected override bool AreEqual(Vector3 a, Vector3 b)
+        {
+            return a.ApproxEquals(b, EqualityTolerance);
+        }
+    }
+}
