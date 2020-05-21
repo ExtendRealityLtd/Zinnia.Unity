b65cd031ba829243d3c3800474f36635e0f5c667
extendreality <hello@extendreality.io>
2020-05-21 14:25:00 +0100

    feat(Conversion): ability to convert between float and normalized float
    The FloatToNormalizedFloat allows a float value to be converted into a
    normalized float (between 0f and 1f) and the NormalizedFloatToFloat
    allows for a normalized float to be converted to a float value based
    within the range provided.
    
    The FloatToBoolean has also had the min/max limit removed so any float
    value can be used to check to see if it should be within the positive
    bounds.

---
+++ Tests/Editor/Data/Type/Transformation/Conversion/NormalizedFloatToFloatTest.cs
@@ -0,0 +1,119 @@
+﻿using Zinnia.Data.Type.Transformation.Conversion;
+
+namespace Test.Zinnia.Data.Type.Transformation.Conversion
+{
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+    using UnityEngine;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class NormalizedFloatToFloatTest
+    {
+        private GameObject containingObject;
+        private NormalizedFloatToFloat subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<NormalizedFloatToFloat>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void TransformWithinBounds()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetRange(new Vector2(0f, 10f));
+
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float expected = 5f;
+            float result = subject.Transform(0.5f);
+
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformUnderBounds()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetRange(new Vector2(1f, 10f));
+
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float expected = 1f;
+            float result = subject.Transform(0f);
+
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformOverBounds()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetRange(new Vector2(0f, 10f));
+
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float expected = 10f;
+            float result = subject.Transform(1f);
+
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, subject.Result);
+            Assert.IsTrue(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveGameObject()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetRange(new Vector2(0f, 10f));
+            subject.gameObject.SetActive(false);
+
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float result = subject.Transform(0.5f);
+
+            Assert.AreEqual(default, result);
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+
+        [Test]
+        public void TransformInactiveComponent()
+        {
+            UnityEventListenerMock transformedListenerMock = new UnityEventListenerMock();
+            subject.Transformed.AddListener(transformedListenerMock.Listen);
+            subject.SetRange(new Vector2(0f, 10f));
+            subject.enabled = false;
+
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+
+            float result = subject.Transform(0.5f);
+
+            Assert.AreEqual(default, result);
+            Assert.AreEqual(default, subject.Result);
+            Assert.IsFalse(transformedListenerMock.Received);
+        }
+    }
+}
