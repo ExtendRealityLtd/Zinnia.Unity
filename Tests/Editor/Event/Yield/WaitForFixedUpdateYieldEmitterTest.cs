f1e70c8ffad0795ac3d30a47b642071a481e8b10
Harvey Ball <me@harveyball.com>
2020-07-09 18:13:32 +0100

    feat(Yield): add ability to emit an event after a yield instruction
    The new Yield events provide the ability to trigger some action after
    a yield instruction has completed such as seconds passed or at end of
    frame.
    
    This can be used in conjunction with the Proxy events to first store
    the payload in the Proxy then trigger the emit after the yield
    instruction has completed.

---
+++ Tests/Editor/Event/Yield/WaitForFixedUpdateYieldEmitterTest.cs
@@ -0,0 +1,80 @@
+﻿using Zinnia.Event.Yield;
+
+namespace Test.Zinnia.Event.Yield
+{
+    using NUnit.Framework;
+    using System.Collections;
+    using Test.Zinnia.Utility.Mock;
+    using UnityEngine;
+    using UnityEngine.TestTools;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class WaitForFixedUpdateYieldEmitterTest
+    {
+        private GameObject containingObject;
+        private WaitForFixedUpdateYieldEmitter subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<WaitForFixedUpdateYieldEmitter>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(subject);
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [UnityTest]
+        public IEnumerator Yielded()
+        {
+            UnityEventListenerMock yieldedMock = new UnityEventListenerMock();
+            UnityEventListenerMock cancelledMock = new UnityEventListenerMock();
+            subject.Yielded.AddListener(yieldedMock.Listen);
+            subject.Cancelled.AddListener(cancelledMock.Listen);
+
+            yield return null;
+
+            Assert.IsFalse(yieldedMock.Received);
+            Assert.IsFalse(cancelledMock.Received);
+
+            subject.Begin();
+
+            while (subject.IsRunning)
+            {
+                yield return null;
+            }
+
+            Assert.IsTrue(yieldedMock.Received);
+            Assert.IsFalse(cancelledMock.Received);
+        }
+
+        [UnityTest]
+        public IEnumerator Cancelled()
+        {
+            UnityEventListenerMock yieldedMock = new UnityEventListenerMock();
+            UnityEventListenerMock cancelledMock = new UnityEventListenerMock();
+            subject.Yielded.AddListener(yieldedMock.Listen);
+            subject.Cancelled.AddListener(cancelledMock.Listen);
+
+            yield return null;
+
+            Assert.IsFalse(yieldedMock.Received);
+            Assert.IsFalse(cancelledMock.Received);
+
+            subject.Begin();
+
+            while (subject.IsRunning)
+            {
+                subject.Cancel();
+                yield return null;
+            }
+
+            Assert.IsFalse(yieldedMock.Received);
+            Assert.IsTrue(cancelledMock.Received);
+        }
+    }
+}
