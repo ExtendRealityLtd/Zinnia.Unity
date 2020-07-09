33463ce4ce4fcccf8578472295774c69e73ae98b
Harvey Ball <me@harveyball.com>
2020-07-09 22:25:19 +0100

    feat(Extension): add enum extension/helper methods
    A couple of new Enum helper methods have been added that make getting
    an enum easier by either being able to provide the index of the enum
    to return or by getting the enum by string name.
    
    The PointerElementPropertyMutator has been updated to take advantage
    of this new method.

---
+++ Tests/Editor/Extension/EnumExtensionsTest.cs
@@ -0,0 +1,39 @@
+﻿using Zinnia.Extension;
+
+namespace Test.Zinnia.Extension
+{
+    using NUnit.Framework;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class EnumExtensionsTest
+    {
+        protected enum Test
+        {
+            First,
+            Second,
+            Third
+        }
+
+        [Test]
+        public void GetByIndex()
+        {
+            Assert.AreEqual(Test.First, EnumExtensions.GetByIndex<Test>(-1));
+            Assert.AreEqual(Test.First, EnumExtensions.GetByIndex<Test>(0));
+            Assert.AreEqual(Test.Second, EnumExtensions.GetByIndex<Test>(1));
+            Assert.AreEqual(Test.Third, EnumExtensions.GetByIndex<Test>(2));
+            Assert.AreEqual(Test.Third, EnumExtensions.GetByIndex<Test>(3));
+        }
+
+        [Test]
+        public void GetByString()
+        {
+            Assert.AreEqual(Test.First, EnumExtensions.GetByString<Test>("first"));
+            Assert.AreEqual(Test.First, EnumExtensions.GetByString<Test>("First"));
+            Assert.AreEqual(Test.Second, EnumExtensions.GetByString<Test>("second"));
+            Assert.AreEqual(Test.Second, EnumExtensions.GetByString<Test>("Second"));
+            Assert.AreEqual(Test.Third, EnumExtensions.GetByString<Test>("third"));
+            Assert.AreEqual(Test.Third, EnumExtensions.GetByString<Test>("Third"));
+            NUnit.Framework.Assert.Throws<System.ArgumentException>(() => EnumExtensions.GetByString<Test>("Fourth"));
+        }
+    }
+}
