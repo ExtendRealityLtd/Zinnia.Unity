24264463278c516b5e81f8e87a95008094056506
Ethan Cheung <ethan@fight4dream.com>
2020-07-25 22:46:14 +0800

    feat(Extraction): add extractors for ObjectFollower.EventData
    A collection of extractors that can extract specific data from
    the ObjectFollower.EventData.
    
    For example, an Interactable is using Follow Rigidbody.
    When it is diverged, the event data can be feed into the
    ObjectFollowerEventDataSourceExtractor to get the grabbing
    interactor.

---
+++ Runtime/Tracking/Follow/Operation/Extraction/ObjectFollowerEventDataTargetOffsetExtractor.cs
@@ -0,0 +1,25 @@
+﻿namespace Zinnia.Tracking.Follow.Operation.Extraction
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.Events;
+    using Zinnia.Data.Operation.Extraction;
+
+    /// <summary>
+    /// Extracts the <see cref="GameObject"/> of the EventTargetOffset from a given <see cref="ObjectFollower.EventData"/>.
+    /// </summary>
+    public class ObjectFollowerEventDataTargetOffsetExtractor : GameObjectExtractor<ObjectFollower.EventData, ObjectFollowerEventDataTargetOffsetExtractor.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the specified <see cref="GameObject"/>.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<GameObject> { }
+
+        /// <inheritdoc />
+        protected override GameObject ExtractValue()
+        {
+            return Source != null ? Source.EventTargetOffset : null;
+        }
+    }
+}
---
+++ Tests/Editor/Tracking/Follow/Operation/Extraction/ObjectFollowerEventDataTargetOffsetExtractorTest.cs
@@ -0,0 +1,53 @@
+﻿using Zinnia.Tracking.Follow;
+using Zinnia.Tracking.Follow.Operation.Extraction;
+
+namespace Test.Zinnia.Tracking.Follow.Operation.Extraction
+{
+    using NUnit.Framework;
+    using Test.Zinnia.Utility.Mock;
+    using UnityEngine;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class ObjectFollowerEventDataTargetOffsetExtractorTest
+    {
+        private GameObject containingObject;
+        private ObjectFollowerEventDataTargetOffsetExtractor subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<ObjectFollowerEventDataTargetOffsetExtractor>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void Extract()
+        {
+            GameObject source = new GameObject();
+
+            UnityEventListenerMock extractedMock = new UnityEventListenerMock();
+            subject.Extracted.AddListener(extractedMock.Listen);
+
+            ObjectFollower.EventData eventData = new ObjectFollower.EventData
+            {
+                EventTargetOffset = source
+            };
+
+            Assert.IsFalse(extractedMock.Received);
+            Assert.IsNull(subject.Result);
+
+            subject.Extract(eventData);
+
+            Assert.IsTrue(extractedMock.Received);
+            Assert.AreEqual(subject.Result, source);
+
+            Object.DestroyImmediate(source);
+        }
+    }
+}
