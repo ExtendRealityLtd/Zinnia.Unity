89207a66ceba41107755249e48b3f46297a9e491
Ethan Cheung <ethan@fight4dream.com>
2020-11-25 01:49:01 +0800

    feat(Tracking): add new velocity trackers
    The ConstantVelocityTracker returns the velocity data
    as set on its properties.
    
    The RigidbodyVelocityTracker returns the velocity data
    from a rigidbody.

---
+++ Runtime/Tracking/Velocity/RigidbodyVelocityTracker.cs
@@ -0,0 +1,38 @@
+﻿namespace Zinnia.Tracking.Velocity
+{
+    using Malimbe.MemberClearanceMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+
+    /// <summary>
+    /// Retrieves the velocity for a <see cref="Rigidbody"/>.
+    /// </summary>
+    public class RigidbodyVelocityTracker : VelocityTracker
+    {
+        /// <summary>
+        /// The source to track and estimate velocities for.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: DocumentedByXml]
+        public Rigidbody Source { get; set; }
+
+        /// <inheritdoc />
+        public override bool IsActive()
+        {
+            return base.IsActive() && Source != null && Source.gameObject.activeInHierarchy;
+        }
+        
+        /// <inheritdoc />
+        protected override Vector3 DoGetAngularVelocity()
+        {
+            return Source != null ? Source.angularVelocity : Vector3.zero;
+        }
+
+        /// <inheritdoc />
+        protected override Vector3 DoGetVelocity()
+        {
+            return Source != null ? Source.velocity : Vector3.zero;
+        }
+    }
+}
---
+++ Tests/Editor/Tracking/Velocity/ConstantVelocityTrackerTest.cs
@@ -0,0 +1,85 @@
+﻿using Zinnia.Tracking.Velocity;
+
+namespace Test.Zinnia.Tracking.Velocity
+{
+    using NUnit.Framework;
+    using UnityEngine;
+    using Assert = UnityEngine.Assertions.Assert;
+
+    public class ConstantVelocityTrackerTest
+    {
+        private GameObject containingObject;
+        private ConstantVelocityTracker subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject();
+            subject = containingObject.AddComponent<ConstantVelocityTracker>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void GetVelocity()
+        {
+            Vector3 expectedResult = new Vector3(1.2f, 1.3f, 1.4f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            subject.Velocity = expectedResult;
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocity()
+        {
+            Vector3 expectedResult = new Vector3(1.2f, 1.3f, 1.4f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            subject.AngularVelocity = expectedResult;
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreNotEqual(unexpectedResult, actualResult);
+        }
+
+        [Test]
+        public void GetVelocityUseLocal()
+        {
+            Vector3 v = new Vector3(-1.4f, 1.3f, 1.2f);
+            Vector3 expectedResult = new Vector3(1.2f, 1.3f, 1.4f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            subject.UseLocal = true;
+            subject.Velocity = v;
+            subject.transform.Rotate(0, 90, 0, Space.Self);
+
+            Vector3 actualResult = subject.GetVelocity();
+            Assert.IsTrue(expectedResult == actualResult);
+            Assert.IsFalse(unexpectedResult == actualResult);
+        }
+
+        [Test]
+        public void GetAngularVelocityUseLocal()
+        {
+            Vector3 v = new Vector3(-1.4f, 1.3f, 1.2f);
+            Vector3 expectedResult = new Vector3(1.2f, 1.3f, 1.4f);
+            Vector3 unexpectedResult = Vector3.zero;
+
+            subject.UseLocal = true;
+            subject.AngularVelocity = v;
+            subject.transform.Rotate(0, 90, 0, Space.Self);
+
+            Vector3 actualResult = subject.GetAngularVelocity();
+            Assert.IsTrue(expectedResult == actualResult);
+            Assert.IsFalse(unexpectedResult == actualResult);
+        }
+    }
+}
