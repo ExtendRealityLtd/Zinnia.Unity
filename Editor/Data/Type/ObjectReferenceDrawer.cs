29ad720bfcb6f24b2a9aa92a03d70a05097f4b21
Harvey Ball <me@harveyball.com>
2020-12-12 10:38:29 +0000

    feat(Type): add ability to provide easy reference access to Objects
    The new ObjectReference type and custom drawer allow a Object
    to be specified as a property which can then be shown as a simple
    button in the inspector to highlight the referenced Object within
    the Unity Hierarchy.

---
+++ Editor/Data/Type/ObjectReferenceDrawer.cs
@@ -0,0 +1,53 @@
+﻿namespace Zinnia.Data.Type
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Displays a custom inspector showing a button for the linked reference.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ObjectReference))]
+    public class ObjectReferenceDrawer : PropertyDrawer
+    {
+        private const float BUTTON_SIZE = 22f;
+        private const float BUTTON_PADDING = 0f;
+
+        /// <inheritdoc/>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty linkedReference = property.FindPropertyRelative("linkedObject");
+            SerializedProperty buttonText = property.FindPropertyRelative("linkText");
+            SerializedProperty isActive = property.FindPropertyRelative("isActive");
+
+            EditorGUI.BeginProperty(position, label, property);
+            if (isActive.boolValue)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel(" ");
+
+                if (GUILayout.Button(buttonText.stringValue))
+                {
+                    EditorGUIUtility.PingObject(linkedReference.objectReferenceValue);
+                }
+                if (GUILayout.Button(EditorGUIUtility.IconContent("IN foldout focus on", "|Edit Reference Data"), GUILayout.Width(BUTTON_SIZE)))
+                {
+                    isActive.boolValue = false;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
+            EditorGUI.EndProperty();
+        }
+
+        /// <inheritdoc/>
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty isActive = property.FindPropertyRelative("isActive");
+
+            return isActive.boolValue ? BUTTON_PADDING : EditorGUI.GetPropertyHeight(property);
+        }
+    }
+}
---
+++ Runtime/Data/Type/ObjectReference.cs
@@ -0,0 +1,30 @@
+﻿namespace Zinnia.Data.Type
+{
+    using Malimbe.XmlDocumentationAttribute;
+    using System;
+    using UnityObject = UnityEngine.Object;
+
+    /// <summary>
+    /// Provides a linkable reference to a <see cref="UnityObject"/>.
+    /// </summary>
+    [Serializable]
+
+    public struct ObjectReference
+    {
+        /// <summary>
+        /// The <see cref="UnityObject"/> to reference.
+        /// </summary>
+        [field: DocumentedByXml]
+        public UnityObject linkedObject;
+        /// <summary>
+        /// The text to use for describing the linked reference.
+        /// </summary>
+        [field: DocumentedByXml]
+        public string linkText;
+        /// <summary>
+        /// Whether the reference should be active.
+        /// </summary>
+        [field: DocumentedByXml]
+        public bool isActive;
+    }
+}
