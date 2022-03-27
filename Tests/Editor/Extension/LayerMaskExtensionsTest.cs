19694aef9b2fed53d453a3915eb572d1f369bd0a
Harvey Ball <me@harveyball.com>
2022-03-27 15:08:06 +0100

    feat(Extension): add extension for setting LayerMask
    The LayerMask has now been extended to have a Set(int) and a Set(name)
    method that allows the layer mask to be set via the index value or the
    string literal value of the desired layer.

---
+++ Tests/Editor/Extension/LayerMaskExtensionsTest.cs
@@ -0,0 +1,52 @@
+﻿using Zinnia.Extension;
+
+namespace Test.Zinnia.Extension
+{
+    using NUnit.Framework;
+    using UnityEngine;
+
+    public class LayerMaskExtensionsTest
+    {
+        private LayerMask subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new LayerMask();
+        }
+
+        [Test]
+        public void SetByInt()
+        {
+            Assert.IsFalse(subject == (subject | (1 << 1)));
+            Assert.IsFalse(subject == (subject | (1 << 2)));
+
+            subject = subject.Set(1);
+
+            Assert.IsTrue(subject == (subject | (1 << 1)));
+            Assert.IsFalse(subject == (subject | (1 << 2)));
+
+            subject = subject.Set(2);
+
+            Assert.IsFalse(subject == (subject | (1 << 1)));
+            Assert.IsTrue(subject == (subject | (1 << 2)));
+        }
+
+        [Test]
+        public void SetByString()
+        {
+            Assert.IsFalse(subject == (subject | (1 << 1)));
+            Assert.IsFalse(subject == (subject | (1 << 2)));
+
+            subject = subject.Set("TransparentFX");
+
+            Assert.IsTrue(subject == (subject | (1 << 1)));
+            Assert.IsFalse(subject == (subject | (1 << 2)));
+
+            subject = subject.Set("Ignore Raycast");
+
+            Assert.IsFalse(subject == (subject | (1 << 1)));
+            Assert.IsTrue(subject == (subject | (1 << 2)));
+        }
+    }
+}
