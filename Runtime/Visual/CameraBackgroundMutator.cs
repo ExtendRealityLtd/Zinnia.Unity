3fe8ab8a12f1756de6246c72545652930d0b4010
Harvey Ball <me@harveyball.com>
2023-03-21 09:17:18 +0000

    feat(Visual): add ability to mutate cameras background settings
    The CameraBackgroundMutator allows the ability to mutate a list of
    cameras clearFlags and backgroundColor state and then to restore
    the previous data prior to the mutation.

---
+++ Runtime/Visual/CameraBackgroundMutator.cs
@@ -0,0 +1,129 @@
+﻿namespace Zinnia.Visual
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Zinnia.Data.Collection.List;
+    using Zinnia.Extension;
+
+    /// <summary>
+    /// Attempts to mutate the valid camera background settings and caches the previous settings for later restoration.
+    /// </summary>
+    public class CameraBackgroundMutator : MonoBehaviour
+    {
+        public struct CameraSettings
+        {
+            public CameraClearFlags clearFlags;
+            public Color backgroundColor;
+        }
+
+        [Tooltip("The Camera collection to mutate the background of.")]
+        [SerializeField]
+        private UnityObjectObservableList targetCameras;
+        /// <summary>
+        /// The <see cref="Camera"/> collection to mutate the background of.
+        /// </summary>
+        public UnityObjectObservableList TargetCameras
+        {
+            get
+            {
+                return targetCameras;
+            }
+            set
+            {
+                targetCameras = value;
+            }
+        }
+        [Tooltip("The camera clear flags to set the valid cameras to.")]
+        [SerializeField]
+        private CameraClearFlags targetClearFlags;
+        /// <summary>
+        /// The camera clear flags to set the valid cameras to.
+        /// </summary>
+        public CameraClearFlags TargetClearFlags
+        {
+            get
+            {
+                return targetClearFlags;
+            }
+            set
+            {
+                targetClearFlags = value;
+            }
+        }
+        [Tooltip("The Color to mutate the background to.")]
+        [SerializeField]
+        private Color targetBackgroundColor = Color.black;
+        /// <summary>
+        /// The <see cref="Color"/> to mutate the background to.
+        /// </summary>
+        public Color TargetBackgroundColor
+        {
+            get
+            {
+                return targetBackgroundColor;
+            }
+            set
+            {
+                targetBackgroundColor = value;
+            }
+        }
+
+        /// <summary>
+        /// The existing cached <see cref="Camera"/> settings before it was changed.
+        /// </summary>
+        protected Dictionary<Camera, CameraSettings> cachedCameraSettings = new Dictionary<Camera, CameraSettings>();
+
+        /// <summary>
+        /// Mutates the <see cref="TargetCameras"/> collection with the target settings.
+        /// </summary>
+        public virtual void Mutate()
+        {
+            if (!this.IsValidState())
+            {
+                return;
+            }
+
+            foreach (Object currentCameraObject in TargetCameras.NonSubscribableElements)
+            {
+                Camera currentCamera = (Camera)currentCameraObject;
+                if (currentCamera == null)
+                {
+                    continue;
+                }
+
+                CameraSettings currentSettings = new CameraSettings()
+                {
+                    clearFlags = currentCamera.clearFlags,
+                    backgroundColor = currentCamera.backgroundColor
+                };
+
+                cachedCameraSettings[currentCamera] = currentSettings;
+
+                if (TargetClearFlags != 0)
+                {
+                    currentCamera.clearFlags = TargetClearFlags;
+                }
+                currentCamera.backgroundColor = TargetBackgroundColor;
+            }
+        }
+
+        /// <summary>
+        /// Restores the <see cref="TargetCameras"/> settings to the cached settings before the mutate occurred.
+        /// </summary>
+        public virtual void Restore()
+        {
+            if (!this.IsValidState())
+            {
+                return;
+            }
+
+            foreach (KeyValuePair<Camera, CameraSettings> currentCamera in cachedCameraSettings)
+            {
+                currentCamera.Key.clearFlags = currentCamera.Value.clearFlags;
+                currentCamera.Key.backgroundColor = currentCamera.Value.backgroundColor;
+            }
+
+            cachedCameraSettings.Clear();
+        }
+    }
+}
---
+++ Tests/Editor/Visual/CameraBackgroundMutatorTest.cs
@@ -0,0 +1,146 @@
+﻿using Zinnia.Visual;
+
+namespace Test.Zinnia.Visual
+{
+    using global::Zinnia.Data.Collection.List;
+    using NUnit.Framework;
+    using UnityEngine;
+
+    public class CameraBackgroundMutatorTest
+    {
+        private GameObject containingObject;
+        private CameraBackgroundMutator subject;
+        private Color defaultColor = Color.white;
+
+        [SetUp]
+        public void SetUp()
+        {
+            containingObject = new GameObject("CameraBackgroundMutatorTest");
+            subject = containingObject.AddComponent<CameraBackgroundMutator>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(containingObject);
+        }
+
+        [Test]
+        public void MutateAndRestore()
+        {
+            GameObject camera1Container = new GameObject("CameraBackgroundMutatorTest_Camera1Container");
+            GameObject camera2Container = new GameObject("CameraBackgroundMutatorTest_Camera2Container");
+            GameObject camera3Container = new GameObject("CameraBackgroundMutatorTest_Camera3Container");
+
+            Camera camera1 = camera1Container.AddComponent<Camera>();
+            Camera camera2 = camera2Container.AddComponent<Camera>();
+            Camera camera3 = camera3Container.AddComponent<Camera>();
+
+            camera1.backgroundColor = defaultColor;
+            camera2.backgroundColor = defaultColor;
+            camera3.backgroundColor = defaultColor;
+
+            UnityObjectObservableList cameraList = containingObject.AddComponent<UnityObjectObservableList>();
+
+            cameraList.Add(camera1);
+            cameraList.Add(camera2);
+            cameraList.Add(camera3);
+
+            subject.TargetCameras = cameraList;
+            subject.TargetClearFlags = CameraClearFlags.Color;
+            subject.TargetBackgroundColor = Color.clear;
+
+            Assert.AreEqual(CameraClearFlags.Skybox, camera1.clearFlags);
+            Assert.AreEqual(CameraClearFlags.Skybox, camera2.clearFlags);
+            Assert.AreEqual(CameraClearFlags.Skybox, camera3.clearFlags);
+
+            Assert.AreEqual(defaultColor, camera1.backgroundColor);
+            Assert.AreEqual(defaultColor, camera2.backgroundColor);
+            Assert.AreEqual(defaultColor, camera3.backgroundColor);
+
+            subject.Mutate();
+
+            Assert.AreEqual(CameraClearFlags.Color, camera1.clearFlags);
+            Assert.AreEqual(CameraClearFlags.Color, camera2.clearFlags);
+            Assert.AreEqual(CameraClearFlags.Color, camera3.clearFlags);
+
+            Assert.AreEqual(Color.clear, camera1.backgroundColor);
+            Assert.AreEqual(Color.clear, camera2.backgroundColor);
+            Assert.AreEqual(Color.clear, camera3.backgroundColor);
+
+            subject.Restore();
+
+            Assert.AreEqual(CameraClearFlags.Skybox, camera1.clearFlags);
+            Assert.AreEqual(CameraClearFlags.Skybox, camera2.clearFlags);
+            Assert.AreEqual(CameraClearFlags.Skybox, camera3.clearFlags);
+
+            Assert.AreEqual(defaultColor, camera1.backgroundColor);
+            Assert.AreEqual(defaultColor, camera2.backgroundColor);
+            Assert.AreEqual(defaultColor, camera3.backgroundColor);
+
+            Object.DestroyImmediate(camera1Container);
+            Object.DestroyImmediate(camera2Container);
+            Object.DestroyImmediate(camera3Container);
+        }
+
+        [Test]
+        public void MutateOnInactiveGameObject()
+        {
+            GameObject camera1Container = new GameObject("CameraBackgroundMutatorTest_Camera1Container");
+
+            Camera camera1 = camera1Container.AddComponent<Camera>();
+
+            camera1.backgroundColor = defaultColor;
+
+            UnityObjectObservableList cameraList = containingObject.AddComponent<UnityObjectObservableList>();
+
+            cameraList.Add(camera1);
+
+            subject.TargetCameras = cameraList;
+            subject.TargetClearFlags = CameraClearFlags.Color;
+            subject.TargetBackgroundColor = Color.clear;
+
+            Assert.AreEqual(CameraClearFlags.Skybox, camera1.clearFlags);
+            Assert.AreEqual(defaultColor, camera1.backgroundColor);
+
+            subject.gameObject.SetActive(false);
+
+            subject.Mutate();
+
+            Assert.AreEqual(CameraClearFlags.Skybox, camera1.clearFlags);
+            Assert.AreEqual(defaultColor, camera1.backgroundColor);
+
+            Object.DestroyImmediate(camera1Container);
+        }
+
+        [Test]
+        public void MutateOnInactiveComponent()
+        {
+            GameObject camera1Container = new GameObject("CameraBackgroundMutatorTest_Camera1Container");
+
+            Camera camera1 = camera1Container.AddComponent<Camera>();
+
+            camera1.backgroundColor = defaultColor;
+
+            UnityObjectObservableList cameraList = containingObject.AddComponent<UnityObjectObservableList>();
+
+            cameraList.Add(camera1);
+
+            subject.TargetCameras = cameraList;
+            subject.TargetClearFlags = CameraClearFlags.Color;
+            subject.TargetBackgroundColor = Color.clear;
+
+            Assert.AreEqual(CameraClearFlags.Skybox, camera1.clearFlags);
+            Assert.AreEqual(defaultColor, camera1.backgroundColor);
+
+            subject.enabled = false;
+
+            subject.Mutate();
+
+            Assert.AreEqual(CameraClearFlags.Skybox, camera1.clearFlags);
+            Assert.AreEqual(defaultColor, camera1.backgroundColor);
+
+            Object.DestroyImmediate(camera1Container);
+        }
+    }
+}
