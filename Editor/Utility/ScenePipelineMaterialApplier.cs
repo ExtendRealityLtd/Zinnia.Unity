bf82afb2b0b3114f9090f69e5182367420d615fe
Harvey Ball <me@harveyball.com>
2023-04-03 10:43:53 +0100

    feat(Utility): add pipeline material applier component
    The PipelineMaterialApplier will set the Materials on a given Renderer
    based on the current render pipeline being used. It gets the materials
    to apply from a given list of materials that has a string group name
    that matches the desired render pipeline name.
    
    If no render pipeline is provided then the matched name will be:
    
    * `default`
    
    There is also an editor menu item that will attempt to find all
    PipelineMaterialApplier components in the scene and run them to
    ensure all materials on the renderers are updated to match the current
    render pipeline.

---
+++ Editor/Utility/ScenePipelineMaterialApplier.cs
@@ -0,0 +1,21 @@
+namespace Zinnia.Utility
+{
+#if UNITY_2019_2_OR_NEWER
+    using UnityEditor;
+#endif
+    using UnityEngine;
+
+    public class ScenePipelineMaterialApplier : ScriptableObject
+    {
+#if UNITY_2019_2_OR_NEWER
+        [MenuItem("Window/Zinnia/Apply Pipeline Materials", false, 51)]
+        private static void AddSpatialTargetsDispatcher()
+        {
+            foreach (PipelineMaterialApplier current in FindObjectsOfType<PipelineMaterialApplier>(true))
+            {
+                current.ApplyMaterialsToRenderer();
+            }
+        }
+#endif
+    }
+}
