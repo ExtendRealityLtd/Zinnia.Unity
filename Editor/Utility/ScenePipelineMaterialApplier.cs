c0a0466399399181b7a26c6f2a25e121d93daaf6
Harvey Ball <me@harveyball.com>
2023-04-15 09:13:30 +0100

    fix(Utility): limit pipeline material applier to unity 2020 or above
    The mechanism within the ScenePipelineMaterialApplier uses the
    `FindObjectsOfType` method that in Unity 2019.2 had the additional
    parameter of `findInactive` added, which is required for this
    functionality to work.
    
    However, in Unity 2019.4 this parameter is randomly removed, probably
    due to poor quality control from Unity, so this means that this
    functionality works in 2019.2, 2019.3 but throws an error in 2019.4.
    
    The feature is reintroduced in 2020.1, so the fix is just to support
    this functionality in 2020.1 as Unity won't back port a fix.

--- Editor/Utility/ScenePipelineMaterialApplier.cs
+++ Editor/Utility/ScenePipelineMaterialApplier.cs
@@ -1,13 +1,13 @@
 namespace Zinnia.Utility
 {
-#if UNITY_2019_2_OR_NEWER
+#if UNITY_2020_1_OR_NEWER
     using UnityEditor;
 #endif
     using UnityEngine;
 
     public class ScenePipelineMaterialApplier : ScriptableObject
     {
-#if UNITY_2019_2_OR_NEWER
+#if UNITY_2020_1_OR_NEWER
         [MenuItem("Window/Zinnia/Apply Pipeline Materials", false, 51)]
         private static void AddSpatialTargetsDispatcher()
         {
